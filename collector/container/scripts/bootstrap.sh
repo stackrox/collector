6c3b718dedcf3204930cb44c1b3842c9da11863e
Yathi <yathi@stackrox.com>
2016-09-22 11:23:19 -0700

    Collector support for CentOS7
    - removed dockerfiles for Debian and b2d
    
    Closes #767
    SROX-5566 #Done

--- collector/container/scripts/bootstrap.sh
+++ collector/container/scripts/bootstrap.sh
@@ -10,15 +10,20 @@ function test {
     return $status
 }
 
-function install_sysdig_module() {
+function remove_sysdig_module() {
     if lsmod | grep -q sysdig_probe; then
-        echo "sysdig-probe has already been loaded. Attempting reload..."
+        echo "sysdig-probe has already been loaded. Removing and relegating the task to the collector..."
         test rmmod sysdig-probe.ko
-        test insmod /usr/bin/sysdig-probe.ko
-    else
-        echo "Loading sysdig-probe..."
-        test insmod /usr/bin/sysdig-probe.ko
     fi
+}
+
+function install_kernel_headers_ubuntu() {
+    echo "deb http://security.ubuntu.com/ubuntu trusty-security main" > /etc/apt/sources.list
+    echo "deb http://ftp.us.debian.org/debian jessie main" >> /etc/apt/sources.list
+
+    apt-get update && apt-get install -y linux-headers-$KERNELVERSION
+
+    make KERNELRELEASE=$KERNELVERSION -C /lib/modules/$KERNELVERSION/build M=/driver clean modules
 }
 
 # Get the hostname from Docker so Extractor can use it in its output.
@@ -30,24 +35,11 @@ echo "$HOSTNAME" > /host/etc/hostname
 #sysdig start
 KERNELVERSION=`uname -r`
 OS_DETAILS=$(cat /etc/os-release)
-if echo $KERNELVERSION | grep -q boot2docker; then
-    echo "Installing boot2docker kernel module for Kernel version: $KERNELVERSION"
-    install_sysdig_module
-elif echo $OS_DETAILS | grep -q CentOS; then
-    echo "Installing CentOS kernel module for kernel version: $KERNELVERSION"
-    install_sysdig_module
+if echo $OS_DETAILS | grep -q CentOS; then
+    remove_sysdig_module
 else
-    echo "deb http://security.ubuntu.com/ubuntu trusty-security main" > /etc/apt/sources.list
-    echo "deb http://ftp.us.debian.org/debian jessie main" >> /etc/apt/sources.list
-
-    apt-get update && apt-get install -y linux-headers-$KERNELVERSION
-
-    make KERNELRELEASE=$KERNELVERSION -C /lib/modules/$KERNELVERSION/build M=/driver clean modules
-
-    if lsmod | grep -q sysdig_probe; then
-        echo "sysdig-probe has already been loaded. Removing and relegating the task to the collector..."
-        test rmmod sysdig-probe.ko
-    fi
+    install_kernel_headers_ubuntu
+    remove_sysdig_module
 fi
 #sysdig done
 
