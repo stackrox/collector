584866f399920d05edf20d3a80c08bffc0dcb333
Connor Gilbert <c@stackrox.com>
2017-08-15 17:32:03 -0700

    SROX-9410: Correct whitespace, add comments in StackRox evt formatter code

--- userspace/libsinsp/eventformatter.cpp
+++ userspace/libsinsp/eventformatter.cpp
@@ -55,33 +55,34 @@ sinsp_evt_formatter::sinsp_evt_formatter(sinsp* inspector, const string& fmt)
 {
 	m_inspector = inspector;
 
-        string sysdigFormat = "*";
-        string prefix = "%";
-        vector<string> fields;
-        char* str = new char[fmt.length() + 1];
-        strcpy(str, fmt.c_str());
-        char* token = strtok(str, ",");
-        while (token != NULL) {
-            fields.push_back(token);
-            token = strtok(NULL, ",");
-        }
-        delete[] str;
-        for (vector<string>::iterator it = fields.begin();
-             it != fields.end(); it++) {
-            token = strtok((char*)(*it).c_str(), ":");
-            string mappedValue = token;
-            token = strtok(NULL, ":");
-            string field = token;
-            sysdigFormat += (prefix + field + " ");
-            m_fields_map[field] = mappedValue;
-        }
+	string sysdigFormat = "*";
+	string prefix = "%";
+	vector<string> fields;
+	char* str = new char[fmt.length() + 1];
+	strcpy(str, fmt.c_str());
+	char* token = strtok(str, ",");
+	while (token != NULL) {
+		fields.push_back(token);
+		token = strtok(NULL, ",");
+	}
+	delete[] str;
+	for (vector<string>::iterator it = fields.begin();
+	     it != fields.end(); it++)
+	{
+		token = strtok((char*)(*it).c_str(), ":");
+		string mappedValue = token;
+		token = strtok(NULL, ":");
+		string field = token;
+		sysdigFormat += (prefix + field + " ");
+		m_fields_map[field] = mappedValue;
+	}
 
-        cout << "sysdig format: " << sysdigFormat << endl;
-        for (map<string, string>::iterator it = m_fields_map.begin();
-             it != m_fields_map.end();
-             it++) {
-            cout << it->first << " -> " << it->second << endl;
-        }
+	cout << "sysdig format: " << sysdigFormat << endl;
+	for (map<string, string>::iterator it = m_fields_map.begin();
+	     it != m_fields_map.end(); it++)
+	{
+		cout << it->first << " -> " << it->second << endl;
+	}
 
 	set_format(sysdigFormat);
 }
@@ -141,7 +142,7 @@ void sinsp_evt_formatter::set_format(const string& fmt)
 				m_tokens.push_back(newtkn);
 				m_tokenlens.push_back(0);
 				m_chks_to_free.push_back(newtkn);
-//    			cout << "New token: " << lfmt.substr(last_nontoken_str_start, j - last_nontoken_str_start) << ", nTokens = " << to_string(m_tokens.size()) << endl;
+				// cout << "New token: " << lfmt.substr(last_nontoken_str_start, j - last_nontoken_str_start) << ", nTokens = " << to_string(m_tokens.size()) << endl;
 			}
 
 			if(j == lfmtlen - 1)
@@ -183,22 +184,23 @@ void sinsp_evt_formatter::set_format(const string& fmt)
 			sinsp_filter_check* chk = g_filterlist.new_filter_check_from_fldname(string(cfmt + j + 1),
 				m_inspector,
 				false);
-
-        	// Begin StackRox section
-//			cout << "Parsing: " << string(cfmt + j + 1) << ", nTokens = " << to_string(m_tokens.size()) << endl;
+			// Begin StackRox section
+			// cout << "Parsing: " << string(cfmt + j + 1) << ", nTokens = " << to_string(m_tokens.size()) << endl;
 			char field_name[64] = "<Unknown>";
 			const char* cptr = cfmt + j + 1;
 			for (unsigned int i = 0; i < strlen(cfmt + j + 1); i++) {
-			    if (cptr[i] != ' ')
-			        field_name[i] = cptr[i];
-			    else {
-			        field_name[i] = '\0';
-			        break;
-			    }
+				if (cptr[i] != ' ')
+				{
+					field_name[i] = cptr[i];
+				}
+				else {
+					field_name[i] = '\0';
+					break;
+				}
 			}
 
 			m_token_to_field_map[m_tokens.size()] = field_name;
-        	// End StackRox section
+			// End StackRox section
 
 			if(chk == NULL)
 			{
@@ -223,7 +225,7 @@ void sinsp_evt_formatter::set_format(const string& fmt)
 		m_tokens.push_back(chk);
 		m_chks_to_free.push_back(chk);
 		m_tokenlens.push_back(0);
-//    	cout << "Final new token: " << lfmt.substr(last_nontoken_str_start, j - last_nontoken_str_start) << ", nTokens = " << to_string(m_tokens.size()) << endl;
+		// cout << "Final new token: " << lfmt.substr(last_nontoken_str_start, j - last_nontoken_str_start) << ", nTokens = " << to_string(m_tokens.size()) << endl;
 	}
 }
 
@@ -352,7 +354,7 @@ bool sinsp_evt_formatter::tostring(sinsp_evt* evt, OUT string* res)
 		//}
 	}
 
-    // Begin StackRox section
+	// Begin StackRox section
 	/*
 	if(m_inspector->get_buffer_format() == sinsp_evt::PF_JSON
 	   || m_inspector->get_buffer_format() == sinsp_evt::PF_JSONEOLS
@@ -365,14 +367,13 @@ bool sinsp_evt_formatter::tostring(sinsp_evt* evt, OUT string* res)
 		(*res) = res->substr(0, res->size() - 1);
 	}
 	*/
-  	// End StackRox section
+	// End StackRox section
 
 	return retval;
 }
 
 // Begin StackRox section
-void sinsp_evt_formatter::to_sparse_string(sinsp_evt* evt, char* buffer, unsigned int snaplen,
-											string& network_key)
+void sinsp_evt_formatter::to_sparse_string(sinsp_evt* evt, char* buffer, unsigned int snaplen, string& network_key)
 {
 	uint32_t j = 0;
 
@@ -380,54 +381,81 @@ void sinsp_evt_formatter::to_sparse_string(sinsp_evt* evt, char* buffer, unsigne
 
 	network_key.clear();
 	buffer[0] = 'S';
-    buffer[1] = '\0';
+	buffer[1] = '\0';
 	char* ptr = &buffer[1];
 	bool past_fixed_format = false;
 
+	// m_token_to_field_map has [index in m_tokens] -> [field name]
 	for(map<int, string>::iterator it = m_token_to_field_map.begin();
-	    it != m_token_to_field_map.end(); it++) {
+			it != m_token_to_field_map.end(); it++)
+	{
 		j = it->first;
 		if (j >= m_tokens.size())
+		{
 			continue;
+		}
 
+		// m_fields_map is set via the format string n:evt.field1,n+1:evt.field2...
+		// keys are field names; values are integer codes
 		map<string, string>::iterator f = m_fields_map.find(it->second);
 		if (f == m_fields_map.end())
+		{
 			continue;
-		string mapped_value = f->second;
+		}
+		string mapped_value = f->second; // output format code
 
-	    char* str = m_tokens[j]->tostring(evt);
-		char* field = (char*)it->second.c_str();
-		if (str != NULL) {
+		char* str = m_tokens[j]->tostring(evt); // the field value
+		char* field = (char*)it->second.c_str(); // the field name
+		if (str != NULL)
+		{
 			if (str[0] == '\0')
+			{
 				continue;
+			}
 
-			if (buffer[0] != 'N') {
+			if (buffer[0] != 'N')
+			{
 				if (!strcmp(field, "evt.category") && !strcmp(str, "net"))
+				{
 					buffer[0] = 'N';
+				}
 				else if (!strcmp(field, "fd.type") &&
 					(!strcmp(str, "ipv4") || !strcmp(str, "ipv6") || !strcmp(str, "unix")))
+				{
 					buffer[0] = 'N';
+				}
 			}
-    		if (past_fixed_format) {
-				if (!strcmp(field, "fd.cip") || !strcmp(field, "fd.cport")) {
+
+			if (past_fixed_format)
+			{
+				if (!strcmp(field, "fd.cip") || !strcmp(field, "fd.cport"))
+				{
 					network_key += str;
 				}
-				if (!strcmp(field, "proc.args") || !strcmp(field, "proc.cmdline") || !strcmp(field, "proc.exeline")) {
+
+				if (!strcmp(field, "proc.args") || !strcmp(field, "proc.cmdline") || !strcmp(field, "proc.exeline"))
+				{
 					// if there are tabs in proc.args, proc.cmdline, or proc.exeline,
 					// then replace them with periods
 					char* t = str;
-					while (*t != '\0') {
-						if (*t == 9) {
+					while (*t != '\0')
+					{
+						if (*t == 9)
+						{
 							*t = 46;
 						}
 						t++;
 					}
-				} else if (!strcmp(field, "evt.args") && buffer[0] == 'N') {
+				}
+				else if (!strcmp(field, "evt.args") && buffer[0] == 'N')
+				{
 					// for network data remove anything that comes after "data=" as this is
 					// already part of the buffer
 					char* t = str;
-					while (*t != '\0') {
-						if (*t == 'd' && *(t + 1) == 'a' && *(t + 2) == 't' && *(t + 3) == 'a' && *(t + 4) == '=') {
+					while (*t != '\0')
+					{
+						if (*t == 'd' && *(t + 1) == 'a' && *(t + 2) == 't' && *(t + 3) == 'a' && *(t + 4) == '=')
+						{
 							*t = '\0';
 							break;
 						}
@@ -435,19 +463,25 @@ void sinsp_evt_formatter::to_sparse_string(sinsp_evt* evt, char* buffer, unsigne
 					}
 				}
 				// truncate to snaplen if over
-				if (strlen(str) > snaplen) {
+				if (strlen(str) > snaplen)
+				{
 					str[snaplen] = '\0';
 				}
 				sprintf(ptr, "\t%s:%s", mapped_value.c_str(), str);
 				ptr += strlen(ptr);
-			} else {
+			}
+			else
+			{
 				sprintf(ptr, "\t%s", str);
 				ptr += strlen(ptr);
-				if (!strcmp(field, "evt.time")) {
+				if (!strcmp(field, "evt.time"))
+				{
 					past_fixed_format = true;
 				}
 			}
-		} else if (!strcmp(field, "container.name") || !strcmp(field, "container.image")) {
+		}
+		else if (!strcmp(field, "container.name") || !strcmp(field, "container.image"))
+		{
 			buffer[0] = '\0';
 			return;
 		}
