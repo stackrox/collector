0a7cf2f0ffd375102f1f1139ee72b2c236651486
Connor Gilbert <c@stackrox.com>
2018-08-02 23:38:49 +0000

    Collector: Improve error messages when kernel unavailable
    Improve the signal-to-noise ratio in collector logs when a
    kernel module is not available by:
    
     * Disabling `wget` verbose output
     * Removing `about do X` logs
     * Removing `set -x`
     * Including English text about what has happened and what to do
    
    Pull request #5440
    Approved-by: Dave Terrell <dbt@stackrox.com>

--- collector/container/scripts/bootstrap.sh
+++ collector/container/scripts/bootstrap.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-set -x
+MODULE_NAME="collector"
 
 function test {
     "$@"
@@ -12,44 +11,39 @@ function test {
     return $status
 }
 
-function remove_collector_module() {
-    if lsmod | grep -q collector; then
+function remove_module() {
+    if lsmod | grep -q "$MODULE_NAME"; then
         echo "Collector kernel module has already been loaded."
         echo "Removing so that collector can insert it at startup."
-        test rmmod collector
+        test rmmod "$MODULE_NAME"
     fi
 }
 
 function download_kernel_module() {
-    MODULE_NAME="$1"
-    KERNEL_MODULE=$MODULE_NAME-$KERNELVERSION.ko
-    echo >&2
-    echo "Attempting to download $KERNEL_MODULE." >&2
+    KERNEL_MODULE=$MODULE_NAME-$KERNEL_VERSION.ko
     if [[ ! -d /module ]]; then
       echo >&2 "/module directory does not exist."
       return 1
     fi
-    wget -O /module/$MODULE_NAME.ko $MODULE_URL/$DISTRO/$KERNEL_MODULE
+    local URL="$MODULE_URL/$DISTRO/$KERNEL_MODULE"
+    wget --no-verbose -O /module/$MODULE_NAME.ko "$URL"
     if [ $? -ne 0 ]; then
-      echo "Error downloading $DISTRO/$KERNEL_MODULE from remote repository." >&2
+      echo "Error downloading $MODULE_NAME module for $DISTRO kernel version $KERNEL_VERSION." >&2
       return 1
     fi
     chmod 0444 /module/$MODULE_NAME.ko
-    echo "Using $DISTRO/$KERNEL_MODULE downloaded from remote repository." >&2
+    echo "Using downloaded $MODULE_NAME module for $DISTRO kernel version $KERNEL_MODULE." >&2
     return 0
 }
 
 function find_kernel_module() {
-    MODULE_NAME="$1"
-    KERNEL_MODULE=$MODULE_NAME-$KERNELVERSION.ko
-    echo >&2
-    echo "Attempting to find built-in $KERNEL_MODULE." >&2
+    KERNEL_MODULE=$MODULE_NAME-$KERNEL_VERSION.ko
     EXPECTED_PATH="/kernel-modules/$DISTRO/$KERNEL_MODULE"
     if [ -f "$EXPECTED_PATH" ]; then
       mkdir -p /module/
       cp "$EXPECTED_PATH" /module/$MODULE_NAME.ko
       chmod 777 /module/$MODULE_NAME.ko
-      echo "Using built-in $EXPECTED_PATH." >&2
+      echo "Using built-in $MODULE_NAME module for $DISTRO kernel version $KERNEL_VERSION." >&2
       return 0
     fi
     echo "Didn't find $KERNEL_MODULE built-in." >&2
@@ -62,16 +56,11 @@ if [ "$ROX_PLATFORM" = "swarm" ]; then
 fi
 
 # Get the hostname from Docker so this container can use it in its output.
-export NODE_HOSTNAME=$(curl -s --unix-socket /host/var/run/docker.sock http://localhost/info | jq --raw-output .Name)
+export NODE_HOSTNAME=""
+NODE_HOSTNAME=$(curl -s --unix-socket /host/var/run/docker.sock http://localhost/info | jq --raw-output .Name)
 
-# kernel module download/build
-KERNELVERSION=`uname -r`
-if [ -z ${KERNEL_VERSION+x} ]; then
-    echo "KERNELVERSION set to $KERNELVERSION.";
-else
-    KERNELVERSION=$KERNEL_VERSION
-    echo "KERNELVERSION is set to '$KERNEL_VERSION'.";
-fi
+KERNEL_VERSION=`uname -r`
+echo "Kernel version detected: $KERNEL_VERSION.";
 
 OS_DETAILS=$(curl -s --unix-socket /host/var/run/docker.sock http://localhost/info | jq --raw-output .OperatingSystem)
 
@@ -92,17 +81,20 @@ else
     exit 1
 fi
 
-find_kernel_module collector
+find_kernel_module
 if [ $? -ne 0 ]; then
-  download_kernel_module collector
+  download_kernel_module
   if [ $? -ne 0 ]; then
-    echo "Could not find or download a collector module."
+    echo "The $MODULE_NAME module may not have been compiled for this version yet." >&2
+    echo "Please provide this complete error message to StackRox support." >&2
+    echo "This program will now exit and retry when it is next restarted." >&2
+    echo "" >&2
     exit 1
   fi
 fi
 
-# The collector binary will insert the collector module.
-remove_collector_module
+# The collector program will insert the kernel module upon startup.
+remove_module
 
 # Uncomment this to enable generation of core for Collector
 # echo '/core/core.%e.%p.%t' > /proc/sys/kernel/core_pattern
@@ -117,10 +109,10 @@ clean_up() {
 
 # Remove "/bin/sh -c" from arguments
 shift;shift
-echo "Exec $@"
+echo "Exec $*"
 # Signal handler for SIGTERM
 trap 'clean_up' TERM QUIT INT
-eval "exec $@" &
+eval exec "$@" &
 PID=$!
 wait $PID
-remove_collector_module
+remove_module
