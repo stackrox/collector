8964a68a3bf25afac0f4f3c9af91730956247e1f
Josh Komoroske <josh@stackrox.com>
2018-08-22 12:05:46 -0700

    Mass import of proto files

---
+++ scripts/go-get-version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+function usage() {
+  echo "To go get a package at a particular version, use:"
+  echo "$0 <package> <hash_or_tag> [--skip-install]"
+  exit 1
+}
+
+[[ "$#" -ge 2 ]] || usage
+
+package="$1"
+hash_or_tag="$2"
+skip_install="$3"
+
+package_without_trailing_dots="${package%/...}"
+go get -d -v "${package_without_trailing_dots}" || true
+cd "${GOPATH}/src/${package_without_trailing_dots}"
+
+git rev-parse --git-dir > /dev/null 2>&1 || { echo "This script only supports git-based packages!"; exit 1; }
+git checkout -q "${hash_or_tag}"
+[[ "$skip_install" = "--skip-install" ]] || go install "${package}"
