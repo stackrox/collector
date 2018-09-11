eab2c165ce4319df55ca9130241a2434f5c07b3a
Josh Komoroske <josh@stackrox.com>
2018-09-10 17:35:24 -0700

    Simplify streaming of command output

--- kernel-modules/build/kobuild/command/command.go
+++ kernel-modules/build/kobuild/command/command.go
@@ -6,19 +6,6 @@ import (
 	"os/exec"
 )
 
-// stream shuttles data from input to output until exhausted.
-func stream(input io.ReadCloser, output *os.File) {
-	buffer := make([]byte, 1024)
-	defer input.Close()
-	for {
-		n, err := input.Read(buffer)
-		if err != nil {
-			return
-		}
-		output.Write(buffer[0:n])
-	}
-}
-
 // Run will exec the given command and stream all output (stdout and stderr)
 // back to the current terminal.
 func Run(name string, arg ...string) error {
@@ -34,8 +21,8 @@ func Run(name string, arg ...string) error {
 		return err
 	}
 
-	go stream(stdout, os.Stdout)
-	go stream(stderr, os.Stderr)
+	go io.Copy(os.Stdout, stdout)
+	go io.Copy(os.Stderr, stderr)
 
 	return cmd.Run()
 }
