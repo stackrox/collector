68355ab2a28d3e44c6657b83c806a98dee2bfdda
Malte Isberner <misberner@users.noreply.github.com>
2018-09-14 12:17:57 -0700

    Link more libs statically (#39)

--- builder/build/install_rdkafka.sh
+++ builder/build/install_rdkafka.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/edenhill/librdkafka.git \
     && cd librdkafka \
     && git checkout $RDKAFKA_REVISION \
-    && ./configure \
+    && ./configure --disable-sasl \
     && make -j 6 \
     && make install \
     ;
