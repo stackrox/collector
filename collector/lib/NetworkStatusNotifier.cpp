b22b4077855a1ebe89687d0f3a791c10763fa0f8
Malte Isberner <mi@stackrox.com>
2018-09-19 11:03:49 -0700

    wip

--- collector/lib/NetworkStatusNotifier.cpp
+++ collector/lib/NetworkStatusNotifier.cpp
@@ -1,53 +1,66 @@
-////
-//// Created by Malte Isberner on 9/18/18.
-////
 //
-//#include "NetworkStatusNotifier.h"
+// Created by Malte Isberner on 9/18/18.
 //
-//namespace collector {
-//
-//void NetworkStatusNotifier::Run() {
-//  v1::Empty empty;
-//  auto client_writer = stub_->PushNetworkConnectionInfo(nullptr, &empty);
-//}
-//
-//bool NetworkStatusNotifier::RunSingle(grpc::ClientWriter<sensor::NetworkConnectionInfoMessage>* writer) {
-//  writer->WaitForInitialMetadata();
-//
-//  ConnMap old_state;
-//
-//  do {
-//    auto new_state = this->conn_tracker_->FetchState(true);
-//    ConnectionTracker::ComputeDelta(new_state, &old_state);
-//
-//    const auto* msg = CreateInfoMessage(old_state);
-//    old_state = std::move(new_state);
-//
-//    if (!msg) continue;
-//
-//    if (!writer->Write(*msg)) {
-//      break;
-//    }
-//  } while (thread_.Pause(std::chrono::seconds(30)));
-//
-//  auto status = writer->Finish();
-//  if (!status.ok()) {
-//    CLOG(ERROR) << "Failed to write network connection info: " << status.error_message();
-//  }
-//
-//  return true;
-//}
-//
-//const sensor::NetworkConnectionInfoMessage* NetworkStatusNotifier::CreateMessage(const ConnMap& delta) {
-//  if (delta.empty()) {
-//    return nullptr;
-//  }
-//
-//  Reset();
-//  const auto* msg = AllocateRoot();
-//
-//
-//}
-//
-//
-//}  // namespace collector
+
+#include "NetworkStatusNotifier.h"
+
+namespace collector {
+
+void NetworkStatusNotifier::Run() {
+  while (!thread_.should_stop()) {
+    v1::Empty empty;
+    auto client_writer = stub_->PushNetworkConnectionInfo(&context_, &empty);
+
+    if (!RunSingle(client_writer.get())) {
+      break;
+    }
+  }
+}
+
+void NetworkStatusNotifier::Start() {
+  thread_.Start([this]{ Run(); });
+}
+
+void NetworkStatusNotifier::Stop() {
+  thread_.Stop();
+  context_.TryCancel();
+}
+
+bool NetworkStatusNotifier::RunSingle(grpc::ClientWriter<sensor::NetworkConnectionInfoMessage>* writer) {
+  ConnMap old_state;
+
+  do {
+    auto new_state = this->conn_tracker_->FetchState(true);
+    ConnectionTracker::ComputeDelta(new_state, &old_state);
+
+    const auto* msg = CreateInfoMessage(old_state);
+    old_state = std::move(new_state);
+
+    if (!msg) continue;
+
+    if (!writer->Write(*msg)) {
+      break;
+    }
+  } while (thread_.Pause(std::chrono::seconds(30)));
+
+  auto status = writer->Finish();
+  if (!status.ok()) {
+    CLOG(ERROR) << "Failed to write network connection info: " << status.error_message();
+  }
+
+  return !thread_.should_stop();
+}
+
+const sensor::NetworkConnectionInfoMessage* NetworkStatusNotifier::CreateMessage(const ConnMap& delta) {
+  if (delta.empty()) {
+    return nullptr;
+  }
+
+  Reset();
+  const auto* msg = AllocateRoot();
+
+
+}
+
+
+}  // namespace collector
--- collector/lib/NetworkStatusNotifier.h
+++ collector/lib/NetworkStatusNotifier.h
@@ -1,46 +1,53 @@
-////
-//// Created by Malte Isberner on 9/18/18.
-////
 //
-//#ifndef COLLECTOR_NETWORKSTATUSNOTIFIER_H
-//#define COLLECTOR_NETWORKSTATUSNOTIFIER_H
+// Created by Malte Isberner on 9/18/18.
 //
-//#include <memory>
-//
-//#include <grpc/grpc.h>
-//#include <grpcpp/channel.h>
-//#include <grpcpp/client_context.h>
-//#include <grpcpp/create_channel.h>
-//#include <grpcpp/security/credentials.h>
-//
-//#include "internalapi/sensor/network_connection_service.grpc.pb.h"
-//
-//#include "ConnTracker.h"
-//#include "ProtoAllocator.h"
-//#include "StoppableThread.h"
-//
-//namespace collector {
-//
-//class NetworkStatusNotifier : protected ProtoAllocator<sensor::NetworkConnectionInfoMessage> {
-// public:
-//  using Stub = sensor::NetworkConnectionInfoService::Stub;
-//
-//  explicit NetworkStatusNotifier(std::shared_ptr<ConnectionTracker> conn_tracker)
-//      : conn_tracker_(std::move(conn_tracker)) {}
-//
-//  void Run();
-//
-// private:
-//  bool RunSingle(grpc::ClientWriter<sensor::NetworkConnectionInfoMessage>* writer);
-//
-//  StoppableThread thread_;
-//
-//  std::unique_ptr<Stub> stub_;
-//  std::unique_ptr<grpc::ClientWriter<sensor::NetworkConnectionInfoMessage>> writer_;
-//
-//  std::shared_ptr<ConnectionTracker> conn_tracker_;
-//};
-//
-//}  // namespace collector
-//
-//#endif //COLLECTOR_NETWORKSTATUSNOTIFIER_H
+
+#ifndef COLLECTOR_NETWORKSTATUSNOTIFIER_H
+#define COLLECTOR_NETWORKSTATUSNOTIFIER_H
+
+#include <memory>
+
+#include <grpc/grpc.h>
+#include <grpcpp/channel.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/security/credentials.h>
+
+#include "internalapi/sensor/network_connection_service.grpc.pb.h"
+
+#include "ConnTracker.h"
+#include "ProtoAllocator.h"
+#include "StoppableThread.h"
+
+namespace collector {
+
+class NetworkStatusNotifier : protected ProtoAllocator<sensor::NetworkConnectionInfoMessage> {
+ public:
+  using Stub = sensor::NetworkConnectionInfoService::Stub;
+
+  explicit NetworkStatusNotifier(std::shared_ptr<ConnectionTracker> conn_tracker)
+      : conn_tracker_(std::move(conn_tracker)) {}
+
+  void Run();
+
+  void Start();
+  void Stop();
+
+ private:
+  NetworkConnectionInfoMessage* CreateMessage(const ConnMap& conn_delta);
+
+  bool RunSingle(grpc::ClientWriter<sensor::NetworkConnectionInfoMessage>* writer);
+
+  grpc::ClientContext context_;
+
+  StoppableThread thread_;
+
+  std::unique_ptr<Stub> stub_;
+  std::unique_ptr<grpc::ClientWriter<sensor::NetworkConnectionInfoMessage>> writer_;
+
+  std::shared_ptr<ConnectionTracker> conn_tracker_;
+};
+
+}  // namespace collector
+
+#endif //COLLECTOR_NETWORKSTATUSNOTIFIER_H
