7b3a82c72976c9fcc0bded4d3df593c288fb1054
Malte Isberner <mi@stackrox.com>
2018-11-19 22:55:18 -0800

    use a proper out-of-tree build

--- sysdig/build/build.sh
+++ sysdig/build/build.sh
@@ -1,9 +1,8 @@
 #! /bin/sh
 set -eux
 
-mkdir -p /sysdig-src/build
-cd /sysdig-src/build
-find .
+mkdir -p /sysdig-build
+cd /sysdig-build
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DRIVER=OFF \
     -DUSE_BUNDLED_DEPS=ON \
     -DUSE_BUNDLED_ZLIB=OFF \
@@ -11,7 +10,6 @@ cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DRIVER=OFF \
     -DUSE_BUNDLED_OPENSSL=OFF \
     -DUSE_BUNDLED_CURL=OFF \
     -DUSE_BUNDLED_LUAJIT=OFF \
-    ..
-find .
+    /sysdig-src
 make --debug=bm
 strip --strip-unneeded ./userspace/libsinsp/libsinsp-wrapper.so
