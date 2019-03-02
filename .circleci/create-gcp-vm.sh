88aee5c90ae076b0bcb362b42fdd6a134b68fb73
Rudi Cilibrasi <rudi@stackrox.com>
2019-03-02 06:48:47 +0000

    begin integration test

--- .circleci/create-gcp-vm.sh
+++ .circleci/create-gcp-vm.sh
@@ -4,6 +4,8 @@ DOCKER_USER="$1"
 shift
 DOCKER_PASS="$1"
 shift
+SOURCE_ROOT="$1"
+shift
 
 set -e
 REGION=us-central1
@@ -33,15 +35,21 @@ fi
 
 echo "A000"
 sleep 30  # give it time to boot
+gitdir="$SOURCE_ROOT"
 cd /tmp
-echo "A001"
-gitdir=${SOURCE_ROOT}
+git clone $gitdir shipdir
+rm -rf shipdir/.git
+echo $CIRCLE_BUILD_NUM > shipdir/buildnum.txt
+mkdir s2
+mv shipdir s2/collector
+cd s2
+tar cvfz collector.tar.gz collector/
 cd ..
-echo "A005"
+mv s2/collector.tar.gz .
 rm -rf s2
-echo "A007"
+gcloud compute scp collector.tar.gz "collector-nb-${CIRCLE_BUILD_NUM}":
 gcloud compute ssh "collector-nb-${CIRCLE_BUILD_NUM}" --command "(which docker || DEBIAN_FRONTEND=noninteractive sudo apt install -y make cmake g++ gcc apt-transport-https ca-certificates curl gnupg-agent wget software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable' && sudo apt update -y && DEBIAN_FRONTEND=noninteractive sudo apt install -y docker-ce && sudo adduser $(id -un) docker)"
-
 gcloud compute ssh "collector-nb-${CIRCLE_BUILD_NUM}" --command "docker login -u '$DOCKER_USER' -p '$DOCKER_PASS'"
+gcloud compute ssh "tar xvpfz collector.tar.gz && rm collector.tar.gz"
 echo "A008"
 exit 0
