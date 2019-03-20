6a00b7da7dcbc6b09d66a8b9bc4f0d45d450d97f
Robby Cochran <rc@stackrox.com>
2019-03-20 06:20:23 +0000

    blacklist cos kernels with loginuid inside audit struct

--- sysdig/src/driver/bpf/quirks.h
+++ sysdig/src/driver/bpf/quirks.h
@@ -15,10 +15,8 @@ or GPL2.txt for full copies of the license.
 #error Kernel version must be >= 4.14 with eBPF enabled
 #endif
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 13, 0) && LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 4)
 #define randomized_struct_fields_start  struct {
 #define randomized_struct_fields_end    };
-#endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0)
 #define BPF_FORBIDS_ZERO_ACCESS
