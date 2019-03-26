18ab5c75e3dfc828334dfd17527e03b12a634871
Rudi <46460885+srudi88@users.noreply.github.com>
2019-03-26 16:57:45 -0700

    Rudi robby check cos (#175)
    * Add patch for eBPF COS compilation on legacy version
    
    * try switching to bash functions instead of scripts for cos
    
    * fix GCP_VM_NAME stuff
    
    * add missing colon

--- .circleci/create-gcp-vm.sh
+++ .circleci/create-gcp-vm.sh
@@ -1,55 +1,21 @@
 #!/usr/bin/env bash
 
-DOCKER_USER="$1"
-shift
-DOCKER_PASS="$1"
-shift
-SOURCE_ROOT="$1"
-shift
-
 set -e
-REGION=us-central1
+GDOCKER_USER="$1"
+shift
+GDOCKER_PASS="$1"
+shift
+GSOURCE_ROOT="$1"
+shift
 
-#zones=$(gcloud compute zones list --filter="region=$REGION" | grep UP | cut -f1 -d' ')
-success=false
-for zone in us-central1-a us-central1-b ; do
-    echo "Trying zone $zone"
-    gcloud config set compute/zone "${zone}"
-    if gcloud compute instances create \
-      --image-family ubuntu-1804-lts \
-      --image-project ubuntu-os-cloud \
-      --service-account=circleci-collector@stackrox-ci.iam.gserviceaccount.com \
-        "collector-nb-${CIRCLE_BUILD_NUM}"
-    then
-				success=true
-        break
-    else
-        gcloud compute instances delete "collector-nb-${CIRCLE_BUILD_NUM}"
-    fi
-done
+source "$GSOURCE_ROOT/.circleci/ma.sh"
 
-if test ! "$success" = "true" ; then
-  echo "Could not boot instance."
-  exit 1
-fi
+CIRCLE_BUILD_VM_NAME="collector-nb-${CIRCLE_BUILD_NUM}"
 
-echo "A000"
-sleep 30  # give it time to boot
-gitdir="$SOURCE_ROOT"
-cd /tmp
-git clone $gitdir shipdir
-rm -rf shipdir/.git
-echo $CIRCLE_BUILD_NUM > shipdir/buildnum.txt
-mkdir s2
-mv shipdir s2/collector
-cd s2
-tar cvfz collector.tar.gz collector/
-cd ..
-mv s2/collector.tar.gz .
-rm -rf s2
-gcloud compute scp collector.tar.gz "collector-nb-${CIRCLE_BUILD_NUM}":
-gcloud compute ssh "collector-nb-${CIRCLE_BUILD_NUM}" --command "(which docker || export DEBIAN_FRONTEND=noninteractive ; sudo apt update -y && sudo apt install -y make cmake g++ gcc apt-transport-https ca-certificates curl gnupg-agent wget software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable' && sudo apt update -y && DEBIAN_FRONTEND=noninteractive sudo apt install -y docker-ce && sudo adduser $(id -un) docker)"
-gcloud compute ssh "collector-nb-${CIRCLE_BUILD_NUM}" --command "docker login -u '$DOCKER_USER' -p '$DOCKER_PASS'"
-gcloud compute ssh "collector-nb-${CIRCLE_BUILD_NUM}" --command "tar xvpfz collector.tar.gz && rm collector.tar.gz"
+createGCPVMUbuntu "$CIRCLE_BUILD_VM_NAME" "$GSOURCE_ROOT"
+installVariousAptDepsViaGCPSSH "$CIRCLE_BUILD_VM_NAME"
+loginDockerViaGCPSSH "$CIRCLE_BUILD_VM_NAME" "$GDOCKER_USER" "$GDOCKER_PASS"
+extractSourceTarballViaGCPSSH "$CIRCLE_BUILD_VM_NAME"
+
 echo "A008"
 exit 0
---
+++ .circleci/ma.sh
@@ -0,0 +1,81 @@
+createGCPVMUbuntu() {
+  GCP_VM_NAME="$1"
+  shift
+  SOURCE_ROOT="$1"
+  shift
+
+  REGION=us-central1
+
+  #zones=$(gcloud compute zones list --filter="region=$REGION" | grep UP | cut -f1 -d' ')
+  success=false
+  for zone in us-central1-a us-central1-b ; do
+      echo "Trying zone $zone"
+      gcloud config set compute/zone "${zone}"
+      if gcloud compute instances create \
+        --image-family ubuntu-1804-lts \
+        --image-project ubuntu-os-cloud \
+        --service-account=circleci-collector@stackrox-ci.iam.gserviceaccount.com \
+          "$GCP_VM_NAME"
+      then
+          success=true
+          break
+      else
+          gcloud compute instances delete "$GCP_VM_NAME"
+      fi
+  done
+
+  if test ! "$success" = "true" ; then
+    echo "Could not boot instance."
+    return 1
+  fi
+  echo "A000"
+  sleep 30  # give it time to boot
+  buildSourceTarball "$SOURCE_ROOT"
+  scpSourceTarballToGcpHost "$GCP_VM_NAME"
+  return 0
+}
+
+# builds collector.tar.gz from $1 git clone (not working dir!)
+buildSourceTarball() {
+  gitdir="$1"
+  cd /tmp
+  git clone $gitdir shipdir
+  rm -rf shipdir/.git
+  echo $CIRCLE_BUILD_NUM > shipdir/buildnum.txt
+  mkdir s2
+  mv shipdir s2/collector
+  cd s2
+  tar cvfz collector.tar.gz collector/
+  cd ..
+  mv s2/collector.tar.gz .
+  rm -rf s2
+}
+
+# assumes file in current working dir collector.tar.gz should be copied
+# to $1 : destination instancename for GCP.
+scpSourceTarballToGcpHost() {
+  GCP_VM_NAME="$1"
+  gcloud compute scp collector.tar.gz "$GCP_VM_NAME":
+}
+
+# TODO: fix function name
+installVariousAptDepsViaGCPSSH() {
+  GCP_VM_NAME="$1"
+  gcloud compute ssh "$GCP_VM_NAME" --command "(which docker || export DEBIAN_FRONTEND=noninteractive ; sudo apt update -y && sudo apt install -y make cmake g++ gcc apt-transport-https ca-certificates curl gnupg-agent wget software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable' && sudo apt update -y && DEBIAN_FRONTEND=noninteractive sudo apt install -y docker-ce && sudo adduser $(id -un) docker)"
+}
+# parameters GCPVMName dockerUsername dockerPassword
+loginDockerViaGCPSSH() {
+  GCP_VM_NAME="$1"
+  shift
+  DOCKER_USER="$1"
+  shift
+  DOCKER_PASS="$1"
+  shift
+  gcloud compute ssh "$GCP_VM_NAME" --command "docker login -u '$DOCKER_USER' -p '$DOCKER_PASS'"
+}
+
+extractSourceTarballViaGCPSSH() {
+  GCP_VM_NAME="$1"
+  gcloud compute ssh "$GCP_VM_NAME" --command "tar xvpfz collector.tar.gz && rm collector.tar.gz"
+}
+
