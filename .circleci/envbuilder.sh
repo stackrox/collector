1f9a73dce6431a94f1ce3e3633fd2281fb412c8e
Rudi Cilibrasi <rudi@stackrox.com>
2019-04-15 18:56:36 -0700

    set up cos integration test for ebpf

--- .circleci/envbuilder.sh
+++ .circleci/envbuilder.sh
@@ -13,6 +13,21 @@ runGCPUbuntuTestViaSSH() {
   extractSourceTarballViaGCPSSH "$GCP_VM_NAME"
 }
 
+runGCPCosTestViaSSH() {
+  local GCP_VM_NAME="$1"
+  shift
+  local GDOCKER_USER="$1"
+  shift
+  local GDOCKER_PASS="$1"
+  shift
+  local GSOURCE_ROOT="$1"
+  shift
+  installVariousAptDepsViaGCPSSH "$GCP_VM_NAME"
+  loginDockerViaGCPSSH "$GCP_VM_NAME" "$GDOCKER_USER" "$GDOCKER_PASS"
+  extractSourceTarballViaGCPSSH "$GCP_VM_NAME"
+  cd ../../integration-tests
+}
+
 runCircleGCPUbuntuTestViaSSH() {
   local GDOCKER_USER="$1"
   shift
