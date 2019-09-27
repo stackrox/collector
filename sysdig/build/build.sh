fa04040dda6354d987b563e936c3e94d9d3a0a10
Robby Cochran <rc@stackrox.com>
2019-09-27 11:40:04 -0700

    Upgrade sysdig from 0.24 to 0.26 (#221)
    collector: sysdig now requires grpc and protobuf libs for crio support -- use packaged versions from stretch backports
    sysdig: rebase our changes on top of sysdig 0.26.4 (their latest release), tweak driver CMakeLists.txt

--- sysdig/build/build.sh
+++ sysdig/build/build.sh
@@ -13,6 +13,9 @@ cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DRIVER=OFF \
     -DUSE_BUNDLED_CURL=OFF \
     -DUSE_BUNDLED_JSONCPP=OFF \
     -DUSE_BUNDLED_LUAJIT=OFF \
+    -DUSE_BUNDLED_CARES=OFF \
+    -DUSE_BUNDLED_PROTOBUF=OFF \
+    -DUSE_BUNDLED_GRPC=OFF \
     /sysdig-src
 make -j6
 strip --strip-unneeded ./userspace/libsinsp/libsinsp-wrapper.so
