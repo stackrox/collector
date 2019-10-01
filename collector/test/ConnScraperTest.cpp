44fad9ccb29c173a81647826be5327e3a21cb263
Malte Isberner <mi@stackrox.com>
2019-10-01 21:48:36 +0200

    add more tests

--- collector/test/ConnScraperTest.cpp
+++ collector/test/ConnScraperTest.cpp
@@ -45,13 +45,21 @@ TEST(ConnScraperTest, TestExtractContainerID) {
           "e73c55f3e7f5",
       },
       {
-          "/docker/951e643e3c241b225b6284ef2b79a37c13fc64cbf65b5d46bda95fcb98fe63a4",
+          "5:pids:/docker/951e643e3c241b225b6284ef2b79a37c13fc64cbf65b5d46bda95fcb98fe63a4",
           "951e643e3c24",
       },
       {
           "12:pids:/kubepods/kubepods/besteffort/pod690705f9-df6e-11e9-8dc5-025000000001/c3bfd81b7da0be97190a74a7d459f4dfa18f57c88765cde2613af112020a1c4b",
           "c3bfd81b7da0",
-      }
+      },
+      {
+          "10:blkio:/kubepods/burstable/pod7cd3dba6-e475-11e9-8f99-42010a8a00d2/2bc55a8cae1704a733ba5d785d146bbed9610483380507cbf00c96b32bb637e1",
+          "2bc55a8cae17",
+      },
+      {
+          "6:hugetlb:/kubepods.slice/kubepods-burstable.slice/kubepods-burstable-podce705797_e47e_11e9_bd71_42010a000002.slice/docker-6525e65814a99d431b6978e8f8c895013176c6c58173b56639d4b020c14e6022.scope",
+          "6525e65814a9",
+      },
   };
 
   for (const auto &c : cases) {
