cb494e20b37de5effdd045d588da811a47808ae6
Connor Gorman <cgorman@stackrox.com>
2019-11-21 10:39:27 -0800

    PR comments

--- collector/lib/ProcessSignalHandler.cpp
+++ collector/lib/ProcessSignalHandler.cpp
@@ -31,11 +31,13 @@ namespace collector {
 
 std::string compute_process_key(const ::storage::ProcessSignal& s) {
   std::stringstream ss;
-  auto args = s.args();
-  if (s.args().length() > 256) {
-    args = args.substr(0, 256);
+  ss << s.container_id() << " " << s.name() << " ";
+  if (s.args().length() <= 256) {
+    ss << s.args();
+  } else {
+    ss.write(s.args().c_str(), 256);
   }
-  ss << s.container_id() << " " << s.name() << " " << args << " " << s.exec_file_path();
+  ss << " " << s.exec_file_path();
   return ss.str();
 }
 
