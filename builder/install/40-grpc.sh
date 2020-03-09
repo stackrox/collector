31bf5dfda239d48e475b7d9d71f82a63a4218067
Joseph <joseph@stackrox.com>
2020-03-09 16:48:18 -0700

    Roll back grpc version and disable the warnings it generates

--- builder/install/40-grpc.sh
+++ builder/install/40-grpc.sh
@@ -5,5 +5,5 @@ set -e
 git clone -b "$GRPC_REVISION" --depth 1 https://github.com/grpc/grpc
 cd grpc
 git submodule update --init
-make -j "${NPROCS:-2}" grpc_cpp_plugin static_cxx static_c
+make -j "${NPROCS:-2}" CXXFLAGS="-Wno-error=class-memaccess -Wno-ignored-qualifiers -Wno-stringop-truncation -Wno-cast-function-type" grpc_cpp_plugin static_cxx static_c
 make prefix=/usr/local install
