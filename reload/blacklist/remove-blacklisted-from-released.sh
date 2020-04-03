fc040349a882a221772df0d6226c89da76b7eb97
Robby Cochran <rc@stackrox.com>
2020-04-03 21:54:01 +0000

    updates

--- reload/blacklist/remove-blacklisted-from-released.sh
+++ reload/blacklist/remove-blacklisted-from-released.sh
@@ -6,26 +6,37 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     [[ -n "$line" ]] || continue
     collector_version="$line"
 
-    #collector_repo="collector.stackrox.io"
-    collector_repo="docker.io/stackrox"
-    collector_image="${collector_repo}/collector:${collector_version}"
-    collector_image_rebuilt="${collector_repo}/collector:${collector_version}-rebuilt"
+    collector_repo="collector.stackrox.io/collector"
+    #collector_repo="docker.io/stackrox/collector"
+    collector_image="${collector_repo}:${collector_version}-latest"
+    collector_image_rebuilt="${collector_repo}:${collector_version}-rebuilt"
     docker pull "${collector_image}"
     docker build \
         -t "${collector_image_rebuilt}" \
-        --build-arg collector_version="${collector_version}" "${DIR}"
+        --build-arg collector_version="${collector_version}" \
+        --build-arg collector_repo="${collector_repo}" \
+        "${DIR}"
+    
     # sanity check
+    echo "Sanity check 4.19 eBPF in latest"
+    docker run -i --rm --entrypoint /bin/sh "${collector_image}" <<EOF
+ls kernel-modules/collector-ebpf-4.19.*.o.gz
+EOF
+
+    # sanity check
+    echo "Sanity check 4.19 eBPF in rebuilt"
     docker run -i --rm --entrypoint /bin/sh "${collector_image_rebuilt}" <<EOF
-set -e
-cat /kernel-modules/MODULE_VERSION.txt
-ls kernel-modules/collector-ebpf-4.19.*-coreos*.o.gz
-ls kernel-modules/collector-ebpf-4.19.*-cos.o.gz
+ls kernel-modules/collector-ebpf-4.19.*.o.gz
 EOF
 
     docker images | grep "${collector_version}"
 
+    echo "Tagging ${collector_image_rebuilt} -> ${collector_image}"
     docker tag "${collector_image_rebuilt}" "${collector_image}"
 
-    #docker push "${collector_image}"
+    docker images | grep "${collector_version}"
+
+    echo "Pushing rebuilt ${collector_image}"
+    docker push "${collector_image}"
 
 done < <(grep -v '^#' <"${DIR}/../../RELEASED_VERSIONS" | awk '{print $1}' | sort -r | uniq)
