5f56148b371cd170eea9ba471e5b347db4241592
Malte Isberner <mi@stackrox.com>
2020-05-07 12:48:07 +0200

    rhel build wtf

--- builder/install/40-grpc.sh
+++ builder/install/40-grpc.sh
@@ -3,7 +3,7 @@
 set -e
 
 CXXFLAGS=""
-if [ -f /etc/redhat-release ]; then
+if [ -f /etc/redhat-release ] && false; then
 	CXXFLAGS="-Wno-error=class-memaccess -Wno-ignored-qualifiers -Wno-stringop-truncation -Wno-cast-function-type"
 fi
 
