b98c082466e2ae39c326cd483cf96dfeb72a6258
Malte Isberner <mi@stackrox.com>
2020-05-28 01:21:21 +0200

    debugggg

--- builder/build/build-sysdig.sh
+++ builder/build/build-sysdig.sh
@@ -4,7 +4,9 @@ set -e
 set -ux
 mkdir -p /sysdig-build
 cd /sysdig-build
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DRIVER=OFF \
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_DRIVER=OFF \
+    -DCMAKE_C_FLAGS="-g -ggdb -O0 -fno-inline" \
+    -DCMAKE_CXX_FLAGS="-g -ggdb -O0 -fno-inline" \
     -DUSE_BUNDLED_DEPS=OFF \
     -DNO_LINK_GRPC_LIBS=ON \
     /sysdig-src
