f28c9f56c329bac47f233bc6686606eb821a583c
Malte Isberner <mi@stackrox.com>
2020-06-22 23:49:45 +0200

    create a debug build that can be used for profiling

--- builder/build/build-sysdig.sh
+++ builder/build/build-sysdig.sh
@@ -7,6 +7,8 @@ cd /sysdig-build
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DRIVER=OFF \
     -DUSE_BUNDLED_DEPS=OFF \
     -DNO_LINK_GRPC_LIBS=ON \
+    -DCMAKE_C_FLAGS="-fno-inline -g" \
+    -DCMAKE_CXX_FLAGS="-fno-inline -g" \
     /sysdig-src
 make -j "${NPROCS:-2}"
 strip --strip-unneeded ./userspace/libsinsp/libsinsp-wrapper.so
