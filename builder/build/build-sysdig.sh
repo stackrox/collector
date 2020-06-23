1f6c26ad5d5a9852c82ec7d46bce90d2ad9a814f
Malte Isberner <mi@stackrox.com>
2020-06-23 03:16:33 +0200

    more realistic debug foo

--- builder/build/build-sysdig.sh
+++ builder/build/build-sysdig.sh
@@ -7,8 +7,8 @@ cd /sysdig-build
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DRIVER=OFF \
     -DUSE_BUNDLED_DEPS=OFF \
     -DNO_LINK_GRPC_LIBS=ON \
-    -DCMAKE_C_FLAGS="-fno-inline -g" \
-    -DCMAKE_CXX_FLAGS="-fno-inline -g" \
+    -DCMAKE_C_FLAGS="-g -fno-inline -finline-small-functions -finline-functions-called-once" \
+    -DCMAKE_CXX_FLAGS="-g -fno-inline -finline-small-functions -finline-functions-called-once" \
     /sysdig-src
 make -j "${NPROCS:-2}"
 strip --strip-unneeded ./userspace/libsinsp/libsinsp-wrapper.so
