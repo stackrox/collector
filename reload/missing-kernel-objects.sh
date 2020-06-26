5f22a9120e2ba34a18a7d6eefc410dbedfa54574
Robby Cochran <rc@stackrox.com>
2020-06-26 18:27:49 -0400

    ROX-4002 Fix syscall arguments in kernels >= 5.1 (#328)
    Add patches to fix syscall memcpy of arguments in module driver for kernel versions >= 5.1.
    Change reload-image step to check probe hash.

--- reload/missing-kernel-objects.sh
+++ reload/missing-kernel-objects.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-# This script takes a collector image and prints a list of kernel modules and ebpf probes
-# that are available in the provided path on GCP but are not contained in the image.
+# This script takes a collector image and a GCS bucket and prints
+# the probes from either that are not found in both sources and/or
+# do not have the same hash.
 
 if [[ ! $# -eq 2 ]] ; then
   echo "Usage: $0 <collector-image> gs://<collector-module-bucket-path>"
@@ -16,10 +17,13 @@ inspect_out="$(mktemp)"
 docker run -i --rm --entrypoint /bin/sh "${image}" /dev/stdin >"${inspect_out}" <<EOF
 set -e
 cat /kernel-modules/MODULE_VERSION.txt
-find /kernel-modules -name '*.gz' -type f
+find /kernel-modules -name '*.gz' -type f | xargs md5sum 2>/dev/null | \
+sed "s/\([[:alnum:]]\+\).*\(collector-.*\.gz\)/\2 \1/"
 EOF
 
-version="$(head -n 1 ${inspect_out})"
+version="$(head -n 1 "${inspect_out}")"
+
+[[ $(gsutil ls "${gcp_bucket}/${version}/*.gz" 2> /dev/null) ]] || exit
 
 {
     while IFS='' read -r line || [[ -n "$line" ]]; do
@@ -27,5 +31,8 @@ version="$(head -n 1 ${inspect_out})"
         basename "$line"
     done < <(tail -n +2 "$inspect_out")
 
-    gsutil ls "${gcp_bucket}/${version}/*.gz" | sed -e 's@^.*/@@g'
-} | sort | uniq -u
+    gsutil hash -h "${gcp_bucket}/${version}/*.gz" | \
+        paste -d " " - - - | \
+        sed "s/.*\(collector-.*.gz\).*Hash (md5)\:[[:space:]]*\([[:alnum:]]\+\)/\1 \2/"
+
+} | sort | uniq -u | awk -F' ' '{print $1}' | sort | uniq
