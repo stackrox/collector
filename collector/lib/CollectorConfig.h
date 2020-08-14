83541649044888d9c768bbbb227f2047d5386ab8
Robby Cochran <rc@stackrox.com>
2020-08-14 13:39:11 -0400

    ROX-4979 Add UID/GID syscalls (#346)
    Capture setuid, setresuid, setgid, setresgid and vfork syscalls in kernel module mode.

--- collector/lib/CollectorConfig.h
+++ collector/lib/CollectorConfig.h
@@ -40,7 +40,25 @@ class CollectorConfig {
   static constexpr bool        kTurnOffScrape = false;
   static constexpr int         kScrapeInterval = 30;
   static constexpr char        kCollectionMethod[] = "kernel-module";
-  static constexpr const char* kSyscalls[] = {"accept","connect","execve","fork","clone","close","shutdown","socket","procexit","procinfo", "chdir", "fchdir"};
+  static constexpr const char* kSyscalls[] = {
+    "accept",
+    "chdir",
+    "clone",
+    "close",
+    "connect",
+    "execve",
+    "fchdir",
+    "fork",
+    "procexit",
+    "procinfo",
+    "setresgid",
+    "setresuid",
+    "setgid",
+    "setuid",
+    "shutdown",
+    "socket",
+    "vfork",
+  };
   static constexpr char        kChisel[] = R"(
 args = {}
 function on_event()
