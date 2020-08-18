d9d41092a6e92da9ef5e31a16ee5c1c2bb46f67c
Moritz Clasmeier <moritz@stackrox.com>
2020-08-18 12:41:57 +0200

    Increase timeout for fetching kernel support

--- collector/container/scripts/bootstrap.sh
+++ collector/container/scripts/bootstrap.sh
@@ -60,7 +60,7 @@ function download_kernel_object() {
 
     local filename_gz="${OBJECT_PATH}.gz"
     local curl_opts=(
-        -sS -4 --retry 5 --retry-connrefused --retry-delay 1 --retry-max-time 10
+        -sS -4 --retry 5 --retry-connrefused --retry-delay 1 --retry-max-time 30
         -f -L -w "HTTP Status Code %{http_code}"
         -o "${filename_gz}"
     )
