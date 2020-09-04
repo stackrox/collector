523bec35dc7bb8c0d15edb038c6f64096152620e
Robby Cochran <rc@stackrox.com>
2020-09-03 23:58:43 -0400

    ssh

--- .circleci/envbuilder.sh
+++ .circleci/envbuilder.sh
@@ -65,8 +65,9 @@ installDockerOnRHELViaGCPSSH() {
   local GCP_SSH_KEY_FILE="$1"
   shift
 
-  gcloud compute ssh "$GCP_VM_NAME" --command "sudo yum install -y yum-utils device-mapper-persistent-data lvm2"
-  gcloud compute ssh "$GCP_VM_NAME" --command "sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo"
+  ssh-keygen -f "/home/circleci/.ssh/google_compute_known_hosts" -R "compute.$(gcloud compute instances describe $GCP_VM_NAME --format='get(id)')"
+  gcloud compute ssh --ssh-key-file="${GCP_SSH_KEY_FILE}" "$GCP_VM_NAME" --command "sudo yum install -y yum-utils device-mapper-persistent-data lvm2"
+  gcloud compute ssh --ssh-key-file="${GCP_SSH_KEY_FILE}" "$GCP_VM_NAME" --command "sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo"
   # using skip-broken and nobest for rhel-8 (https://linuxconfig.org/how-to-install-docker-in-rhel-8)
   local extra_param=""
   local docker_version=""
@@ -75,9 +76,9 @@ installDockerOnRHELViaGCPSSH() {
     docker_version="-3:18.09.1-3.el7"
     docker_cli_version="-1:18.09.1-3.el7"
   fi
-  gcloud compute ssh  "$GCP_VM_NAME" --command "sudo yum install $extra_params -y docker-ce${docker_version} docker-ce-cli${docker_cli_version} containerd.io"
-  gcloud compute ssh  "$GCP_VM_NAME" --command "sudo systemctl start docker"
-  gcloud compute ssh  "$GCP_VM_NAME" --command "sudo usermod -aG docker $(whoami)"
+  gcloud compute ssh --ssh-key-file="${GCP_SSH_KEY_FILE}" "$GCP_VM_NAME" --command "sudo yum install $extra_params -y docker-ce${docker_version} docker-ce-cli${docker_cli_version} containerd.io"
+  gcloud compute ssh --ssh-key-file="${GCP_SSH_KEY_FILE}" "$GCP_VM_NAME" --command "sudo systemctl start docker"
+  gcloud compute ssh --ssh-key-file="${GCP_SSH_KEY_FILE}" "$GCP_VM_NAME" --command "sudo usermod -aG docker $(whoami)"
 }
 
 gcpSSHReady() {
