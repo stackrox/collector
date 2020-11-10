a23b56d8c6b38b5ddd7fc061218ffb8794f9c2cd
Robby Cochran <rc@stackrox.com>
2020-11-10 08:26:56 -0500

    X-Smart-Squash: Squashed 49 commits:
    9d33f15b drop ebpf syscalls
    3f1f732a format
    4871dc8e dbg
    ebb2d6ab dbg
    7080f67f -F
    67a36631 grep
    718ec8c4 patch
    823da481 fix
    3b79f67a circle kernel
    e556d6df update
    b8eada7b dbg
    8e1f0547 debug
    2e511fde disable-bench
    f4fd4c1b filler
    ccc384bd update-kernels
    9cdb5cdb dbg
    89108ca0 dbg2
    8567af82 segfault
    2a83b504 segfault
    a81efdd1 update
    e11f3421 format
    95f4428b fmt
    a1261a3f test
    347be926 typo
    fb4f2b6a drop
    653df145 drop
    db531377 flush
    d8e5e121 simplify
    5e8a2cee revert sysdig changes
    787da932 sysdig changes
    c453e786 patch
    396124a9 update patch
    f3b74aff heredoc
    29716278 print
    88605063 indent
    22fc1739 debug
    56f99109 test patch with no-cache
    34d84a78 fix
    f3b9bdb7 kernels
    f48293c1 remove signals, page faults
    f7ec12ad fix generic
    e828eba1 clean up patch
    1ae4f158 fix comments
    d059b85b cleanup
    06cb924c PR feedback
    4f2a5801 rebuild all probes
    bbc7d379 Increment driver version: Apply driver changes for ebpf drop and existing patch (95eb0815c4e7b59e0e5d0e53adb1a4faa5d5d902ad4caef2a27ed57a7f6260c3.patch) to the sysdig repo
    0c577efb parallelism, fix non-existing missing modules from gcloud
    98aab491 rebase with updated patches

--- collector/lib/EventNames.cpp
+++ collector/lib/EventNames.cpp
@@ -24,6 +24,7 @@ You should have received a copy of the GNU General Public License along with thi
 #include "EventNames.h"
 
 extern const struct ppm_event_info g_event_info[];  // defined in libscap
+extern const struct syscall_evt_pair g_syscall_table[];  // defined in libscap
 
 namespace collector {
 
@@ -35,6 +36,7 @@ const EventNames& EventNames::GetInstance() {
 EventNames::EventNames() {
   for (int i = 0; i < PPM_EVENT_MAX; i++) {
     std::string name(g_event_info[i].name);
+    syscall_by_id_[i] = 0;
     names_by_id_[i] = name;
     ppm_event_type event_type(static_cast<ppm_event_type>(i));
     events_by_name_[name].push_back(event_type);
@@ -43,6 +45,19 @@ EventNames::EventNames() {
     } else {
       events_by_name_[name + "<"].push_back(event_type);
     }
+  }
+  for (int i = 0; i < SYSCALL_TABLE_SIZE; i++) {
+    ppm_event_type enter_evt = g_syscall_table[i].enter_event_type;
+    if (enter_evt < 0 || enter_evt >= syscall_by_id_.size()) {
+      throw CollectorException("Invalid syscall event id " + std::to_string(enter_evt));
+    }
+    syscall_by_id_[enter_evt] = i;
+
+    ppm_event_type exit_evt = g_syscall_table[i].exit_event_type;
+    if (exit_evt < 0 || exit_evt >= syscall_by_id_.size()) {
+      throw CollectorException("Invalid syscall event id " + std::to_string(exit_evt));
+    }
+    syscall_by_id_[exit_evt] = i;
   }
 }
 
--- collector/lib/EventNames.h
+++ collector/lib/EventNames.h
@@ -49,6 +49,7 @@ class EventNames {
     return it->second;
   }
 
+  // Return event name for given event id
   const std::string& GetEventName(uint16_t id) const {
     if (id < 0 || id >= names_by_id_.size()) {
       throw CollectorException("Invalid event id " + std::to_string(id));
@@ -56,11 +57,20 @@ class EventNames {
     return names_by_id_[id];
   }
 
+  // Return associated syscall id for given event id
+  uint16_t GetEventSyscallID(uint16_t id) const {
+    if (id < 0 || id >= syscall_by_id_.size()) {
+      throw CollectorException("Invalid event id " + std::to_string(id));
+    }
+    return syscall_by_id_[id];
+  }
+
  private:
   EventNames();
 
   std::unordered_map<std::string, EventIDVector> events_by_name_;
   std::array<std::string, PPM_EVENT_MAX> names_by_id_;
+  std::array<uint16_t, PPM_EVENT_MAX> syscall_by_id_;
 };
 
 }  // namespace collector
