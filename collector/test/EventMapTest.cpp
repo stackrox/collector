bd25135e0f1633d9da3441e3a5defdde2926f169
Robby Cochran <rc@stackrox.com>
2020-11-11 13:14:10 -0500

    unittest

---
+++ collector/test/EventMapTest.cpp
@@ -0,0 +1,24 @@
+#include <string>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "EventMap.h"
+
+TEST(EventMap, MissingElement) {
+  using namespace collector;
+  using ::testing::StartsWith;
+  enum class TestModifier : uint8_t {
+    INVALID = 0,
+    SHUTDOWN,
+  };
+  EventMap<TestModifier> modifiers = {
+          {
+                  {"shutdown<", TestModifier::SHUTDOWN},
+          },
+          TestModifier::INVALID,
+  };
+
+  EXPECT_EQ(TestModifier::SHUTDOWN, modifiers[PPME_SOCKET_SHUTDOWN_X]);
+  EXPECT_EQ(TestModifier::INVALID, modifiers[PPME_SOCKET_SHUTDOWN_E]);
+}
