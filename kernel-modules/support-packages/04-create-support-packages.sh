b725ae50c0353ce4d0233f1a31b08fc4dbcff757
Robby Cochran <rc@stackrox.com>
2020-12-03 11:36:14 -0800

    move kernel module license and add it to support packages

--- kernel-modules/support-packages/04-create-support-packages.sh
+++ kernel-modules/support-packages/04-create-support-packages.sh
@@ -7,10 +7,11 @@ die() {
     exit 1
 }
 
-MD_DIR="$1"
-OUT_DIR="$2"
+LICENSE_FILE="$1"
+MD_DIR="$2"
+OUT_DIR="$3"
 
-[[ -n "$MD_DIR" && -n "$OUT_DIR" ]] || die "Usage: $0 <metadata directory> <output directory>"
+[[ -n "$LICENSE_FILE" && -n "$MD_DIR" && -n "$OUT_DIR" ]] || die "Usage: $0 <license-file> <metadata directory> <output directory>"
 [[ -d "$MD_DIR" ]] || die "Metadata directory $MD_DIR does not exist or is not a directory."
 
 [[ -n "${COLLECTOR_MODULES_BUCKET:-}" ]] || die "Must specify a COLLECTOR_MODULES_BUCKET"
@@ -32,6 +33,8 @@ for mod_ver_dir in "${MD_DIR}/module-versions"/*; do
     mkdir -p "$package_out_dir"
     filename="support-pkg-${mod_ver::6}-$(date '+%Y%m%d%H%M%S').zip"
     latest_filename="support-pkg-${mod_ver::6}-latest.zip"
+
+    cp "${LICENSE_FILE}" "${probe_dir}"/
 
     ( cd "$package_root" ; zip -r "${package_out_dir}/${filename}" . )
     cp "${package_out_dir}/${filename}" "${package_out_dir}/${latest_filename}"
