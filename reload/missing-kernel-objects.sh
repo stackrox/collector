86cf5487033c1df7b4ce4c60a5c26771084da6d7
Robby Cochran <rc@stackrox.com>
2021-03-26 17:43:51 -0700

    dbg/skip

--- reload/missing-kernel-objects.sh
+++ reload/missing-kernel-objects.sh
@@ -5,15 +5,25 @@ set -e
 # the probes from either that are not found in both sources and/or
 # do not have the same hash.
 
-if [[ ! $# -eq 2 ]] ; then
-  echo "Usage: $0 <collector-image> gs://<collector-module-bucket-path>"
-  exit 1
-fi
+die() {
+    echo >&2 "$@"
+    exit 1
+}
 
-image="$1"
+collector_version="$1"
 gcp_bucket="$2"
+output_dir="$3"
 
-inspect_out="$(mktemp)"
+[[ -n "$collector_version" && -n "$gcp_bucket" && -n "$output_dir" ]] || \
+    die "Usage: $0 <collector-version> <collector-module-gcp-bucket> <output directory>"
+[[ -d "$output_dir" ]] || \
+    die "Output directory ${output_dir} does not exist or is not a directory"
+
+mkdir -p "${output_dir}/${collector_version}"
+
+image="stackrox/collector:${collector_version}-latest"
+
+inspect_out="${output_dir}/${collector_version}/image-probes"
 docker run -i --rm --entrypoint /bin/sh "${image}" /dev/stdin >"${inspect_out}" <<EOF
 set -e
 cat /kernel-modules/MODULE_VERSION.txt
@@ -21,9 +31,9 @@ find /kernel-modules -name '*.gz' -type f | xargs md5sum 2>/dev/null | \
 sed "s/\([[:alnum:]]\+\).*\(collector-.*\.gz\)/\2 \1/"
 EOF
 
-version="$(head -n 1 "${inspect_out}")"
+module_version="$(head -n 1 "${inspect_out}")"
 
-[[ $(gsutil ls "${gcp_bucket}/${version}/*.gz" 2> /dev/null) ]] || exit
+[[ $(gsutil ls "${gcp_bucket}/${module_version}/*.gz" 2> /dev/null) ]] || exit
 
 {
     while IFS='' read -r line || [[ -n "$line" ]]; do
@@ -31,8 +41,11 @@ version="$(head -n 1 "${inspect_out}")"
         basename "$line"
     done < <(tail -n +2 "$inspect_out")
 
-    gsutil hash -h "${gcp_bucket}/${version}/*.gz" | \
+    gsutil hash -h "${gcp_bucket}/${module_version}/*.gz" | \
+        tee "${output_dir}/${collector_version}/gsutil-output" | \
         paste -d " " - - - | \
-        sed "s/.*\(collector-.*.gz\).*Hash (md5)\:[[:space:]]*\([[:alnum:]]\+\)/\1 \2/"
+        gsed "s/.*\(collector-.*.gz\).*Hash (md5)\:[[:space:]]*\([[:alnum:]]\+\)/\1 \2/" | \
+        tee "${output_dir}/${collector_version}/bucket-probes"
 
-} | sort | uniq -u | awk -F' ' '{print $1}' | sort | uniq
+} | sort | uniq -u | awk -F' ' '{print $1}' | sort | uniq \
+    > "${output_dir}/${collector_version}/missing-probes"
