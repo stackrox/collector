14788726fad780fce485ad22ee50c3959018f7a0
gavin-stackrox <gavin@stackrox.com>
2021-06-02 18:06:03 -0700

    install scripts for cpaas

--- builder/install/40-grpc.sh
+++ builder/install/40-grpc.sh
@@ -7,10 +7,15 @@ if [ -f /etc/redhat-release ]; then
 	CXXFLAGS="-Wno-error=class-memaccess -Wno-ignored-qualifiers -Wno-stringop-truncation -Wno-cast-function-type"
 fi
 
-git clone -b "$GRPC_REVISION" --depth 1 https://github.com/grpc/grpc
-cd grpc
+if [ -n "${CPAAS_BUILD}" ]; then
+    tar xzf grpc-${GRPC_VERSION}.tar.gz
+    cd grpc-${GRPC_VERSION}
+else
+    git clone -b "$GRPC_REVISION" --depth 1 https://github.com/grpc/grpc
+    cd grpc
+    git submodule update --init
+fi
 cp NOTICE.txt "${LICENSE_DIR}/grpc-${GRPC_REVISION}"
-git submodule update --init
 mkdir -p cmake/build
 cd cmake/build
 cmake \
@@ -24,3 +29,12 @@ cmake \
 
 make -j "${NPROCS:-2}" CXXFLAGS="${CXXFLAGS}"
 make install
+
+if [ -n "${CPAAS_BUILD}" ]; then
+    cd ../../third_party/googletest
+    mkdir -p cmake/build
+    cd cmake/build
+    cmake -DCMAKE_BUILD_TYPE=Release ../..
+    make -j "${NPROCS:-2}"
+    make install
+fi
--- builder/install/50-prometheus.sh
+++ builder/install/50-prometheus.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-git clone https://github.com/jupp0r/prometheus-cpp.git -b "$PROMETHEUS_CPP_REVISION" --depth 1
-cd prometheus-cpp
-git submodule update --init
+if [ -n "${CPAAS_BUILD}" ]; then
+    tar xzf prometheus-cpp-${PROMETHEUS_CPP_REVISION}.tar.gz
+    cd prometheus-cpp-${PROMETHEUS_CPP_REVISION}
+else
+    git clone https://github.com/jupp0r/prometheus-cpp.git -b "$PROMETHEUS_CPP_REVISION" --depth 1
+    cd prometheus-cpp
+    git submodule update --init
+fi
 mkdir -p /usr/local/include
 cp -R 3rdparty/civetweb/include /usr/local/include/civetweb
 cat LICENSE 3rdparty/civetweb/LICENSE.md > "${LICENSE_DIR}/prometheus-${PROMETHEUS_CPP_REVISION}"
