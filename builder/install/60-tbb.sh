c8f4f4e60ef77a8226e9242fb682d6d045430d54
Gavin Jefferies <gavin@stackrox.com>
2021-06-10 17:50:23 +0000

    now I get it

--- builder/install/60-tbb.sh
+++ builder/install/60-tbb.sh
@@ -6,7 +6,7 @@ if [ ! -n "${CPAAS_BUILD}" ]; then
     git clone --branch "$TBB_VERSION" --depth 1 https://github.com/intel/tbb
 fi
 cd tbb
-cp LICENSE.txt "${LICENSE_DIR}/tbb-${TBB_VERSION}"
+cp LICENSE "${LICENSE_DIR}/tbb-${TBB_VERSION}"
 make -j "${NPROCS:-2}" tbb_build_dir=./build tbb_build_prefix=lib extra_inc=big_iron.inc
 mkdir -p /usr/local/include
 cp -r ./include/tbb /usr/local/include/
