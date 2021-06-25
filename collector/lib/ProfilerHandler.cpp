3f9816b902d7d69b839c87c1a307d07ace3ed64d
Robby Cochran <rc@stackrox.com>
2021-06-25 09:46:53 -0700

    fix

--- collector/lib/ProfilerHandler.cpp
+++ collector/lib/ProfilerHandler.cpp
@@ -13,8 +13,7 @@
 
 namespace collector {
 
-StringEnvVar cpu_profile_filename("ROX_COLLECTOR_PROFILE_CPU_PATH", "/module/cpu_profile");
-
+const std::string ProfilerHandler::kCPUProfileFilename = "/module/cpu_profile";
 const std::string ProfilerHandler::kBaseRoute = "/profile";
 const std::string ProfilerHandler::kCPURoute = kBaseRoute + "/cpu";
 const std::string ProfilerHandler::kHeapRoute = kBaseRoute + "/heap";
