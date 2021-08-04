8da01e442ca9751f09a59a699bbc930ed6e63247
JoukoVirtanen <jouko@stackrox.com>
2021-08-03 21:10:50 -0700

    Still trying to compile sysdig in Debug mode

--- builder/build/build-collector.sh
+++ builder/build/build-collector.sh
@@ -6,7 +6,7 @@ ldconfig -v
 cd /tmp/cmake-build
 cmake -DCMAKE_BUILD_TYPE=Debug /src
 make -j "${NPROCS:-2}" all
-strip --strip-unneeded \
-    ./collector \
-    ./EXCLUDE_FROM_DEFAULT_BUILD/userspace/libsinsp/libsinsp-wrapper.so
+#strip --strip-unneeded \
+#    ./collector \
+#    ./EXCLUDE_FROM_DEFAULT_BUILD/userspace/libsinsp/libsinsp-wrapper.so
 cp -r /THIRD_PARTY_NOTICES .
