00c32c1c128d8113fd71f5b3988f5057f58635d4
Robby Cochran <rc@stackrox.com>
2021-08-10 12:17:33 -0700

    Use circle tag env var instead of git describe (#447)
    (cherry picked from commit 697796db3c86b0de05f120f45ea434655dd74c86)

--- Makefile-constants.mk
+++ Makefile-constants.mk
@@ -4,5 +4,9 @@ COLLECTOR_BUILDER_TAG=cache
 endif
 
 ifeq ($(COLLECTOR_TAG),)
+ifeq ($(CIRCLE_TAG),)
 COLLECTOR_TAG=$(shell git describe --tags --abbrev=10 --dirty)
+else
+COLLECTOR_TAG := $(CIRCLE_TAG)
+endif
 endif
