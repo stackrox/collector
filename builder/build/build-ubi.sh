77ed9b07b7e5c42f1fc11ca7929f1e6f0b6162e1
JoukoVirtanen <jouko@stackrox.com>
2021-08-11 12:10:25 -0700

    Fixed the way that builder/build/build-ubi.sh and collector/container/Dockerfile.ubi use CMAKE_BUILD_TYPE

--- builder/build/build-ubi.sh
+++ builder/build/build-ubi.sh
@@ -33,12 +33,13 @@ cd ..
 ### Build
 
 export DISABLE_PROFILING="true"
+CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}"
 
 cp -a collector/generated src/generated
 echo '/usr/local/lib' > /etc/ld.so.conf.d/usrlocallib.conf && ldconfig
 mkdir -p cmake-collector
 cd cmake-collector
-cmake -DCMAKE_BUILD_TYPE=Debug ../src
+cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE ../src
 make -j "${NPROCS:-2}" all
 ./runUnitTests
 strip --strip-unneeded \
@@ -50,7 +51,6 @@ cd ..
 
 mkdir -p cmake-sysdig
 cd cmake-sysdig
-CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}"
 cmake \
         -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
         -DCMAKE_C_FLAGS="-fno-pie" \
