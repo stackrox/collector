a3261864d643f6e7dc8b2dedb6ad00a41abb4b94
Robby Cochran <rc@stackrox.com>
2021-08-10 20:33:23 -0700

    X-Smart-Squash: Squashed 17 commits:
    aa18bf27 parallel image reload and include collector-rhel images
    118ea274 syntax
    c8fca287 debug
    dfa796fb mkdir
    e4d44153 path
    25894abb fix
    1f697e54 Fix
    0469f003 check empty
    4a17cc24 repo + tmp test
    0a362ea7 rm test
    01b8ec08 check if -rhel image exists
    cb2f4711 path
    e950f2b5 dbg
    2e1ac67d quote
    98e70e3c use correct repo
    d7e41ff2 esc
    ddf36c4f fix

--- reload/missing-kernel-objects.sh
+++ reload/missing-kernel-objects.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# This script takes a collector image and a GCS bucket and prints
+# This script takes a collector repo, collector version, and a GCS bucket and prints
 # the probes from either that are not found in both sources and/or
 # do not have the same hash.
 
@@ -10,20 +10,22 @@ die() {
     exit 1
 }
 
-collector_version="$1"
-gcp_bucket="$2"
-output_dir="$3"
+image_repo="$1"
+collector_version="$2"
+gcp_bucket="$3"
+output_dir="$4"
 
-[[ -n "$collector_version" && -n "$gcp_bucket" && -n "$output_dir" ]] || \
-    die "Usage: $0 <collector-version> <collector-module-gcp-bucket> <output directory>"
+[[ -n "$image_repo" && -n "$collector_version" && -n "$gcp_bucket" && -n "$output_dir" ]] || \
+    die "Usage: $0 <image-repo> <collector-version> <collector-module-gcp-bucket> <output directory>"
 [[ -d "$output_dir" ]] || \
     die "Output directory ${output_dir} does not exist or is not a directory"
 
-mkdir -p "${output_dir}/${collector_version}"
+result_dir="${output_dir}/$(basename "${image_repo}"):${collector_version}"
+mkdir -p "${result_dir}"
 
-image="stackrox/collector:${collector_version}-latest"
+image="${image_repo}:${collector_version}-latest"
 
-inspect_out="${output_dir}/${collector_version}/image-probes"
+inspect_out="${result_dir}/image-probes"
 docker run -i --rm --entrypoint /bin/sh "${image}" /dev/stdin >"${inspect_out}" <<EOF
 set -e
 cat /kernel-modules/MODULE_VERSION.txt
@@ -32,7 +34,7 @@ sed "s/\([[:alnum:]]\+\).*\(collector-.*\.gz\)/\2 \1/"
 EOF
 
 module_version="$(head -n 1 "${inspect_out}")"
-echo "${module_version}" > "${output_dir}/${collector_version}/module-version"
+echo "${module_version}" > "${result_dir}/module-version"
 
 [[ $(gsutil ls "${gcp_bucket}/${module_version}/*.gz" 2> /dev/null) ]] || exit
 
@@ -43,10 +45,10 @@ echo "${module_version}" > "${output_dir}/${collector_version}/module-version"
     done < <(tail -n +2 "$inspect_out")
 
     gsutil hash -h -m "${gcp_bucket}/${module_version}/*.gz" | \
-        tee "${output_dir}/${collector_version}/gsutil-output" | \
+        tee "${result_dir}/gsutil-output" | \
         paste -d " " - - - | \
         sed "s/.*\(collector-.*.gz\).*Hash (md5)\:[[:space:]]*\([[:alnum:]]\+\).*/\1 \2/" | \
-        tee "${output_dir}/${collector_version}/gcp-bucket-probes"
+        tee "${result_dir}/gcp-bucket-probes"
 
 } | sort | uniq -u | awk -F' ' '{print $1}' | sort | uniq \
-    > "${output_dir}/${collector_version}/missing-probes"
+    > "${result_dir}/missing-probes"
