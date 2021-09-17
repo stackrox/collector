b0d579b6b0d47ad8bf9946a6e113f00577a76b63
JoukoVirtanen <jouko@stackrox.com>
2021-09-16 19:34:22 -0700

    Attempt to fix issues with check-file-for-valgrind-errors.sh

--- .circleci/check-file-for-valgrind-errors.sh
+++ .circleci/check-file-for-valgrind-errors.sh
@@ -1,12 +1,13 @@
-VALGRIND_UNIT_TESTS=$1
+log_file=$1
+use_valgrind=$2
 
-if [[ "$VALGRIND_UNIT_TESTS" == "true" ]]; then
-  valgrind_summary_lines="$({ grep 'ERROR SUMMARY' "$UNITTEST_OUTPUT_FILE" || true ; } | wc -l)"
+if [[ "$use_valgrind" == "true" ]]; then
+  valgrind_summary_lines="$({ grep 'ERROR SUMMARY' "$log_file" || true ; } | wc -l)"
   if (( valgrind_summary_lines == 0 )); then
     echo "Error: Valgrind summary not found"
     exit 2
   fi
-  valgrind_errors="$(grep 'ERROR SUMMARY' "$UNITTEST_OUTPUT_FILE" | awk '{print $7}')"
+  valgrind_errors="$(grep 'ERROR SUMMARY' "$log_file" | awk '{print $7}')"
   if (( valgrind_errors > 0 ))
   then
     echo "Found $valgrind_errors Valgrind errors"
