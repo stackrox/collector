f36d811b02a2abc0abd175f1d9028266e9dbc082
JoukoVirtanen <jouko@stackrox.com>
2021-10-23 19:02:56 -0700

    Fixed check valgrind errors

--- .circleci/check-file-for-valgrind-errors.sh
+++ .circleci/check-file-for-valgrind-errors.sh
@@ -6,7 +6,7 @@ use_valgrind=$2
 if [[ "$use_valgrind" == "true" ]]; then
   valgrind_summary_lines="$({ grep 'ERROR SUMMARY' "$log_file" || true ; } | wc -l)"
   valgrind_invalid_errors="$({ grep '== Invalid' "$log_file" || true ; } | wc -l)"
-  if (( $valgrind_invalid_error > 0)); then
+  if (( $valgrind_invalid_errors > 0)); then
     echo "Found invalid valgrind error"
     exit 2
   fi
