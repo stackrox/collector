785c0ae959b6cac82f15fa2074dbe9ee669f4b32
JoukoVirtanen <jouko@stackrox.com>
2021-10-25 15:27:21 -0700

    Valgrind only fails if there is an invalid read or write

--- .circleci/check-file-for-valgrind-errors.sh
+++ .circleci/check-file-for-valgrind-errors.sh
@@ -5,15 +5,10 @@ use_valgrind=$2
 
 if [[ "$use_valgrind" == "true" ]]; then
   valgrind_summary_lines="$({ grep 'ERROR SUMMARY' "$log_file" || true ; } | wc -l)"
-  if (( valgrind_summary_lines == 0 )); then
-    echo "Error: Valgrind summary not found"
+  valgrind_invalid_errors="$({ grep '== Invalid' "$log_file" || true ; } | wc -l)"
+  if (( $valgrind_invalid_errors > 0)); then
+    echo "Found invalid valgrind error"
     exit 2
-  fi
-  valgrind_errors="$(grep 'ERROR SUMMARY' "$log_file" | awk '{print $7}')"
-  if (( valgrind_errors > 0 ))
-  then
-    echo "Found $valgrind_errors Valgrind errors"
-    exit 3
   fi
 fi
 
