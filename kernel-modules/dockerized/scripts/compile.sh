c4a45885d89a117d55cbad60433d236ac2577f99
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2021-10-26 14:23:06 +0200

    Squashed changes to dockerized tests
    Attempt to run tests with dockerized driver
    
    Build drivers locally and export to test VM
    
    Attempt to build test image on remote machine
    
    Also add ubuntu variations to the tests
    
    Minor improvements
    
    Upload collector repo to remote VM as a tarball.
    Add RH credentials to dockerized tests.
    
    Add more dockerized integration tests
    
    Remove unneeded sudo commands
    
    Remove trailing '+' from cos kernels
    
    Remove tests requiring newer GLIBC
    
    Force builder to run
    
    Blank
    
    Ensure copied script has execute permission
    
    Run failling command directly
    
    Remove unused script
    
    Fix flatcar drivers compilation
    
    Prevent bpf from compiling on unsupported kernels
    
    Fix dockerized integration tests
    
    Catch compilation errors
    
    Reenable ubuntu tests
    
    Free disk space on remote machine when done
    
    Fix wrong option
    
    Fix fedora CoreOS login on tests
    
    Blank
    
    Fedora CoreOS requires sudo for registration
    
    Pull ubi image before building
    
    Fail tests when module is not compiled
    
    Remove local
    
    Fix flatcar module IT
    
    Make test pattern more strict
    
    Use older GCC for v3 kernels
    
    Use regular docker build with dockerized drivers
    
    This change has been made due to yum not working correctly when run under the newer buildkit.
    
    Attempt to attach ubi7 subs
    
    Remove tests failing due to GLIBC version

--- kernel-modules/dockerized/scripts/compile.sh
+++ kernel-modules/dockerized/scripts/compile.sh
@@ -37,28 +37,90 @@ compile() (
 )
 
 compile_kmod() (
+	local collector_src="$1"
+	local kernel_src_dir="$2"
+	local kernel_build_dir="$3"
+	local kversion="$4"
+
+	# Attempt to run modpost
+	if ! "${kernel_src_dir}/scripts/mod/modpost"; then
+		echo "Failed to run kbuild tools, skipping module for ${kversion}"
+		return 0
+	fi
+
 	compile "$@" "mod"
 )
 
 compile_bpf() (
+	local collector_src="$1"
+	local kernel_src_dir="$2"
+	local kernel_build_dir="$3"
+	local kversion="$4"
+
+	# Check eBPF is supported by the kernel version
+	local bundle_uname bundle_version bundle_major bundle_distro
+	bundle_uname="$(cat "${kernel_build_dir}/BUNDLE_UNAME")"
+	bundle_version="$(cat "${kernel_build_dir}/BUNDLE_VERSION")"
+	bundle_major="$(cat "${kernel_build_dir}/BUNDLE_MAJOR")"
+	bundle_distro="$(cat "${kernel_build_dir}/BUNDLE_DISTRO")"
+
+	local rhel7_kernel_with_ebpf=false
+	if (( bundle_version == 3 && bundle_major >= 10 )); then
+		if [[ "$bundle_distro" == "redhat" ]]; then
+			rhel_build_id="$(echo "$bundle_uname" | awk -F'[-.]' '{ print $4 }')"
+			if (( rhel_build_id >= 957 )); then
+				echo "Kernel ${bundle_uname} has backported eBPF support"
+				rhel7_kernel_with_ebpf=true
+			fi
+		fi
+	fi
+
+	if [[ ! -d "${collector_src}/bpf" ]]; then
+		echo "Module version does not support eBPF probe building, skipping ..."
+		return 0
+	fi
+
+	[[ -n "$bundle_version" && -n "$bundle_major" ]] || {
+		echo >&2 "Bundle does not contain major/minor version information!"
+		return 1
+	}
+
+	# Check if this module version supports RHEL 7.6 with backported eBPF support
+	if [[ "$rhel7_kernel_with_ebpf" == true ]]; then
+		if ! grep -qRIs "SUPPORTS_RHEL76_EBPF" "${collector_src}/bpf/quirks.h"; then
+			echo "Module version ${collector_src#"/kobuild-tmp/versions-src/"} does not support eBPF on RHEL 7"
+			return 0
+		fi
+
+	# Check kernel version is at least 4.14 (unless RHEL 7.6 kernel detected)
+	elif (( bundle_version < 4 || (bundle_version == 4 && bundle_major < 14) )); then
+		echo "Kernel version ${kversion} does not support eBPF probe building, skipping ..."
+		return 0
+	fi
+
 	compile "$@" "bpf"
 )
 
+shopt -s nullglob
 for file in /bundles/bundle-*.tgz; do
 	# Get the output directory and kernel version
-	KERNEL_SRC="${file%".tgz"}"
-	KERNEL_VERSION="${KERNEL_SRC#"/bundles/bundle-"}"
+	KERNEL_BUILD="${file%".tgz"}"
+	KERNEL_VERSION="${KERNEL_BUILD#"/bundles/bundle-"}"
 
 	# Extract a kernel source bundle
-	mkdir -p "$KERNEL_SRC"
-	tar -xzf "$file" -C "$KERNEL_SRC"
+	mkdir -p "$KERNEL_BUILD"
+	tar -xzf "$file" -C "$KERNEL_BUILD"
 
-	KERNEL_BUILD="${KERNEL_SRC}/$(cat "${KERNEL_SRC}/BUNDLE_BUILD_DIR")"
+	KERNEL_SRC="${KERNEL_BUILD}/$(cat "${KERNEL_BUILD}/BUNDLE_BUILD_DIR")"
 
 	for module in /kobuild-tmp/versions-src/*; do
 		# TODO: apply blocklist to tasks
 
-		compile_kmod "$module" "$KERNEL_SRC" "$KERNEL_BUILD" "$KERNEL_VERSION"
-		compile_bpf "$module" "$KERNEL_SRC" "$KERNEL_BUILD" "$KERNEL_VERSION"
+		if ! compile_kmod "$module" "$KERNEL_SRC" "$KERNEL_BUILD" "$KERNEL_VERSION"; then
+			echo "Failed to build kernel driver for ${KERNEL_VERSION}"
+		fi
+		if ! compile_bpf "$module" "$KERNEL_SRC" "$KERNEL_BUILD" "$KERNEL_VERSION"; then
+			echo "Failed to build eBPF probe for ${KERNEL_VERSION}"
+		fi
 	done
 done
