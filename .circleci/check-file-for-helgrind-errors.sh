44edeed41e58ff891f670de1487400bc3edaa5cf
JoukoVirtanen <jouko@stackrox.com>
2021-11-08 16:37:51 -0800

    Removed data race condition check from check-file-for-valgrind-errors.sh

--- .circleci/check-file-for-helgrind-errors.sh
+++ .circleci/check-file-for-helgrind-errors.sh
@@ -5,7 +5,7 @@ log_file=$1
 use_helgrind=$2
 
 if [[ "$use_helgrind" == "true" ]]; then
-  data_race_lines="$({ grep ^==[0-9].*data "$log_file" || true | grep race || true; } | wc -l)"
+  data_race_lines="$({ grep ^==[0-9].*data\ race "$log_file" || true ; } | wc -l)"
   if (( data_race_lines > 0 ))
   then
     echo "Found $data_race_lines possible data races"
--- .circleci/check-file-for-valgrind-errors.sh
+++ .circleci/check-file-for-valgrind-errors.sh
@@ -4,12 +4,6 @@ log_file=$1
 use_valgrind=$2
 
 if [[ "$use_valgrind" == "true" ]]; then
-  data_race_lines="$({ grep ^==[0-9]+==.*data race.* "$log_file" || true ; } | wc -l)"
-  if (( data_race_lines > 0 ))
-  then
-    echo "Found $data_race_lines possible data races"
-    exit 4
-  fi
   valgrind_summary_lines="$({ grep 'ERROR SUMMARY' "$log_file" || true ; } | wc -l)"
   if (( valgrind_summary_lines == 0 )); then
     echo "Error: Valgrind summary not found"
