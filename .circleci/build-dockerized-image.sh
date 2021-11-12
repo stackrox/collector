a7d16907e5fb6f441a685f2084d165f60ef7cb2c
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2021-11-12 16:20:24 +0100

    Rollback some changes

--- .circleci/build-dockerized-image.sh
+++ .circleci/build-dockerized-image.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+set -eo pipefail
+
 GCP_VM_USER="$(whoami)"
 if [[ "$VM_TYPE" =~ "coreos" ]]; then
 	GCP_VM_USER="core"
@@ -25,27 +27,3 @@ docker build \
 	--tag kernel-builder \
 	-f "/home/${GCP_VM_USER}/ctx/collector/kernel-modules/dockerized/Dockerfile" \
 	"/home/${GCP_VM_USER}/ctx"
-
-# Check the required module has been built
-if [[ "${COLLECTION_METHOD}" == "module" ]]; then
-	test_pattern="${KERNEL_VERSION}"
-	if [[ "<< parameters.image_family >>" == "ubuntu-2004-lts" ]]; then
-		test_pattern="${KERNEL_VERSION}~20\.04"
-	elif [[ "<< parameters.image_family >>" == "ubuntu-1604-lts" ]]; then
-		test_pattern="${KERNEL_VERSION}~16\.04"
-	fi
-
-	echo "${test_pattern}"
-	if ! gcloud compute ssh --ssh-key-file="${GCP_SSH_KEY_FILE}" "${GCP_VM_USER}@${GCLOUD_INSTANCE}" --command "docker run --rm kernel-builder 'find /kernel-modules -name \"*\.ko\.gz\"'" | grep -q "${test_pattern}\.ko\.gz"; then
-		echo >&2 "Required kernel module for ${KERNEL_VERSION} has not been built"
-		exit 1
-	fi
-fi
-
-# Build and tag the test image
-docker build \
-	--tag "${COLLECTOR_REPO}:${CIRCLE_BUILD_NUM}" \
-	--build-arg COLLECTOR_TAG="${COLLECTOR_TAG}-slim" \
-	/home/${GCP_VM_USER}/ctx/collector/kernel-modules/dockerized/tests
-
-docker push ${COLLECTOR_REPO}:${CIRCLE_BUILD_NUM}"
