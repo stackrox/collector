3563dce7dd640fb683070ca528a197e80a326759
Piotr Rygielski <prygiels@redhat.com>
2021-11-17 13:51:07 +0100

    Move the unified uninstall command to final-step.sh

--- collector/container/rhel/final-step.sh
+++ collector/container/rhel/final-step.sh
@@ -10,10 +10,9 @@ if [ "$ADDRESS_SANITIZER" == "true" ]; then
   dnf install -y libasan
 fi
 
+rpm -e --nodeps $(rpm -qa '*rpm*' '*dnf*' '*libsolv*' '*hawkey*' 'yum*')
 dnf clean all
 rm -rf /var/cache/dnf
-# (Optional) Remove line below to keep package management utilities
-rpm -e --nodeps rpm rpm-build-libs rpm-libs python3-rpm subscription-manager python3-subscription-manager-rhsm yum $(rpm -qa *dnf*) python3-hawkey
 
 if [ "$USE_VALGRIND" == "true" ]; then
   ln -s /valgrind-3.17.0/install/bin/valgrind /usr/local/bin/valgrind
