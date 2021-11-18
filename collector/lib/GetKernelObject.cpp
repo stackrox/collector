185d8142ae14a93133b7d031482bd8ff37c08560
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2021-11-18 15:25:07 +0100

    Add a parameter to kernel driver downloads
    The optional parameter is mostly used when running integration tests on PRs and is meant to be used for filtering alerts when not finding the required driver.

--- collector/lib/GetKernelObject.cpp
+++ collector/lib/GetKernelObject.cpp
@@ -56,6 +56,10 @@ std::string GetModuleVersion() {
 bool DownloadKernelObjectFromURL(FileDownloader& downloader, const std::string& base_url, const std::string& kernel_module, const std::string& module_version) {
   std::string url(base_url + "/" + module_version + "/" + kernel_module + ".gz");
 
+#ifdef COLLECTOR_DEBUG_CID
+  url += "?cid=collector";
+#endif
+
   CLOG(DEBUG) << "Attempting to download kernel object from " << url;
 
   if (!downloader.SetURL(url)) return false;
