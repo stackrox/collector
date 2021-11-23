62ccf44e5e8ee648248418847f1a859d5f0a5c25
Giles Hutton <ghutton@redhat.com>
2021-11-23 13:17:41 +0000

    ROX-8508: added string view tests and improvements to splitting into more views or into strings

--- collector/lib/StringView.h
+++ collector/lib/StringView.h
@@ -29,6 +29,8 @@ You should have received a copy of the GNU General Public License along with thi
 #include <string>
 #include <vector>
 
+#include "Logging.h"
+
 namespace collector {
 class StringView {
  public:
