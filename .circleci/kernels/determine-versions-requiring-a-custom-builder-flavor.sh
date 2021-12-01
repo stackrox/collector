f03396df8b8c3a79abf35b9acf5bb6491d855a6b
Jouko Virtanen <jouko@stackrox.com>
2021-12-01 09:46:27 -0800

    Apply suggestions from code review
    Co-authored-by: Giles Hutton <ghutton@redhat.com>
    Co-authored-by: Mauro Moltrasio <mmoltras@redhat.com>

--- .circleci/kernels/determine-versions-requiring-a-custom-builder-flavor.sh
+++ .circleci/kernels/determine-versions-requiring-a-custom-builder-flavor.sh
@@ -5,12 +5,12 @@ mkdir -p ~/kobuild-tmp/custom-flavors
 make --quiet -C "${SOURCE_ROOT}/kernel-modules" print-custom-flavors >~/kobuild-tmp/custom-flavors/all
 mkdir ~/kobuild-tmp/meta-inspect
 for bundle_file in ~/kobuild-tmp/bundles/bundle-*.tgz; do
-  version="$(basename "$bundle_file" | sed -E 's/^bundle-(.*)\.tgz$/\1/')"
+  version="$(basename "${bundle_file}" | sed -E 's/^bundle-(.*)\.tgz$/\1/')"
   tar -xzf "${bundle_file}" -C ~/kobuild-tmp/meta-inspect ./BUNDLE_DISTRO ./BUNDLE_VERSION ./BUNDLE_MAJOR
   distro="$(< ~/kobuild-tmp/meta-inspect/BUNDLE_DISTRO)"
   kernel_version="$(< ~/kobuild-tmp/meta-inspect/BUNDLE_VERSION)"
   kernel_major="$(< ~/kobuild-tmp/meta-inspect/BUNDLE_MAJOR)"
-  flavor="$("${SOURCE_ROOT}/.circleci/kernels/get-builder-flavor.sh" $version $distro $kernel_version $kernel_major)"
+  flavor="$("${SOURCE_ROOT}/.circleci/kernels/get-builder-flavor.sh" "${version}" "${distro}" "${kernel_version}" "${kernel_major}")"
   if [[ "$flavor" != "default" ]]; then
     echo "$version" >>~/kobuild-tmp/custom-flavors/versions."$flavor"
   fi
