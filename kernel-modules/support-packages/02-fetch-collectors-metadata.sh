e9a4f277c204fc6f55a84c03588cb4f1b6f01bd8
Robby Cochran <rc@stackrox.com>
2021-12-06 21:08:45 -0800

    remote docker version

--- kernel-modules/support-packages/02-fetch-collectors-metadata.sh
+++ kernel-modules/support-packages/02-fetch-collectors-metadata.sh
@@ -16,17 +16,15 @@ for version_dir in "${MD_DIR}/collector-versions"/*; do
     [[ -d "$version_dir" ]] || continue
     version="$(basename "$version_dir")"
 
-    collector_image="collector.stackrox.io/collector:${version}-latest"
+    collector_image="collector.stackrox.io/collector:${version}"
     docker pull "$collector_image"
     tmp_output="$(mktemp)"
 
-    echo "collector_image=${collector_image}"
     docker run --rm --entrypoint /bin/bash "$collector_image" -c '
         cd /kernel-modules &&
         head -n 1 MODULE_VERSION.txt &&
         ls *.gz
     ' >"$tmp_output"
-    docker image rm --force "$collector_image"
 
     head -n 1 "$tmp_output" >"${version_dir}/MODULE_VERSION"
     tail -n +2 "$tmp_output" >"${version_dir}/INVENTORY"
