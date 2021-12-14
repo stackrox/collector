d22d6c00457174ff756f32cbd68a1066088d52ce
JoukoVirtanen <jouko@stackrox.com>
2021-12-14 11:51:16 -0800

    Moved around some things

--- .circleci/upload-modules/create-docker-context.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-mkdir -p ~/workspace/go/src/github.com/stackrox/bundles
-
-## Get the bundle for the running machine
-gsutil -m cp "gs://stackrox-kernel-bundles/bundle-$(uname -r).tgz" ~/workspace/go/src/github.com/stackrox/bundles
