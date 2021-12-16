7e03ee24006fe02c5d091d524c8c5da47220479a
JoukoVirtanen <jouko@stackrox.com>
2021-12-15 18:37:52 -0800

    Some improvements to ShellCheckAll.sh

--- TestScripts/ShellCheckAll.sh
+++ TestScripts/ShellCheckAll.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-for file in $(find ../ -type f -name \*.sh)
+#shellcheck checks for potential errors and smells in shell scripts.
+#This script runs shellcheck on all shell scripts in Collector
+
+if ! command -v shellcheck > /dev/null; then
+  "shellcheck not installed. Please install shellcheck to run this script."
+  exit 0
+fi
+
+SCRIPT_DIR=$(dirname "$0")
+for file in $(find $SCRIPT_DIR/.. -type f -name \*.sh)
 do
   shellcheck $file
 done
