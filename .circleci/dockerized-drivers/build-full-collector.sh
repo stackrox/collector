ed9b1d2871323571ddfb7bd34a3feaba794fd772
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2021-12-21 17:44:21 +0100

    Copy only the required drivers onto collector

--- .circleci/dockerized-drivers/build-full-collector.sh
+++ .circleci/dockerized-drivers/build-full-collector.sh
@@ -3,6 +3,13 @@ set -euo pipefail
 
 COLLECTOR_REPO="$1"
 
+# Get the module version from the collector image and pass it as a build-arg
+docker create --name collector "${COLLECTOR_REPO}/collector:${COLLECTOR_TAG}-slim"
+docker cp collector:/kernel-modules/MODULE_VERSION.txt ./MODULE_VERSION.txt
+module_version="$(cat ./MODULE_VERSION.txt)"
+rm -f ./MODULE_VERSION.txt
+docker rm collector
+
 # Copy all drivers into the new image
 DOCKER_BUILDKIT=1 \
 BUILDKIT_PROGRESS=plain \
@@ -10,6 +17,7 @@ docker build \
 	--tag "${COLLECTOR_REPO}/collector:${COLLECTOR_TAG}-dockerized" \
 	--build-arg COLLECTOR_TAG="${COLLECTOR_TAG}-slim" \
 	--build-arg DRIVERS_TAG="${COLLECTOR_DRIVERS_TAG}" \
+	--build-arg MODULE_VERSION="${module_version}" \
 	-f "${SOURCE_ROOT}/kernel-modules/dockerized/Dockerfile.collector" \
 	"${SOURCE_ROOT}/kernel-modules/dockerized"
 
