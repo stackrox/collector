6529437afde911354050946f1ad75556be890e60
Giles Hutton <ghutton@redhat.com>
2022-01-07 14:46:38 +0000

    Adds GardenLinux integration test (#530)

--- .circleci/envbuilder.sh
+++ .circleci/envbuilder.sh
@@ -42,6 +42,50 @@ createGCPVM() {
   return 0
 }
 
+createGCPVMFromImage() {
+  local GCP_VM_NAME="$1"
+  shift
+  local GCP_IMAGE_NAME="$1"
+  shift
+  local GCP_IMAGE_PROJECT="$1"
+  shift
+
+  [ -z "$GCP_VM_NAME" ] && echo "error: missing parameter GCP_VM_NAME" && return 1
+  [ -z "$GCP_IMAGE_NAME" ] && echo "error: missing parameter GCP_IMAGE_NAME" && return 1
+  [ -z "$GCP_IMAGE_PROJECT" ] && echo "error: missing parameter GCP_IMAGE_PROJECT" && return 1
+
+  success=false
+  for zone in us-central1-a us-central1-b ; do
+      echo "Trying zone $zone"
+      gcloud config set compute/zone "${zone}"
+      if gcloud compute instances create \
+        --image "$GCP_IMAGE_NAME" \
+        --image-project "$GCP_IMAGE_PROJECT" \
+        --service-account=circleci-collector@stackrox-ci.iam.gserviceaccount.com \
+        --machine-type e2-standard-2 \
+        --labels="stackrox-ci=true,stackrox-ci-job=${CIRCLE_JOB},stackrox-ci-workflow=${CIRCLE_WORKFLOW_ID}" \
+        --boot-disk-size=20GB \
+          "$GCP_VM_NAME"
+      then
+          success=true
+          break
+      else
+          gcloud compute instances delete "$GCP_VM_NAME"
+      fi
+  done
+
+  if test ! "$success" = "true" ; then
+    echo "Could not boot instance."
+    return 1
+  fi
+
+  gcloud compute instances add-metadata "$GCP_VM_NAME" --metadata serial-port-logging-enable=true
+  gcloud compute instances describe --format json "$GCP_VM_NAME"
+  echo "Instance created successfully: $GCP_VM_NAME"
+
+  return 0
+}
+
 installDockerOnUbuntuViaGCPSSH() {
   local GCP_VM_NAME="$1"
   shift
@@ -145,6 +189,8 @@ setupGCPVM() {
   shift
   local GCP_IMAGE_FAMILY="$1"
   shift
+  local GCP_IMAGE_NAME="$1"
+  shift
   local GCP_SSH_KEY_FILE="$1"
   shift
   local GDOCKER_USER="$1"
@@ -152,7 +198,7 @@ setupGCPVM() {
   local GDOCKER_PASS="$1"
   shift
 
-  if [[ ! "$GCP_VM_TYPE" =~ ^(coreos|cos|rhel|suse|suse-sap|ubuntu-os|flatcar|fedora-coreos)$ ]]; then
+  if [[ ! "$GCP_VM_TYPE" =~ ^(coreos|cos|rhel|suse|suse-sap|ubuntu-os|flatcar|fedora-coreos|garden-linux)$ ]]; then
     echo "Unsupported GPC_VM_TYPE: $GCP_VM_TYPE"
     exit 1
   fi
@@ -162,13 +208,19 @@ setupGCPVM() {
     GCP_VM_USER="core"
   fi
 
-  if [[ "$GCP_VM_TYPE" != "flatcar" ]]; then
-    GCP_IMAGE_PROJECT="$GCP_VM_TYPE-cloud"
-  else
+  if [[ "$GCP_VM_TYPE" == "flatcar" ]]; then
     GCP_IMAGE_PROJECT="kinvolk-public"
+  elif [[ "$GCP_VM_TYPE" == "garden-linux" ]]; then
+    GCP_IMAGE_PROJECT="sap-se-gcp-gardenlinux"
+  else
+    GCP_IMAGE_PROJECT="$GCP_VM_TYPE-cloud"
   fi
 
-  createGCPVM "$GCP_VM_NAME" "$GCP_IMAGE_FAMILY" "$GCP_IMAGE_PROJECT"
+  if [[ -n "$GCP_IMAGE_NAME" && "$GCP_IMAGE_NAME" != "unset" ]]; then
+    createGCPVMFromImage "$GCP_VM_NAME" "$GCP_IMAGE_NAME" "$GCP_IMAGE_PROJECT"
+  else
+    createGCPVM "$GCP_VM_NAME" "$GCP_IMAGE_FAMILY" "$GCP_IMAGE_PROJECT"
+  fi
 
   if ! gcpSSHReady "$GCP_VM_USER" "$GCP_VM_NAME" "$GCP_SSH_KEY_FILE"; then
     echo "GCP SSH failure"
