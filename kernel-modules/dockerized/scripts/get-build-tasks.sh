7d1ac5bae00aded825ea4942d133b07a3878b72f
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-01-13 16:20:25 +0100

    Prevent rebuilding cached drivers

--- kernel-modules/dockerized/scripts/get-build-tasks.sh
+++ kernel-modules/dockerized/scripts/get-build-tasks.sh
@@ -43,44 +43,25 @@ driver_is_cached() {
 	[ -f "/kernel-modules/${version}/${driver}" ]
 }
 
-fill_redundant_task_file() {
-	for module_dir in /kernel-modules/*/; do
-		for driver in "$module_dir"*; do
-			local kernel_version
-			kernel_version="${driver#"${module_dir}"}"
-
-			if [[ "$kernel_version" =~ ^collector-ebpf-(.*)\.o\.gz$ ]]; then
-				kernel_version="${BASH_REMATCH[1]}"
-				driver_type="bpf"
-			elif [[ "$kernel_version" =~ ^collector-ebpf-(.*)\.unavail$ ]]; then
-				kernel_version="${BASH_REMATCH[1]}"
-				driver_type="bpf"
-			elif [[ "$kernel_version" =~ ^collector-(.*)\.ko\.gz$ ]]; then
-				kernel_version="${BASH_REMATCH[1]}"
-				driver_type="mod"
-			else
-				echo >&2 "Unkown driver format: '${kernel_version}'"
-				continue
-			fi
-
-			append_task "$kernel_version" "$module_dir" "$driver_type" /redundant-build-tasks
-		done
-	done
-}
-
 shopt -s nullglob
 for module_dir in /kobuild-tmp/versions-src/*/; do
 	if [[ "${USE_KERNELS_FILE,,}" == "true" ]]; then
 		while IFS="" read -r kernel_version || [[ -n "$kernel_version" ]]; do
 			append_task "$kernel_version" "$module_dir" "mod" /all-build-tasks
 
+			# If the driver is cached, we prevent rebuilding it by adding it to '/redundant-build-tasks'
+			if driver_is_cached "$kernel_version" "$module_dir" "mod"; then
+				append_task "$kernel_version" "$module_dir" "mod" /redundant-build-tasks
+			fi
+
 			if [[ -d "${module_dir}/bpf" ]]; then
 				append_task "$kernel_version" "$module_dir" "bpf" /all-build-tasks
+
+				if driver_is_cached "$kernel_version" "$module_dir" "bpf"; then
+					append_task "$kernel_version" "$module_dir" "bpf" /redundant-build-tasks
+				fi
 			fi
 		done < /KERNEL_VERSIONS
-
-		# We now create a list of existing modules in our cache
-		fill_redundant_task_file
 	else
 		for bundle in /bundles/bundle-*.tgz; do
 			kernel_version="${bundle%".tgz"}"
