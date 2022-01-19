4b18f19caf11e57fd620e16eec08bd2a3314be1e
JoukoVirtanen <jouko@stackrox.com>
2022-01-19 10:35:29 -0800

    Fixed CreateInfra.sh so that it actually creates the cluster

--- utilities/PerformanceTesting/CreateInfra.sh
+++ utilities/PerformanceTesting/CreateInfra.sh
@@ -8,10 +8,8 @@ lifespan=$3
 does_cluster_exist() {
 	nline="$({ infractl get "$name" 2> /dev/null || true; } | wc -l)"
 	if (( "$nline" == 0 )); then
-		echo "The cluster is not running."
 		cluster_exist="false"
 	else
-		echo "The cluster is already running."
 		cluster_exist="true"
 	fi
 
@@ -19,8 +17,12 @@ does_cluster_exist() {
 }
 
 
+cluster_exist=$(does_cluster_exist)
+
 if [[ $(does_cluster_exist) == "false" ]]; then
 	echo "Creating an infra cluster with name $name, flavor $flavor"
 	infractl create "$flavor" "$name" --description "Performance testing cluster"
 	infractl lifespan "$name" "$lifespan"
+else
+	echo "Unable to create cluster"
 fi
