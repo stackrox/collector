9afb55c4ce317a1e359416a30f471ace992d2127
JoukoVirtanen <jouko@stackrox.com>
2022-01-24 10:34:06 -0800

    X-Smart-Squash: Squashed 5 commits:
    3b2d2bfb Removed shellcheck smells from builder/build/build-collector.sh and builder/build/build-ubi.sh
    3115deee Fixed all shellcheck smells except for those in bootstrap.sh and collector-wrapper.sh. Exempted one unused variable warning, which is used to make loop.
    c6c1005b Fixed shfmt error
    eda71c67 Fixed path in builder/build/build-collector.sh
    35dd2828 Added comment explaining why one shellcheck is diabled in push-as-manifest-list.sh

--- builder/install/install.sh
+++ builder/install/install.sh
@@ -6,6 +6,7 @@ export LICENSE_DIR="/THIRD_PARTY_NOTICES"
 
 mkdir -p "${LICENSE_DIR}"
 cd /install-tmp/
+# shellcheck source=SCRIPTDIR/versions.sh
 source ./versions.sh
 for f in [0-9][0-9]-*.sh; do
     ./"$f"
