86a70ff2bd3b3db46523848e3a14d22436871921
JoukoVirtanen <jouko@stackrox.com>
2022-02-09 21:00:51 -0800

    Removed some debugging

--- collector/lib/NetworkStatusNotifier.cpp
+++ collector/lib/NetworkStatusNotifier.cpp
@@ -240,19 +240,19 @@ void NetworkStatusNotifier::RunSingle(DuplexClientWriter<sensor::NetworkConnecti
     ConnMap new_conn_state;
     ContainerEndpointMap new_cep_state;
     int64_t time_micros = NowMicros();
-    CLOG(INFO) << "time_micros= " << google::protobuf::util::TimeUtil::MicrosecondsToTimestamp(time_micros);
+    //CLOG(INFO) << "time_micros= " << google::protobuf::util::TimeUtil::MicrosecondsToTimestamp(time_micros);
     WITH_TIMER(CollectorStats::net_fetch_state) {
       new_conn_state = conn_tracker_->FetchConnState(true, true);
-      CLOG(INFO) << "";
-      CLOG(INFO) << "new_conn_state";
-      for (auto conn : new_conn_state) {
-        CLOG(INFO) << google::protobuf::util::TimeUtil::MicrosecondsToTimestamp(conn.second.LastActiveTime());
-      }
-      CLOG(INFO) << "";
-      CLOG(INFO) << "old_conn_state";
-      for (auto conn : old_conn_state) {
-        CLOG(INFO) << google::protobuf::util::TimeUtil::MicrosecondsToTimestamp(conn.second.LastActiveTime());
-      }
+      //CLOG(INFO) << "";
+      //CLOG(INFO) << "new_conn_state";
+      //for (auto conn : new_conn_state) {
+      //  CLOG(INFO) << google::protobuf::util::TimeUtil::MicrosecondsToTimestamp(conn.second.LastActiveTime());
+      //}
+      //CLOG(INFO) << "";
+      //CLOG(INFO) << "old_conn_state";
+      //for (auto conn : old_conn_state) {
+      //  CLOG(INFO) << google::protobuf::util::TimeUtil::MicrosecondsToTimestamp(conn.second.LastActiveTime());
+      //}
       ConnectionTracker::ComputeDelta(new_conn_state, &old_conn_state);
 
       new_cep_state = conn_tracker_->FetchEndpointState(true, true);
@@ -293,22 +293,22 @@ void NetworkStatusNotifier::RunSingleAfterglow(DuplexClientWriter<sensor::Networ
     if (!success) continue;
 
     int64_t time_micros = NowMicros();
-    CLOG(INFO) << "time_micros= " << google::protobuf::util::TimeUtil::MicrosecondsToTimestamp(time_micros);
+    //CLOG(INFO) << "time_micros= " << google::protobuf::util::TimeUtil::MicrosecondsToTimestamp(time_micros);
     const sensor::NetworkConnectionInfoMessage* msg;
     ContainerEndpointMap new_cep_state, delta_cep;
     ConnMap new_conn_state, delta_conn;
     WITH_TIMER(CollectorStats::net_fetch_state) {
       new_conn_state = conn_tracker_->FetchConnState(true, true);
-      CLOG(INFO) << "";
-      CLOG(INFO) << "new_conn_state";
-      for (auto conn : new_conn_state) {
-        CLOG(INFO) << google::protobuf::util::TimeUtil::MicrosecondsToTimestamp(conn.second.LastActiveTime());
-      }
-      CLOG(INFO) << "";
-      CLOG(INFO) << "old_conn_state";
-      for (auto conn : old_conn_state) {
-        CLOG(INFO) << google::protobuf::util::TimeUtil::MicrosecondsToTimestamp(conn.second.LastActiveTime());
-      }
+      //CLOG(INFO) << "";
+      //CLOG(INFO) << "new_conn_state";
+      //for (auto conn : new_conn_state) {
+      //  CLOG(INFO) << google::protobuf::util::TimeUtil::MicrosecondsToTimestamp(conn.second.LastActiveTime());
+      //}
+      //CLOG(INFO) << "";
+      //CLOG(INFO) << "old_conn_state";
+      //for (auto conn : old_conn_state) {
+      //  CLOG(INFO) << google::protobuf::util::TimeUtil::MicrosecondsToTimestamp(conn.second.LastActiveTime());
+      //}
       ConnectionTracker::ComputeDeltaAfterglow(new_conn_state, old_conn_state, delta_conn, time_micros, time_at_last_scrape, afterglow_period_micros_);
 
       new_cep_state = conn_tracker_->FetchEndpointState(true, true);
