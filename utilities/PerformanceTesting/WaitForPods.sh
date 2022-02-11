fbad1da72ec9d596b9fb065c0b3f93ef8c24a36c
JoukoVirtanen <jouko@stackrox.com>
2022-02-10 16:28:13 -0800

    Improved WaitForPods.sh

--- utilities/PerformanceTesting/WaitForPods.sh
+++ utilities/PerformanceTesting/WaitForPods.sh
@@ -12,7 +12,10 @@ do
   pod_ready="$(kubectl -n stackrox get pod -o jsonpath='{.items[*].status.containerStatuses[*].ready}')"
   has_ready="$({ echo "$pod_ready" | grep true || true ; } | wc -l)"
   has_unready="$({ echo "$pod_ready" | grep false || true ; } | wc -l)"
-  if [[ (( $has_ready == 1 )) && (( $has_unready == 0 )) ]]; then
+  ready_containers="$(kubectl -n stackrox get pod -o jsonpath='{.items[*].status.containerStatuses[?(@.ready == true)]}')"
+  not_ready_containers="$(kubectl -n stackrox get pod -o jsonpath='{.items[*].status.containerStatuses[?(@.ready == false)]}')"
+  #if [[ (( $has_ready == 1 )) && (( $has_unready == 0 )) ]]; then
+  if [[ -n "$ready_containers" && -z "$not_ready_containers" ]]; then
     echo "All pods are running"
     break
   fi
