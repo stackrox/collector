c881b976a61dd0472c61ca9ad3ac9512ecbdc537
JoukoVirtanen <jouko@stackrox.com>
2022-02-18 10:18:26 -0800

    Added ability to set image main

--- utilities/PerformanceTesting/SecureCluster.sh
+++ utilities/PerformanceTesting/SecureCluster.sh
@@ -9,18 +9,30 @@ echo "Starting secure cluster services"
 
 export KUBECONFIG=$artifacts_dir/kubeconfig
 
+settings=(
+    --set collector.roxAfterglowPeriod=10
+    --set imagePullSecrets.username="$DOCKER_USERNAME"
+    --set imagePullSecrets.password="$DOCKER_PASSWORD"
+    --set clusterName=perf-test
+    --set image.collector.registry="$collector_image_registry"
+    --set image.collector.name=collector
+    --set image.collector.tag="$collector_image_tag"
+    --set enableOpenShiftMonitoring=true
+    --set exposeMonitoring=true
+)
+
+if [[ -n ${IMAGE_MAIN_REGISTRY:-} ]]; then
+    settings+=(--set image.main.registry="$IMAGE_MAIN_REGISTRY")
+fi
+
+if [[ -n ${IMAGE_MAIN_NAME:-} ]]; then
+    settings+=(--set image.main.name="$IMAGE_MAIN_NAME")
+fi
+
+if [[ -n ${IMAGE_MAIN_TAG:-} ]]; then
+    settings+=(--set image.main.tag="$IMAGE_MAIN_TAG")
+fi
+
 helm install -n stackrox stackrox-secured-cluster-services rhacs/secured-cluster-services \
-  -f perf-bundle.yml \
-  --set collector.roxAfterglowPeriod=10 \
-  --set imagePullSecrets.username="$DOCKER_USERNAME" \
-  --set imagePullSecrets.password="$DOCKER_PASSWORD" \
-  --set clusterName=perf-test \
-  --set image.collector.registry="$collector_image_registry" \
-  --set image.collector.name=collector \
-  --set image.collector.tag="$collector_image_tag" \
-  --set enableOpenShiftMonitoring=true \
-  --set exposeMonitoring=true \
-  --set image.main.registry=docker.io/stackrox \
-  --set image.main.name=main \
-  --set image.main.tag=3.68.x-198-g16f3ea3c01-dirty
-  #--set image.main.tag=3.68.x-196-gc531c2675f
+    -f perf-bundle.yml \
+    "${settings[@]}"
