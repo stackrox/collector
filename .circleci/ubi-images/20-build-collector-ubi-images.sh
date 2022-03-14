cbe06f7d014b28ead060a53d7e0f2190aff64167
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-03-14 18:11:22 +0100

    Ignore missing kernel alerts for UBI image in PRs

--- .circleci/ubi-images/20-build-collector-ubi-images.sh
+++ .circleci/ubi-images/20-build-collector-ubi-images.sh
@@ -7,6 +7,7 @@ docker build \
     --build-arg REDHAT_PASSWORD="${REDHAT_PASSWORD}" \
     --build-arg USE_VALGRIND="${BUILD_USE_VALGRIND}" \
     --build-arg ADDRESS_SANITIZER="${SANITIZER_TESTS}" \
+    --build-arg COLLECTOR_APPEND_CID="${COLLECTOR_APPEND_CID}" \
     -t "collector-test-cpaas:${COLLECTOR_VERSION}" \
     --build-arg CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
     -f "${SOURCE_ROOT}/collector/container/Dockerfile.ubi" \
