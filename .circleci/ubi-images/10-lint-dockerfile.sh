accb92cbc02a32c71d6d7db61e2cc869494b3701
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-03-23 12:26:52 +0100

    Replace pull command with --pull option

--- .circleci/ubi-images/10-lint-dockerfile.sh
+++ .circleci/ubi-images/10-lint-dockerfile.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-docker pull hadolint/hadolint
-
-docker run --rm -i hadolint/hadolint < \
+docker run --pull always --rm -i hadolint/hadolint < \
     "${SOURCE_ROOT}/collector/container/Dockerfile.ubi"
