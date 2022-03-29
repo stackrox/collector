d451e18803192a8765c9c29d66949b9a9a0e6d86
JoukoVirtanen <jouko@stackrox.com>
2022-03-29 14:53:56 -0700

    Removed call to circleci api

--- .circleci/run-stackrox-ci/10-run-stackrox-ci.sh
+++ .circleci/run-stackrox-ci/10-run-stackrox-ci.sh
@@ -23,19 +23,19 @@ git commit -m "Automatic commit ${BUILD_NUM}. Testing collector version ${COLLEC
 
 git push -f origin HEAD
 
-generate_data() {
-    cat << EOF
-    {
-         "branch": "$stackrox_branch",
-         "parameters": {
-             "trigger_on_demand": false
-         }
-    }
-EOF
-}
-
-curl -X POST --header 'Content-Type: application/json' \
-        --header "Circle-Token: $CIRCLE_TOKEN_ROXBOT" https://circleci.com/api/v2/project/github/stackrox/stackrox/pipeline -d "$(generate_data)"
+#generate_data() {
+#    cat << EOF
+#    {
+#         "branch": "$stackrox_branch",
+#         "parameters": {
+#             "trigger_on_demand": false
+#         }
+#    }
+#EOF
+#}
+#
+#curl -X POST --header 'Content-Type: application/json' \
+#        --header "Circle-Token: $CIRCLE_TOKEN_ROXBOT" https://circleci.com/api/v2/project/github/stackrox/stackrox/pipeline -d "$(generate_data)"
 
 stackrox_branch_encoded="${stackrox_branch/\//%2F}"
 pr_url="https://app.circleci.com/pipelines/github/stackrox/stackrox?branch=${stackrox_branch_encoded}&filter=all"
