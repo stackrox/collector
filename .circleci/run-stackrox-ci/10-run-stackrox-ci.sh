131f4679690c227f92ef24c1ab1b741fa632a196
JoukoVirtanen <jouko@stackrox.com>
2022-04-04 14:45:08 -0700

    X-Smart-Squash: Squashed 22 commits:
    935d03c8 Started work. Added job run-stacrox-ci, which is incomplete
    d009c471 Added script to create new branch in stackrox. Not actually called
    a368ed8e Fixed typo
    eea6d252 Should actually run the run-stackrox-ci job
    300c935c Trying to fix syntax error in config
    54f863db Still trying to fix syntax error
    95f0ec86 Trying to fix problem with permissions
    60495a65 Switched to using defaultMachine as a test
    f3e50e88 Fixed typo in script path
    8dbe3682 Only running run-stackrox-ci job if label is used. Prints link to CircleCI workflow
    154e964f Switched to using CircleCI API to trigger stackrox CircleCI run
    29510c15 Changed name of step to Run stackrox CI
    0260f99a Only use the circleci api the first time when the branch is created
    0a38daca Use git branch -a to check for branches
    1528f943 Using variable instead of literal
    83b47eb6 Addressed pr review comments
    c2450dd5 Switched back to defaultMachine for now
    f99d264c Added run-stackrox-ci/ci-url-comment-template.tpl
    237c5262 Switched back to using defaultImage
    84d05ffd Switched back to defaultMachine
    59aa6f57 Trying to fix hub-comment
    11106d74 Not creating a pr or branch. Using the api to change the collector version

---
+++ .circleci/run-stackrox-ci/10-run-stackrox-ci.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -eox pipefail
+
+comment_on_pr_with_ci_link() {
+    local ci_url=$1
+
+    wget --quiet https://github.com/joshdk/hub-comment/releases/download/0.1.0-rc6/hub-comment_linux_amd64
+    sudo install hub-comment_linux_amd64 /usr/bin/hub-comment
+
+    export CI_URL=$ci_url
+
+    hub-comment -template-file "${CI_ROOT}/run-stackrox-ci/ci-url-comment-template.tpl"
+}
+
+DATA=$(jq -cn --arg collector_version "$COLLECTOR_VERSION" '{
+    "branch": "gavin/build-all-on-demand",
+    "parameters": {
+        "trigger_on_demand": true,
+	"workflow_name": "build_all",
+	"collector_version": $collector_version
+    }
+}')
+
+endpoint=https://circleci.com/api/v2/project/github/stackrox/stackrox/pipeline
+response="$(curl -X POST --header 'Content-Type: application/json' \
+        --header "Circle-Token: $CIRCLE_TOKEN_ROXBOT" "$endpoint" -d "$DATA")"
+
+build_num="$(echo "$response" | jq '.number')"
+ci_url="https://app.circleci.com/pipelines/github/stackrox/stackrox/$build_num"
+
+echo "You can find the StackRox CircleCI results here $ci_url"
+
+comment_on_pr_with_ci_link "$ci_url"
