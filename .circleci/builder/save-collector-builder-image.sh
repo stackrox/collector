cfa95cc2273715982c7d011e7f4ee87e038250a0
Dmitrii Dolgov <9erthalion6@gmail.com>
2022-04-04 17:10:14 +0200

    Remove unnecessary stuff

--- .circleci/builder/save-collector-builder-image.sh
+++ .circleci/builder/save-collector-builder-image.sh
@@ -15,11 +15,10 @@ for repo in "${image_repos[@]}"; do
         docker login -u "$QUAY_STACKROX_IO_RW_USERNAME" -p "$QUAY_STACKROX_IO_RW_PASSWORD" quay.io
     fi
 
+    image="${repo}:${COLLECTOR_BUILDER_TAG}"
+
     if [[ "${COLLECTOR_BUILDER_TAG}" != "cache" ]]; then
         # Push cache only if creating a new builder
-        docker push "stackrox/collector-builder:${COLLECTOR_BUILDER_TAG}"
-
-        image="${repo}:${COLLECTOR_BUILDER_TAG}"
         echo "Pushing image ${image}"
         docker image inspect "${image}" > /dev/null
         "${WORKSPACE_ROOT}/go/src/github.com/stackrox/collector/scripts/push-as-manifest-list.sh" "${image}"
@@ -27,8 +26,6 @@ for repo in "${image_repos[@]}"; do
     fi
 
     if [[ "$BRANCH" == "master" ]]; then
-        image="${repo}:${COLLECTOR_BUILDER_TAG}"
-        echo "Pushing image ${image} as cache"
         docker tag "${image}" "${repo}:cache"
         docker image inspect "${repo}:cache" > /dev/null
         "${WORKSPACE_ROOT}/go/src/github.com/stackrox/collector/scripts/push-as-manifest-list.sh" "${repo}:cache"
