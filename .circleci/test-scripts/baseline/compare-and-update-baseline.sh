eaf5b75984a73d0fbba67a8d6c71467e976dff24
Dmitrii Dolgov <9erthalion6@gmail.com>
2022-04-05 14:46:44 +0200

    Uncomment updating only on the main branch

--- .circleci/test-scripts/baseline/compare-and-update-baseline.sh
+++ .circleci/test-scripts/baseline/compare-and-update-baseline.sh
@@ -10,11 +10,7 @@ export BASELINE="${CI_ROOT}/test-scripts/baseline/"
     | sort \
     | awk -f "${BASELINE}"/format.awk > "${WORKSPACE_ROOT}"/benchmark.md
 
-"${BASELINE}"/main.py --update "${WORKSPACE_ROOT}"/all-perf.json
-
-# The baseline has to be updated only in the main branch, uncomment that before
-# merging.
-#
-#if [[ "$CIRCLE_BRANCH" == "master" || -n "$CIRCLE_TAG" ]]; then
-    #${BASELINE}/main.py --update ${WORKSPACE_ROOT}/all-perf.json
-#fi
+# The baseline has to be updated only in the main branch.
+if [[ "$CIRCLE_BRANCH" == "master" || -n "$CIRCLE_TAG" ]]; then
+    "${BASELINE}"/main.py --update "${WORKSPACE_ROOT}"/all-perf.json
+fi
