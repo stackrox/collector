bb62bc4b808ff01c2e3daabf75e5c2bd1323ece5
Giles Hutton <ghutton@redhat.com>
2022-04-05 11:37:50 +0100

    ROX-9223: updated performance measure to run only on local integration tests

--- .circleci/run-integration-tests.sh
+++ .circleci/run-integration-tests.sh
@@ -8,32 +8,26 @@ BUILD_NUM=$3
 echo "Running tests with image '${COLLECTOR_IMAGE}'"
 
 if [[ $remote_host_type != "local" ]]; then
-    if [[ "${MEASURE_DRIVER_PERFORMANCE}" == "true" ]]; then
-        make -C "${SOURCE_ROOT}/integration-tests/container/perf" all
-
-        COLLECTOR_BCC_COMMAND="/tools/do_syscall_64.py -o /tmp/baseline.json" make -C "${SOURCE_ROOT}" integration-tests-baseline
-        COLLECTOR_BCC_COMMAND="/tools/do_syscall_64.py -o /tmp/${COLLECTION_METHOD}-benchmark.json" make -C "${SOURCE_ROOT}" integration-tests-benchmark
-
-        make -C "${SOURCE_ROOT}" integration-tests-repeat-network integration-tests integration-tests-report
-
-        cp "/tmp/baseline.json" "${WORKSPACE_ROOT}/${TEST_NAME}-baseline.json"
-        cp "/tmp/${COLLECTION_METHOD}-benchmark.json" "${WORKSPACE_ROOT}/${TEST_NAME}-${COLLECTION_METHOD}-benchmark.json"
-    else
-        # I am not sure why there is a difference in the tests that are run locally and in VMs. Perhaps they should be the same
-        make -C "${SOURCE_ROOT}" integration-tests-repeat-network integration-tests-baseline integration-tests integration-tests-report
-    fi
+    # I am not sure why there is a difference in the tests that are run locally and in VMs. Perhaps they should be the same
+    make -C "${SOURCE_ROOT}" integration-tests-repeat-network integration-tests-baseline integration-tests integration-tests-report
 
     cp "${SOURCE_ROOT}/integration-tests/perf.json" "${WORKSPACE_ROOT}/${TEST_NAME}-perf.json"
 else
     sudo cp /proc/sys/kernel/core_pattern /tmp/core_pattern
     echo '/tmp/core.out' | sudo tee /proc/sys/kernel/core_pattern
 
-    exit_code=0
-    make -C "${SOURCE_ROOT}" integration-tests-repeat-network integration-tests-missing-proc-scrape integration-tests-image-label-json integration-tests integration-tests-report \
-        || exit_code=$?
+    if [[ "${MEASURE_DRIVER_PERFORMANCE}" == "true" ]]; then
+        make -C "${SOURCE_ROOT}/integration-tests/container/perf" all
 
-    if [ -f /tmp/core.out ]; then
-        sudo chmod 755 /tmp/core.out
+        COLLECTOR_BCC_COMMAND="/tools/do_syscall_64.py -o /tmp/baseline.json" make -C "${SOURCE_ROOT}" integration-tests-baseline
+        COLLECTOR_BCC_COMMAND="/tools/do_syscall_64.py -o /tmp/${COLLECTION_METHOD}-benchmark.json" make -C "${SOURCE_ROOT}" integration-tests-benchmark
+
+        make -C "${SOURCE_ROOT}" integration-tests-repeat-network integration-tests integration-tests-report || exit_code=$?
+
+        cp "/tmp/baseline.json" "${WORKSPACE_ROOT}/${TEST_NAME}-baseline.json"
+        cp "/tmp/${COLLECTION_METHOD}-benchmark.json" "${WORKSPACE_ROOT}/${TEST_NAME}-${COLLECTION_METHOD}-benchmark.json"
+    else
+        make -C "${SOURCE_ROOT}" integration-tests-repeat-network integration-tests-missing-proc-scrape integration-tests-image-label-json integration-tests integration-tests-report || exit_code=$?
     fi
 
     if [ "${exit_code}" -ne 0 ]; then
