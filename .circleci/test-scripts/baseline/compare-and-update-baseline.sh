dde5e122bc1d115c5b130c108c847d0643bc06ed
Dmitrii Dolgov <9erthalion6@gmail.com>
2022-04-05 09:22:22 +0200

    Fix shellcheck complains

--- .circleci/test-scripts/baseline/compare-and-update-baseline.sh
+++ .circleci/test-scripts/baseline/compare-and-update-baseline.sh
@@ -3,14 +3,14 @@ set -eo pipefail
 
 pip install --upgrade scipy google-cloud-storage
 
-jq -s 'flatten' ${WORKSPACE_ROOT}/*perf.json > ${WORKSPACE_ROOT}/all-perf.json
+jq -s 'flatten' "${WORKSPACE_ROOT}"/*perf.json > "${WORKSPACE_ROOT}"/all-perf.json
 
 export BASELINE="${CI_ROOT}/test-scripts/baseline/"
-${BASELINE}/main.py --test ${WORKSPACE_ROOT}/all-perf.json \
+"${BASELINE}"/main.py --test "${WORKSPACE_ROOT}"/all-perf.json \
     | sort \
-    | awk -f ${BASELINE}/format.awk > ${WORKSPACE_ROOT}/benchmark.md
+    | awk -f "${BASELINE}"/format.awk > "${WORKSPACE_ROOT}"/benchmark.md
 
-${BASELINE}/main.py --update ${WORKSPACE_ROOT}/all-perf.json
+"${BASELINE}"/main.py --update "${WORKSPACE_ROOT}"/all-perf.json
 
 # The baseline has to be updated only in the main branch, uncomment that before
 # merging.
