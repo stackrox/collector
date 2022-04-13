3b9ac3a9b7e81b8b9c68e5b305e3fee958f87d69
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-04-13 12:00:16 +0200

    Add a small description to safe-halt script

--- .circleci/safe-halt.sh
+++ .circleci/safe-halt.sh
@@ -2,6 +2,15 @@
 
 set -euo pipefail
 
+# This script checks the 'circleci-agent' command exists before attempting to
+# call it. We were having failures in CircleCI where the 'circleci' command was
+# not found, so this script attempts to mitigate this problem by:
+# - Checking the script exists before calling it.
+# - Retry up to 5 times every 5 seconds if the first call fails.
+#
+# The 'circleci' command was also replace by ' circleci-agent', since this last
+# command is actually in charge of handling 'circleci step' calls.
+
 circleci_halt() {
     if command -v circleci-agent > /dev/null; then
         circleci-agent step halt
