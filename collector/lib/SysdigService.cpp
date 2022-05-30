62ed696f7dd05bc9a02f3d5449dcf1d1841fbcb2
Giles Hutton <ghutton@redhat.com>
2022-05-30 08:00:37 +0100

    ROX-10009: ensures that global_event_filter is only enabled on RHEL7 eBPF

--- collector/lib/SysdigService.cpp
+++ collector/lib/SysdigService.cpp
@@ -31,6 +31,7 @@ You should have received a copy of the GNU General Public License along with thi
 
 #include "CollectorException.h"
 #include "EventNames.h"
+#include "HostInfo.h"
 #include "Logging.h"
 #include "NetworkSignalHandler.h"
 #include "ProcessSignalHandler.h"
@@ -132,16 +133,15 @@ sinsp_evt* SysdigService::GetNext() {
 
   if (event->get_category() & EC_INTERNAL) return nullptr;
 
+  HostInfo& host_info = HostInfo::Instance();
+
   // This additional userspace filter is a guard against additional events
   // from the eBPF probe. This can occur when using sys_enter and sys_exit
-  // tracepoints rather than a targeted approach.
-  //
-  // When using targeted tracepoints this filter has no effect because
-  // the only events that the probe produces are of the types that we want to
-  // process.
-  // if (useEbpf && !global_event_filter_[event->get_type()]) {
-  //   return nullptr;
-  // }
+  // tracepoints rather than a targeted approach, which we currently only do
+  // on RHEL7 with backported eBPF
+  if (useEbpf && host_info.IsRHEL76() && !global_event_filter_[event->get_type()]) {
+    return nullptr;
+  }
 
   userspace_stats_.event_parse_micros[event->get_type()] += (NowMicros() - parse_start);
   ++userspace_stats_.nUserspaceEvents[event->get_type()];
