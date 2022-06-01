2d12d22c683cc1fbce9839d9c48beb959a0ef90b
Olivier Valentin <ovalenti@redhat.com>
2022-06-01 10:56:03 +0200

    Use only root and the instance user from inside the container.
    When running in rootless mode, podman will map both root and the
    user running the instance (inside) to the user running the instance
    (outside).
    TAR, when run as root, tries to restore the owner/group store
    in the archive, which then fails in podman.
    
    We revert to use the usual behaviour of TAR (non-root) and exctact
    the files as ourselves.

--- builder/install/10-protobuf.sh
+++ builder/install/10-protobuf.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-curl -L "https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/protobuf-cpp-${PROTOBUF_VERSION}.tar.gz" | tar xz
+curl -L "https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/protobuf-cpp-${PROTOBUF_VERSION}.tar.gz" | tar xz --no-same-owner
 cd "protobuf-${PROTOBUF_VERSION}"
 cp LICENSE "${LICENSE_DIR}/protobuf-${PROTOBUF_VERSION}"
 
