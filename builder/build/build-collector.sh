9d1531619ab632e0a27713f3e852277e3cc37513
Robby Cochran <rcochran@redhat.com>
2022-06-01 20:08:50 -0400

    X-Smart-Squash: Squashed 8 commits:
    3d109c8a move falcosecurity-libs directory
    e7a6e45b change builder to handle alternate mount paths use third_party/googleapis always
    0ef31e82 Add top-level cmake file, fix include paths
    ba5e3445 revert falco move
    0adb84ad revert
    8e512682 add stackrox submodule
    4be7d929 delete
    a165e508 makefile

--- builder/build/build-collector.sh
+++ builder/build/build-collector.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-set -e
+set -eux
 
-set -ux
 ldconfig -v
-cd /tmp/cmake-build
+
+SRC_ROOT_DIR="${SRC_ROOT_DIR:-/tmp/collector}"
+CMAKE_BUILD_DIR="${CMAKE_BUILD_DIR:-${SRC_ROOT_DIR}/cmake-build}"
 CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}"
 ADDRESS_SANITIZER="${ADDRESS_SANITIZER:-false}"
 COLLECTOR_APPEND_CID="${COLLECTOR_APPEND_CID:-false}"
@@ -15,21 +16,22 @@ if [ "$ADDRESS_SANITIZER" = "true" ]; then
     # two different definitions and Collector will crash when trying to connect to a grpc server.
     while read -r file; do
         sed -i 's|#include <grpc/impl/codegen/port_platform.h>|#include <grpc/impl/codegen/port_platform.h>\n#ifdef GRPC_ASAN_ENABLED\n#  undef GRPC_ASAN_ENABLED\n#endif|' "$file"
-    done < <(grep -rl port_platform.h /src/generated --include=*.h)
+    done < <(grep -rl port_platform.h "${SRC_ROOT_DIR}/generated" --include=*.h)
 fi
 
-extra_flags=(
+cmake_extra_flags=(
     -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE"
     -DADDRESS_SANITIZER="$ADDRESS_SANITIZER"
     -DCOLLECTOR_APPEND_CID="$COLLECTOR_APPEND_CID"
 )
 
-cmake "${extra_flags[@]}" /src
-make -j "${NPROCS:-2}" all
+cmake "${cmake_extra_flags[@]}" -S "${SRC_ROOT_DIR}" -B "${CMAKE_BUILD_DIR}"
+cmake --build "${CMAKE_BUILD_DIR}" --target all -- -j "${NPROCS:-2}"
+
 if [ "$CMAKE_BUILD_TYPE" = "Release" ]; then
-    echo "Strip unneeded"
     strip --strip-unneeded \
-        ./collector \
-        ./EXCLUDE_FROM_DEFAULT_BUILD/libsinsp/libsinsp-wrapper.so
+        "${CMAKE_BUILD_DIR}/collector/collector" \
+        "${CMAKE_BUILD_DIR}/collector/EXCLUDE_FROM_DEFAULT_BUILD/libsinsp/libsinsp-wrapper.so"
 fi
-cp -r /THIRD_PARTY_NOTICES .
+
+cp -r /THIRD_PARTY_NOTICES "${CMAKE_BUILD_DIR}/"
