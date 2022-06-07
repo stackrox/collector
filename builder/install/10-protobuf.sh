d1947bfc3f7123926a3410a9d53b4a8865da68dd
Robby Cochran <rcochran@redhat.com>
2022-06-07 15:10:36 -0400

    Protobuf and GRPC code generation using CMake (#685)
    - Use cmake to generate protobuf and grpc (see `collector/proto/CMakeLists.txt`) instead of `builder/build/protogen.mk` --This allows us to regenerate as part of the collector build, instead of using `make generated-srcs`.
    - Add protobuf definitions via stackrox/stackrox submodule rather than checking them in directly.
    - Other changes:
      - Remove unnecessary ncurses and cmake install scripts.
      - Use cmake when available for dependencies in the builder image.
      - Bump protobuf version from 3.11.4 to 3.20.1.

--- builder/install/10-protobuf.sh
+++ builder/install/10-protobuf.sh
@@ -5,8 +5,12 @@ set -e
 curl -L "https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/protobuf-cpp-${PROTOBUF_VERSION}.tar.gz" | tar xz
 cd "protobuf-${PROTOBUF_VERSION}"
 cp LICENSE "${LICENSE_DIR}/protobuf-${PROTOBUF_VERSION}"
-./autogen.sh
-./configure --with-zlib CXXFLAGS=-fPIC
-make -j "${NPROCS:-2}"
+
+mkdir cmake-build && cd cmake-build
+cmake ../cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DBUILD_SHARED_LIBS=OFF \
+    -Dprotobuf_BUILD_TESTS=OFF
 make install
 ldconfig
