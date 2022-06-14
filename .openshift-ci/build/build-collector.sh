6f6ec2bcd586d5330032c3ab8b094970752fe7d6
JoukoVirtanen <jouko@stackrox.com>
2022-06-13 20:16:54 -0700

    Fixed syntax error

--- .openshift-ci/build/build-collector.sh
+++ .openshift-ci/build/build-collector.sh
@@ -11,5 +11,5 @@ export SRC_ROOT_DIR=/tmp/collector
 
 make -C /tmp/collector/collector pre-build
 /build-collector.sh
-
+cp -r /tmp/collector/cmake "${ROOT}"
 make -C /tmp/collector/collector post-build
