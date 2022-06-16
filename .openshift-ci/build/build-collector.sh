35ccb2dda5f11c3c0353a2f334c9af2a40d37ba2
JoukoVirtanen <jouko@stackrox.com>
2022-06-16 15:34:50 -0700

    Addressed pr review comments from Robby

--- .openshift-ci/build/build-collector.sh
+++ .openshift-ci/build/build-collector.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-export SRC_ROOT_DIR=$1
 export DISABLE_PROFILING="true"
 export CMAKE_BUILD_DIR="$SRC_ROOT_DIR/cmake-build"
+export COLLECTOR_APPEND_CID=true
 
-make -C $SRC_ROOT_DIR/collector pre-build
-/build-collector.sh
-make -C $SRC_ROOT_DIR/collector post-build
+make -C "$SRC_ROOT_DIR/collector" pre-build
+"$SRC_ROOT_DIR/builder/build-collector.sh"
+make -C "$SRC_ROOT_DIR/collector" post-build
