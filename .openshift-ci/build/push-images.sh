4b79209868856004c99dd5e82fb8d37537c698c9
Jouko Virtanen <jouko@stackrox.com>
2022-07-14 08:44:20 -0700

    ROX-11211-collector-ci-migration-images (#738)

---
+++ .openshift-ci/build/push-images.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+oc registry login
--- builder/install/50-luajit.sh
+++ builder/install/50-luajit.sh
@@ -5,8 +5,8 @@ set -e
 if [ -n "${WITH_RHEL8_RPMS}" ]; then
     cd luajit
 else
-    wget "http://luajit.org/download/LuaJIT-${LUAJIT_VERSION}.tar.gz"
-    tar -zxf "LuaJIT-${LUAJIT_VERSION}.tar.gz"
+    wget "https://github.com/LuaJIT/LuaJIT/archive/refs/tags/v${LUAJIT_VERSION}.tar.gz"
+    tar -zxf "v${LUAJIT_VERSION}.tar.gz"
     cd "LuaJIT-${LUAJIT_VERSION}"
 fi
 cp COPYRIGHT "${LICENSE_DIR}/LuaJIT-${LUAJIT_VERSION}"
