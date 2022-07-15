6fa0e0a5f36ee3647c61c04f4049f7bb45af8bf3
JoukoVirtanen <jouko@stackrox.com>
2022-07-15 16:03:19 -0700

    X-Smart-Squash: Squashed 16 commits:
    0b4b1d4d Testing out environment variable
    5cdcb6a7 Trying to mirror image [skip ci]
    d62a7ec7 Trying to use env var for push [skip ci]
    63cbee68 Trying to use secrets
    5166feb6 Trying to login to docker [skip ci]
    c97c9efd Added debugging [skip ci]
    844787f0 Trying to use registry_rw_login
    a0575b88 Pushing the correct image [skip ci]
    f41d5619 Removed blank line
    0c872b29 Fixed shfmt and shellcheck errors
    51bf809e Not installing oc in Dockerfile.build_root
    d26b5c00 Installing oc again. Working on pushing collector with correct tag [skip ci]
    034c56a0 Using make tag to get the tag for the collector image
    a0d72403 Using full path instead of relative path for make tag [skip ci]
    d66b803e Clean up [skip ci]
    62605536 Empty commit to trigger ci

--- .openshift-ci/build/push-images.sh
+++ .openshift-ci/build/push-images.sh
@@ -1,4 +1,44 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+registry_rw_login() {
+    if [[ "$#" -ne 1 ]]; then
+        die "missing arg. usage: registry_rw_login <registry>"
+    fi
+
+    local registry="$1"
+
+    echo "registry= $registry"
+
+    case "$registry" in
+        docker.io/stackrox)
+            echo "Trying to log into docker.io/stackrox"
+            docker login -u "$DOCKER_IO_PUSH_USERNAME" -p "$DOCKER_IO_PUSH_PASSWORD" docker.io
+            ;;
+        quay.io/rhacs-eng)
+            echo "Trying to log into quay.io/rhacs-eng"
+            docker login -u "$QUAY_RHACS_ENG_RW_USERNAME" -p "$QUAY_RHACS_ENG_RW_PASSWORD" quay.io
+            ;;
+        quay.io/stackrox-io)
+            echo "Trying to log into quay.io/stackrox-io"
+            docker login -u "$QUAY_STACKROX_IO_RW_USERNAME" -p "$QUAY_STACKROX_IO_RW_PASSWORD" quay.io
+            ;;
+        *)
+            echo "Unsupported registry login: $registry"
+            ;;
+    esac
+}
+
+shopt -s nullglob
+for cred in /tmp/secret/**/[A-Z]*; do
+    export "$(basename "$cred")"="$(cat "$cred")"
+done
+
 oc registry login
+
+registry_rw_login quay.io/rhacs-eng
+
+cd /go/src/github.com/stackrox/collector
+COLLECTOR_VERSION="$(make tag)"
+
+oc image mirror "$COLLECTOR_FULL" quay.io/rhacs-eng/collector:"$COLLECTOR_VERSION"
