5a4a5b07a85a295159c6a76526e963db247b7d15
Olivier Valentin <ovalenti@redhat.com>
2022-07-19 19:15:40 +0200

    ROX-11761 Factorize builder and ubi dependencies (#752)
    In order to detect build issues as early as possible, we share the same dependencies between the 'builder' flavor and the UBI build.
    For dependencies on development packages available as RPMs, we prefer them, unless the version is too old.
    Dependencies only required by the builder image are kept as internet archive downloads

--- builder/install/30-cares.sh
+++ builder/install/30-cares.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ -n "${WITH_RHEL8_RPMS}" ]; then
+    # Already installed as an RPM, nothing more to do.
+    exit 0
+fi
+
 wget "https://c-ares.haxx.se/download/c-ares-${CARES_VERSION}.tar.gz"
 tar -zxf "c-ares-${CARES_VERSION}.tar.gz"
 cd "c-ares-${CARES_VERSION}"
@@ -9,6 +14,5 @@ cp LICENSE.md "${LICENSE_DIR}/c-ares-${CARES_VERSION}"
 
 mkdir cmake-build
 cd cmake-build
-cmake -DCMAKE_BUILD_TYPE=Release -DCARES_INSTALL=ON -DCMAKE_INSTALL_PREFIX=/usr -DCARES_SHARED=OFF -DCARES_STATIC=ON -DCARES_STATIC_PIC=ON ..
-make -j "${NPROCS:-2}"
-make install
+cmake -DCMAKE_BUILD_TYPE=Release -DCARES_INSTALL=ON -DCMAKE_INSTALL_PREFIX=/usr/local -DCARES_SHARED=OFF -DCARES_STATIC=ON -DCARES_STATIC_PIC=ON ..
+cmake --build . --target install ${NPROCS:+-j ${NPROCS}}
