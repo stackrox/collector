27079ff8e6c6cf1387993fe3c0768f65fee07d8e
Giles Hutton <ghutton@redhat.com>
2022-07-19 18:04:05 +0100

    ROX-11222: some additional logging

--- .openshift-ci/jobs/integration-tests/env.sh
+++ .openshift-ci/jobs/integration-tests/env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x
+
 export JOB_ID="${PROW_JOB_ID:0:8}"
 export GCP_SSH_KEY_FILE="$HOME/.ssh/GCP_SSH_KEY"
 export GCLOUD_INSTANCE="collector-osci-${COLLECTION_METHOD}-${VM_TYPE}-${IMAGE_FAMILY}-${JOB_ID}"
@@ -10,6 +12,7 @@ export VM_CONFIG="${VM_TYPE}.${IMAGE_FAMILY}"
 export COLLECTOR_REPO="quay.io/rhacs-eng/collector"
 export COLLECTOR_IMAGE="${COLLECTOR_REPO}:3.9.0"
 
+whoami
 ls -alh /go/pkg || true
 
 mkdir -p "$HOME/mod-cache"
