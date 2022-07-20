36da1e1e450dd4fea4949354a3f89958f1a4d57b
sumitd2 <91451282+sumitd2@users.noreply.github.com>
2022-07-20 05:52:44 +0530

    ppc64le support - Installment 4

--- builder/install/50-luajit.sh
+++ builder/install/50-luajit.sh
@@ -2,8 +2,14 @@
 
 set -e
 
-cd third_party/luajit
-
-cp COPYRIGHT "${LICENSE_DIR}/LuaJIT-${LUAJIT_VERSION}"
+if [ "$(arch)" = "x86_64" ]; then
+  cd third_party/luajit
+  cp COPYRIGHT "${LICENSE_DIR}/LuaJIT-${LUAJIT_VERSION}"
+else
+  cd third_party
+  git clone https://github.com/openresty/luajit2.git
+  cd luajit2
+  git checkout v${LUAJIT_VERSION}
+fi
 
 make ${NPROCS:+-j ${NPROCS}} install PREFIX=/usr/local CFLAGS=-fPIC
