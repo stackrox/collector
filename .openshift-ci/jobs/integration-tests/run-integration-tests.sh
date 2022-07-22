cf0daacdffcfd80eff103e5dcf252ddedae2261a
Giles Hutton <ghutton@redhat.com>
2022-07-22 09:01:29 +0100

    ROX-11222: ensures all container logs are saved as artifacts

--- .openshift-ci/jobs/integration-tests/run-integration-tests.sh
+++ .openshift-ci/jobs/integration-tests/run-integration-tests.sh
@@ -51,11 +51,20 @@ function run_tests() {
     return $exit_code
 }
 
+function save_artifacts() {
+    cp "integration-tests/perf.json" "${ARTIFACT_DIR}/${JOB_NAME_SAFE}-perf.json"
+    cp -r "integration-tests/container-logs/" "${ARTIFACT_DIR}"
+
+    if [[ -d "integration-tests/performance-logs" ]]; then
+        cp -r "integration-tests/performance-logs" "${ARITFACT_DIR}"
+    fi
+}
+
 exit_code=0
 
 if [[ $REMOTE_HOST_TYPE != "local" ]]; then
     run_tests || exit_code=$?
-    cp "integration-tests/perf.json" "${ARTIFACT_DIR}/${JOB_NAME_SAFE}-perf.json"
+    save_artifacts
     if [ "${exit_code}" -ne 0 ]; then
         exit "${exit_code}"
     fi
