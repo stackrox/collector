a1aca98ebd7f9ee85c72d6393e98195cc964a39d
JoukoVirtanen <jouko@stackrox.com>
2022-07-29 16:15:56 -0700

    Removed civetweb, Dockerfile for builder, and install scripts

--- builder/install/50-luajit.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-cd third_party/luajit
-
-cp COPYRIGHT "${LICENSE_DIR}/LuaJIT-${LUAJIT_VERSION}"
-
-make ${NPROCS:+-j ${NPROCS}} install PREFIX=/usr/local CFLAGS=-fPIC
