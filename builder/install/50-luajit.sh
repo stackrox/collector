aeb3f2d9b595a01fec53846d7e46f67c84782d03
sumitd2 <91451282+sumitd2@users.noreply.github.com>
2022-08-04 12:44:37 +0530

    Kernel modules workdir, copy googletest from third-party

--- builder/install/50-luajit.sh
+++ builder/install/50-luajit.sh
@@ -3,7 +3,6 @@
 set -e
 
 cd third_party/luajit2
-git checkout v${LUAJIT2_VERSION}
 cp COPYRIGHT "${LICENSE_DIR}/LuaJIT-${LUAJIT2_VERSION}"
 
 make ${NPROCS:+-j ${NPROCS}} install PREFIX=/usr/local CFLAGS=-fPIC
