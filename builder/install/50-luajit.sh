98f10f35c52dfabe4bba11804e3e8894dd1a55e3
sumitd2 <91451282+sumitd2@users.noreply.github.com>
2022-08-04 11:31:29 +0530

    Remove luajit references

--- builder/install/50-luajit.sh
+++ builder/install/50-luajit.sh
@@ -2,12 +2,8 @@
 
 set -e
 
-if [ "$(arch)" = "x86_64" ]; then
-    cd third_party/luajit
-    cp COPYRIGHT "${LICENSE_DIR}/LuaJIT-${LUAJIT_VERSION}"
-else
-    cd third_party/luajit2
-    cp COPYRIGHT "${LICENSE_DIR}/LuaJIT-${LUAJIT2_VERSION}"
-fi
+cd third_party/luajit2
+git checkout v${LUAJIT2_VERSION}
+cp COPYRIGHT "${LICENSE_DIR}/LuaJIT-${LUAJIT2_VERSION}"
 
 make ${NPROCS:+-j ${NPROCS}} install PREFIX=/usr/local CFLAGS=-fPIC
