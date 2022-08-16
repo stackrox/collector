077c82e7d39d5ec5eb8a7310f01187f0e60bf8bc
JoukoVirtanen <jouko@stackrox.com>
2022-08-16 10:50:08 -0700

    Fixed typo

--- kernel-modules/support-packages/run-all.sh
+++ kernel-modules/support-packages/run-all.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 SOURCE_ROOT=$1
 SUPPORT_PKG_SRC_ROOT=$2
 COLLECTOR_MODULES_BUCKET=${3:-"gs://collector-modules/612dd2ee06b660e728292de9393e18c81a88f347ec52a39207c5166b5302b656"}
-LICENSE_FILE={$4:-"${SOURCE_ROOT}/collector/LICENSE-kernel-modules.txt"}
+LICENSE_FILE=${4:-"${SOURCE_ROOT}/collector/LICENSE-kernel-modules.txt"}
 
 "${SUPPORT_PKG_SRC_ROOT}/01-collector-to-rox-version-map.py" \
         "${SOURCE_ROOT}/RELEASED_VERSIONS" \
