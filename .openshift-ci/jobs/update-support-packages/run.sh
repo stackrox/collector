dacd7d2a0a399c3a86146e2eb33f120b4ec43fba
JoukoVirtanen <jouko@stackrox.com>
2022-08-24 12:08:35 -0700

    Will now push to public cloud on master

--- .openshift-ci/jobs/update-support-packages/run.sh
+++ .openshift-ci/jobs/update-support-packages/run.sh
@@ -5,6 +5,14 @@ gcloud_command() {
     gcloud compute ssh --ssh-key-file="$GCP_SSH_KEY_FILE" "$GCLOUD_USER"@"$GCLOUD_INSTANCE" -- -T "$1"
 }
 
+sleep_echo() {
+    for i in {1..120}; do
+        sleep 1
+        echo -n .
+    done
+    echo
+}
+
 export WORKDIR=/go/src/github.com/stackrox/collector
 
 source "$WORKDIR/.openshift-ci/drivers/scripts/lib.sh"
@@ -14,12 +22,11 @@ BRANCH="$(get_branch)"
 # shellcheck source=SCRIPTDIR/../../drivers/scripts/lib.sh
 source "${WORKDIR}/.openshift-ci/drivers/scripts/lib.sh"
 
-# Uncomment before merging
-#if ! pr_has_label "test-support-packages"; then
-#   if [ "${BRANCH}" != "master" ]; then
-#      exit 0
-#   fi
-#fi
+if ! pr_has_label "test-support-packages"; then
+   if [ "${BRANCH}" != "master" ]; then
+      exit 0
+   fi
+fi
 
 # shellcheck source=SCRIPTDIR/../../jobs/update-support-packages/env.sh
 source "${WORKDIR}/.openshift-ci/jobs/update-support-packages/env.sh"
@@ -33,9 +40,9 @@ gcloud_command "$SUPPORT_PKG_SRC_ROOT/run-all.sh $SOURCE_ROOT $SUPPORT_PKG_SRC_R
 
 RELATIVE_PATH="collector/support-packages"
 GCLOUD_BUCKET="gs://sr-roxc"
-# Will be used in the future
-#PUBLIC_RELATIVE_PATH="offline/v1/support-packages"
-#PUBLIC_GCLOUD_BUCKET="gs://collector-support-public"
+PUBLIC_GCLOUD_BUCKET="gs://collector-support-public"
+PUBLIC_RELATIVE_PATH="offline/v1/support-packages"
+PUBLIC_GCLOUD_TARGET="${PUBLIC_GCLOUD_BUCKET}/${PUBLIC_RELATIVE_PATH}"
 
 relative_path="$RELATIVE_PATH"
 
@@ -46,3 +53,19 @@ fi
 GCLOUD_TARGET="${GCLOUD_BUCKET}/${relative_path}"
 
 gcloud_command "gsutil -m rsync -r /tmp/support-packages/output $GCLOUD_TARGET"
+
+sleep_echo
+
+[[ "$GCLOUD_TARGET" =~ ^gs://[^/]+/.*collector.*/.*support-packages.*$ ]]
+
+[[ "$BRANCH" == "master" || "$GCLOUD_TARGET" = *.test* ]]
+gcloud_command "gsutil -m rsync -n -r -d /tmp/support-packages/output $GCLOUD_TARGET"
+
+if [[ "$BRANCH" != "master" ]]; then
+    exit 0
+fi
+
+gcloud_command "gsutil -m rsync -r /tmp/support-packages/output $PUBLIC_GCLOUD_TARGET"
+sleep_echo
+gcloud_command "gsutil -m rsync -n -r -d /tmp/support-packages/output $PUBLIC_GCLOUD_TARGET"
+
