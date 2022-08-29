ae95f6fa32704f4cb270d59eb93e7ecc3aef4308
Robby Cochran <rcochran@redhat.com>
2022-08-29 11:29:37 -0700

    remove changes

--- .openshift-ci/drivers/scripts/kernel-splitter.py
+++ .openshift-ci/drivers/scripts/kernel-splitter.py
@@ -153,7 +153,6 @@ def main(task_file):
     fc36_kernels = r"(?:5\.[1-9]\d+\..*)"
     rhel8_kernels = r"(?:(?:4|5)\.\d+\..*)"
     rhel7_kernels = r"(?:3\.\d+\..*)"
-    # rhel8_ebpf_kernels = r'(?:5\.\d+\..*)'
     rhel7_ebpf_kernels = r"(?:(?:3|4|5)\.\d+\..*)"
 
     fc36 = Builder("fc36", rf"^{fc36_kernels}", {})
