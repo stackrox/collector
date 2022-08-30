c72a3d4e8986fcd1854859d077ac8a30716f7c57
Robby Cochran <rcochran@redhat.com>
2022-08-29 23:32:14 -0700

    Revert "base_ref"
    This reverts commit f72ebccd3dfbf0b0b266ed04f07a3d2eb2e39e97.

--- .openshift-ci/drivers/scripts/lib.sh
+++ .openshift-ci/drivers/scripts/lib.sh
@@ -85,7 +85,11 @@ get_branch() {
     if [[ -n "${CLONEREFS_OPTIONS:-}" ]]; then
         # periodics - CLONEREFS_OPTIONS exists in binary_build_commands and images.
         local base_ref
-        base_ref="$(jq -r '.refs[] | select(.repo=="collector") | .base_ref' <<< "${CLONEREFS_OPTIONS}")" || die "invalid CLONEREFS_OPTIONS json"
+        if is_openshift_CI_rehearse_PR; then
+            base_ref="$(jq -r '.refs[] | select(.repo=="collector") | .base_ref' <<< "${CLONEREFS_OPTIONS}")" || die "invalid CLONEREFS_OPTIONS json"
+        else
+            base_ref="$(jq -r '.refs[0].base_ref' <<< "${CLONEREFS_OPTIONS}")" || die "invalid CLONEREFS_OPTIONS json"
+        fi
         if [[ "$base_ref" == "null" ]]; then
             die "expect: base_ref in CLONEREFS_OPTIONS.refs[0]"
         fi
