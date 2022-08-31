7d7c37189b2b368ddefaf1bad8e13d079d76337a
Dmitrii Dolgov <9erthalion6@gmail.com>
2022-08-31 14:51:06 +0200

    Correct branch (#806)
    .extra_ref doesn't contain the branch for builds from the main branch.
    Temporarily fix it by fetching directly from JOB_SPEC, but in the long
    term use scripts/lib.sh for this.

--- .openshift-ci/slack/notify-if-needed.sh
+++ .openshift-ci/slack/notify-if-needed.sh
@@ -4,8 +4,9 @@ set -eo pipefail
 ERROR_MSG=$1
 ERROR_CODE=$2
 
+# TODO: Replace with get_branch as soon as scripts/lib.sh will be merged
 # shellcheck disable=SC2155
-export BRANCH="$(echo "$JOB_SPEC" | jq -r '.extra_refs[0].base_ref')"
+export BRANCH="$(echo "$JOB_SPEC" | jq -r '.refs.base_ref')"
 
 # No notifications for feature branches
 if [[ ! "$BRANCH" =~ ^(master|main|release)$ ]]; then
