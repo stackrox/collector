e1b8a4abc085b7466630a81d2a7fdbcd6fd690a7
Dmitrii Dolgov <9erthalion6@gmail.com>
2022-09-07 16:40:37 +0200

    Merge conditions

--- .openshift-ci/build/build-collector.sh
+++ .openshift-ci/build/build-collector.sh
@@ -6,10 +6,8 @@ export CMAKE_BUILD_DIR="$SRC_ROOT_DIR/cmake-build"
 export COLLECTOR_APPEND_CID=true
 
 # For all PRs build in debug mode, unless requested otherwise via labels.
-if is_in_PR_context; then
-    if ! pr_has_label "release-build-type"; then
-        export CMAKE_BUILD_TYPE="Debug"
-    fi
+if is_in_PR_context && ! pr_has_label "release-build-type"; then
+    export CMAKE_BUILD_TYPE="Debug"
 fi
 
 make -C "$SRC_ROOT_DIR/collector" pre-build
