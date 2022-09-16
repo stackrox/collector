cb0f8b3c2804773f63ba0739dba5b50c871659c6
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-09-16 16:51:42 +0200

    Use static re2 library

--- builder/install/50-re2.sh
+++ builder/install/50-re2.sh
@@ -6,5 +6,5 @@ cd third_party/re2
 
 cp LICENSE "${LICENSE_DIR}/re2-${RE2_VERSION}"
 
-make ${NPROCS:+-j ${NPROCS}}
-make ${NPROCS:+-j ${NPROCS}} shared-install
+make ${NPROCS:+-j ${NPROCS}} CPPFLAGS=-fPIC
+make ${NPROCS:+-j ${NPROCS}} static-install
