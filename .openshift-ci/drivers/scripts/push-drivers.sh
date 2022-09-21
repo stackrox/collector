78f00f79035fa4fb0c3bcd7215e1aa48c4a2a277
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-09-21 18:17:44 +0200

    Allow drivers to be pushed for testing

--- .openshift-ci/drivers/scripts/push-drivers.sh
+++ .openshift-ci/drivers/scripts/push-drivers.sh
@@ -32,7 +32,8 @@ shopt -s nullglob
 shopt -s dotglob
 upload_drivers "/built-drivers/" "${target}"
 
-if ! is_in_PR_context; then
+# vvvvvvvv Rollback this comment before merging vvvvvvvv
+#if ! is_in_PR_context; then
     # On tags/master builds, additionally upload modules from cache
     upload_drivers "/kernel-modules/" "${target}"
-fi
+#fi
