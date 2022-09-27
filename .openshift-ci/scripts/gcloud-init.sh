2c981ca859c075089380f1c9341836733c631680
Giles Hutton <ghutton@redhat.com>
2022-09-27 15:18:23 +0100

    Makes push-drivers use gcloud-init script and makes that script more flexible to different creds / buckets

--- .openshift-ci/scripts/gcloud-init.sh
+++ .openshift-ci/scripts/gcloud-init.sh
@@ -5,6 +5,9 @@ CI_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
 # shellcheck source=SCRIPTDIR/lib.sh
 source "${CI_ROOT}/scripts/lib.sh"
 
+CREDS_FILE="${1:-$(get_secret_file GOOGLE_CREDENTIALS_COLLECTOR_SVC_ACCT)}"
+TEST_BUCKET="${2:-gs://collector-build-cache}"
+
 which gcloud
 which gsutil
 gsutil ver
@@ -12,11 +15,11 @@ gsutil ver -l | grep crcmod
 gcloud version
 cat ~/.boto 2> /dev/null || true
 echo '[Credentials]' > ~/.boto
-echo "gs_service_key_file = $(get_secret_file GOOGLE_CREDENTIALS_COLLECTOR_SVC_ACCT)" >> ~/.boto
-gcloud auth activate-service-account --key-file "$(get_secret_file GOOGLE_CREDENTIALS_COLLECTOR_SVC_ACCT)"
+echo "gs_service_key_file = ${CREDS_FILE}" >> ~/.boto
+gcloud auth activate-service-account --key-file "${CREDS_FILE}"
 gcloud config set project stackrox-ci
 gcloud config set compute/region us-central1
 gcloud config set compute/zone us-central1-a
 gcloud config set core/disable_prompts True
 gcloud auth list
-gsutil ls "gs://collector-build-cache" || echo "ERROR: Could not ls bucket"
+gsutil ls "${TEST_BUCKET}" || echo "ERROR: Could not ls ${TEST_BUCKET}"
