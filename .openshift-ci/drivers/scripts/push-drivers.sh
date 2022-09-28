2f3578da7d0ac6d3309e4dcbbb42f604b2777962
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-09-28 17:00:38 +0200

    Rollback debug changes

--- .openshift-ci/drivers/scripts/push-drivers.sh
+++ .openshift-ci/drivers/scripts/push-drivers.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-set -exo pipefail
+set -eo pipefail
 
 # shellcheck source=SCRIPTDIR/../../scripts/lib.sh
 source /scripts/lib.sh
@@ -27,9 +27,6 @@ if is_in_PR_context; then
     BRANCH="$(get_branch)"
     target="gs://stackrox-collector-modules-staging/pr-builds/${BRANCH}/${BUILD_ID}"
 fi
-
-# DEBUG: No pushing for now
-exit 0
 
 shopt -s nullglob
 shopt -s dotglob
