df352b11ff98f42ad0c9a07ee3d6cbf2edc9eafb
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-10-04 11:10:16 +0200

    Have CI build all necessary QA containers (#674)
    This commit makes it so all containers needed to
    run integration tests and benchmarking are built
    by our CI and pushed to quay.io periodically.
    
    The containers will not be built in PRs if the
    build-qa-containers label is not present. Layer
    caching is enabled in the job in charge of
    building the containers.

--- integration-tests/utils.go
+++ integration-tests/utils.go
@@ -24,3 +24,22 @@ func quoteArgs(args []string) []string {
 	}
 	return quotedArgs
 }
+
+// Generate the QA tag to be used for containers by attaching the contents of
+// the 'COLLECTOR_QA_TAG' environment variable if it exists. Return the base
+// tag as is otherwise.
+func getQATag(base_tag string) string {
+	collector_qa_tag := ReadEnvVar("COLLECTOR_QA_TAG")
+
+	if collector_qa_tag != "" {
+		return base_tag + "-" + collector_qa_tag;
+	}
+	return base_tag
+}
+
+// Return the full image to be used for a QA container from a given image name
+// and a tag. The tag will be adjusted accordingly to the description of
+// 'getQaTag'
+func qaImage(image string, tag string) string {
+	return image + ":" + getQATag(tag);
+}
