4a867d64caa8a99b92b5c4f98992f7854c50e64d
Kirk Crane <kcrane@redhat.com>
2022-10-05 14:18:03 -0400

    add comment explaining ebpf on z situation
    Signed-off-by: Kirk Crane <kcrane@redhat.com>

--- collector/lib/HostInfo.h
+++ collector/lib/HostInfo.h
@@ -109,12 +109,14 @@ struct KernelVersion {
   // Whether or not the kernel has built-in eBPF support
   // Anything before 4.14 doesn't have support, anything newer does.
   bool HasEBPFSupport() const {
+#ifdef __s390x__
+    // TODO: only kernel module will ship with s390x until socketcall is handled in ebpf probe.
+    //       back out github.com/stackrox/collector/pull/855 when ebpf is ready to ship.
+    return false;
+#else
     if (kernel < 4 || (kernel == 4 && major < 14)) {
       return false;
     }
-#ifdef __s390x__
-    return false;
-#else
     return true;
 #endif
   }
