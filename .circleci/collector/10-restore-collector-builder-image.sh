93174971166e888f7d8d439073d2d93f892c5144
Jouko Virtanen <jouko@stackrox.com>
2022-10-06 08:19:56 -0700

    Jv rox 12304 remove unused scripts in circleci directory (#801)

--- .circleci/collector/10-restore-collector-builder-image.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-docker pull "quay.io/rhacs-eng/collector-builder:${COLLECTOR_BUILDER_TAG}"
--- .circleci/images/60-sanity-check.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-cd "$SOURCE_ROOT"
-echo '>>> Collector Artifacts:'
-find collector/container
-echo '>>> Kernel Modules:'
-find kernel-modules/container
