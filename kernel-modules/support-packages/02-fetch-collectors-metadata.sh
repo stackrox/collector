b5a37d2319d8a03115b074538c012d3199c52051
JoukoVirtanen <jouko@stackrox.com>
2022-10-20 15:58:03 -0400

    Fixed shellcheck error

--- kernel-modules/support-packages/02-fetch-collectors-metadata.sh
+++ kernel-modules/support-packages/02-fetch-collectors-metadata.sh
@@ -37,6 +37,6 @@ done
 
 master_dir="${MD_DIR}/collector-versions/master"
 mkdir -p "${master_dir}"
-cd ${SOURCE_ROOT}
+cd "${SOURCE_ROOT}"
 git checkout master -- "kernel-modules/MODULE_VERSION"
 cp "${SOURCE_ROOT}/kernel-modules/MODULE_VERSION" "$master_dir"
