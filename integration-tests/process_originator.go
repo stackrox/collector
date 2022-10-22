c81a4214d491b9da381b6eb0d18ea4cfbdd2dc6d
JoukoVirtanen <jouko@stackrox.com>
2022-10-22 17:16:58 -0400

    X-Smart-Squash: Squashed 12 commits:
    94887919 ConnScraperTest checkes for Originator
    a4029f94 Asserts that process info is nil
    012282ff Updated mock grpc server image
    81473db9 ProcessOriginator object is now a part of the Endpoint object
    a0582572 Improved parsing of string into ProcessOriginator
    56045875 Removed commented out code
    9eca79f5 Changed the way that ProcessOriginator is parsed and how errors in parsing it are handled
    2e2fd322 Is still able to get endpoints when ProcessOriginator is nil. This will change in a future PR
    6e10fd0a Pulled in changes from jv-rox-13015-collector-integration-tests-should-check-for-process-info-in-endpoints-and-find-it
    7e1c518f Added back in the changes lost from other prs including retries and checking for endpoints from networksignal handler
    670fb279 Add back deleted comment
    b147767e Not running connscraper for rhel-7 and ubuntu-pro-1804-lts

---
+++ integration-tests/process_originator.go
@@ -0,0 +1,51 @@
+package integrationtests
+
+import (
+	"strings"
+	"fmt"
+	"regexp"
+)
+
+type ProcessOriginator struct {
+	ProcessName string
+	ProcessExecFilePath string
+	ProcessArgs string
+}
+
+func removeQuotesAndWhiteSpace(s string) string {
+	s = strings.TrimSpace(s)
+	if len(s) > 0 && s[0] == '"' {
+		s = s[1:]
+	}
+	if len(s) > 0 && s[len(s)-1] == '"' {
+		s = s[:len(s)-1]
+	}
+	return s
+}
+
+func NewProcessOriginator(line string) (*ProcessOriginator, error) {
+	if line == "<nil>\n" {
+		return nil, fmt.Errorf("ProcessOriginator is nil")
+	}
+
+	var processArgs string
+	r := regexp.MustCompile("process_name:(.*)process_exec_file_path:(.*)process_args(.*)\n$")
+	processArr := r.FindStringSubmatch(line)
+	if len(processArr) !=4 {
+		r := regexp.MustCompile("process_name:(.*)process_exec_file_path:(.*)\n$")
+		processArr = r.FindStringSubmatch(line)
+		if len(processArr) != 3 {
+			return nil, fmt.Errorf("Could not parse process originator %s", line)
+		}
+	} else {
+		processArgs = removeQuotesAndWhiteSpace(processArr[3])
+	}
+	processName := removeQuotesAndWhiteSpace(processArr[1])
+	processExecFilePath := removeQuotesAndWhiteSpace(processArr[2])
+
+	return &ProcessOriginator {
+		ProcessName: processName,
+		ProcessExecFilePath: processExecFilePath,
+		ProcessArgs: processArgs,
+	}, nil
+}
