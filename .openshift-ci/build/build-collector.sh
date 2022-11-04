805f925d06e4d329e8217a6f447778920e5777e2
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-11-04 11:11:20 +0100

    Only append CID on PR builds

--- .openshift-ci/build/build-collector.sh
+++ .openshift-ci/build/build-collector.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+CI_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
+
+# shellcheck source=SCRIPTDIR/../../scripts/lib.sh
+source "${CI_ROOT}/scripts/lib.sh"
+
 export DISABLE_PROFILING="true"
 export CMAKE_BUILD_DIR="$SRC_ROOT_DIR/cmake-build"
-export COLLECTOR_APPEND_CID=true
+
+if ! is_in_PR_context; then
+    export COLLECTOR_APPEND_CID=true
+fi
 
 make -C "$SRC_ROOT_DIR/collector" pre-build
 "$SRC_ROOT_DIR/builder/build/build-collector.sh"
