03703d921e41b584d10e24b029b6729a9c2601c6
Giles Hutton <ghutton@redhat.com>
2022-11-21 14:17:58 +0000

    Reinstates baseline scripts and associated templates

---
+++ .openshift-ci/scripts/baseline/compare-and-update-baseline.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+jq -s 'flatten' "${WORKSPACE_ROOT}"/*perf.json > "${WORKSPACE_ROOT}"/all-perf.json
+
+export BASELINE="${CI_ROOT}/test-scripts/baseline"
+"${BASELINE}"/main.py --test "${WORKSPACE_ROOT}"/all-perf.json \
+    | sort \
+    | awk -f "${BASELINE}"/format.awk > "${WORKSPACE_ROOT}"/benchmark.md
+
+# The baseline has to be updated only in the main branch.
+# Take into account that CIRCLE_TAG maybe not set.
+if [[ "$CIRCLE_BRANCH" == "master" || -n "${CIRCLE_TAG:-}" ]]; then
+    "${BASELINE}"/main.py --update "${WORKSPACE_ROOT}"/all-perf.json
+fi
