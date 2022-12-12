1297550d53377e4fde8a726242f5b7f0d43aff93
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-12-12 15:37:25 +0100

    Minor fix for building new versions of drivers

--- .github/drivers/mock-cache.sh
+++ .github/drivers/mock-cache.sh
@@ -9,6 +9,11 @@ for module_version_dir in /tmp/kobuild-tmp/versions-src/*; do
     module_version="$(basename "${module_version_dir}")"
     mkdir -p "/tmp/kernel-modules/$module_version/"
 
+    # If the bucket doesn't exist, we are building a new version of drivers
+    if ! gsutil ls "gs://collector-modules-public/${module_version}/" 1>&2 2> /dev/null; then
+        continue
+    fi
+
     # The awk command is really ugly, but basically extracts the driver name
     # from the ls command and creates the path where the driver will be looked
     # for on next steps.
