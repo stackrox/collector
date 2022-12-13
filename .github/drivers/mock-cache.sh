06fd8a1894339e36d6f491415525949991bd034e
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-12-13 13:25:43 +0100

    Follow up PR for GHA driver builds (#933)

--- .github/drivers/mock-cache.sh
+++ .github/drivers/mock-cache.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+# TODO: Move the buckets back to the official ones once we are done testing
+
 # This script creates a mock cache of drivers, meaning the actual drivers
 # aren't downloaded, but the rest of the scripts will think they are.
 
@@ -9,10 +11,16 @@ for module_version_dir in /tmp/kobuild-tmp/versions-src/*; do
     module_version="$(basename "${module_version_dir}")"
     mkdir -p "/tmp/kernel-modules/$module_version/"
 
+    # If the bucket doesn't exist, we are building a new version of drivers
+    if ! gsutil ls "gs://mauro-drivers-test/drivers/${module_version}/" 1>&2 2> /dev/null; then
+        echo "Could not find cached drivers for '${module_version}'. Continuing with no cache"
+        continue
+    fi
+
     # The awk command is really ugly, but basically extracts the driver name
     # from the ls command and creates the path where the driver will be looked
     # for on next steps.
-    gsutil ls "gs://collector-modules-public/${module_version}/" \
+    gsutil ls "gs://mauro-drivers-test/drivers/${module_version}/" \
         | awk -F "/" -v version="${module_version}" '{print "/tmp/kernel-modules/" version "/" $NF}' \
         | xargs touch
 done
