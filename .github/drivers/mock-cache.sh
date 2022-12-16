d1a881821d9bd1d536d01008615633e81aab3830
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-12-16 15:01:37 +0100

    Move some common logic to an init workflow

--- .github/drivers/mock-cache.sh
+++ .github/drivers/mock-cache.sh
@@ -7,12 +7,14 @@ set -euo pipefail
 # This script creates a mock cache of drivers, meaning the actual drivers
 # aren't downloaded, but the rest of the scripts will think they are.
 
+GCP_BUCKET="gs://${DRIVERS_BUCKET}"
+
 for module_version_dir in /tmp/kobuild-tmp/versions-src/*; do
     module_version="$(basename "${module_version_dir}")"
     mkdir -p "/tmp/kernel-modules/$module_version/"
 
     # If the bucket doesn't exist, we are building a new version of drivers
-    if ! gsutil ls "gs://mauro-drivers-test/drivers/${module_version}/" > /dev/null; then
+    if ! gsutil ls "${GCP_BUCKET}/${module_version}/" > /dev/null; then
         echo "Could not find cached drivers for '${module_version}'. Continuing with no cache"
         continue
     fi
@@ -20,7 +22,7 @@ for module_version_dir in /tmp/kobuild-tmp/versions-src/*; do
     # The awk command is really ugly, but basically extracts the driver name
     # from the ls command and creates the path where the driver will be looked
     # for on next steps.
-    gsutil ls "gs://mauro-drivers-test/drivers/${module_version}/" \
+    gsutil ls "${GCP_BUCKET}/${module_version}/" \
         | awk -F "/" -v version="${module_version}" '{print "/tmp/kernel-modules/" version "/" $NF}' \
         | xargs touch
 done
