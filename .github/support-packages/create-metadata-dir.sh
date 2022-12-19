6b7619c4120550a0cad42119668e3c10936eeeea
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-12-19 15:47:18 +0100

    Address some review comments

--- .github/support-packages/create-metadata-dir.sh
+++ .github/support-packages/create-metadata-dir.sh
@@ -6,8 +6,7 @@ for version_dir in /tmp/support-packages/metadata/collector-versions/*; do
     version="$(basename "$version_dir")"
     git checkout "${version}"
 
-    driver_version="$(cat "${GITHUB_WORKSPACE}/kernel-modules/MODULE_VERSION")"
-    echo "${driver_version}" > "${version_dir}/MODULE_VERSION"
+    cp "${GITHUB_WORKSPACE}/kernel-modules/MODULE_VERSION" "${version_dir}/MODULE_VERSION"
 done
 
 git checkout "${BRANCH_NAME}"
