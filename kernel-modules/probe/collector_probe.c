4ce6271eeadc9459df5193f2952b3beb8c2582c0
Giles Hutton <ghutton@redhat.com>
2023-06-19 15:36:23 +0100

    Remove final reference to mapped_id

--- kernel-modules/probe/collector_probe.c
+++ kernel-modules/probe/collector_probe.c
@@ -310,7 +310,7 @@ static __always_inline int exit_probe(long id, struct sys_exit_args* ctx) {
 
 #if defined(CAPTURE_SOCKETCALL)
   if (id == __NR_socketcall) {
-    mapped_id = convert_network_syscalls(ctx);
+    id = convert_network_syscalls(ctx);
   }
 #endif
 
