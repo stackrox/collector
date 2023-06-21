5fb575a315c19361ef07daa3cd9b46b2f9062a02
Olivier Valentin <ovalenti@redhat.com>
2023-06-21 17:02:53 +0200

    Install libelf in the final image

--- collector/container/rhel/install.sh
+++ collector/container/rhel/install.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 microdnf upgrade -y
-microdnf install -y kmod findutils
+microdnf install -y kmod findutils elfutils-libelf
 
 microdnf clean all
 rpm --query --all 'curl' '*rpm*' '*dnf*' '*libsolv*' '*hawkey*' 'yum*' 'findutils' | xargs -t rpm -e --nodeps
