432cb33dfa77c218d74111928b9a94fede16be06
Olivier Valentin <ovalenti@redhat.com>
2023-06-21 16:47:32 +0200

    Bump images and submodules

--- builder/install/30-abseil.sh
+++ builder/install/30-abseil.sh
@@ -8,5 +8,11 @@ cp LICENSE "${LICENSE_DIR}/Abseil-${ABSEIL_VERSION}"
 
 mkdir cmake-build
 cd cmake-build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_POSITION_INDEPENDENT_CODE=True ..
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr/local \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DABSL_PROPAGATE_CXX_STD=ON
+
 cmake --build . --target install ${NPROCS:+-j ${NPROCS}}
--- builder/install/35-re2.sh
+++ builder/install/35-re2.sh
@@ -6,5 +6,10 @@ cd third_party/re2
 
 cp LICENSE "${LICENSE_DIR}/re2-${RE2_VERSION}"
 
-make ${NPROCS:+-j ${NPROCS}} CPPFLAGS=-fPIC
-make ${NPROCS:+-j ${NPROCS}} static-install
+mkdir cmake-build && cd cmake-build
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr/local \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+    -DBUILD_SHARED_LIBS=OFF
+cmake --build . --target install ${NPROCS:+-j ${NPROCS}}
