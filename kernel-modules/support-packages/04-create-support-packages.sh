94fe3a273f65f9d27ced36912c0f7f5f299a20aa
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-07-21 09:56:15 +0200

    Use number of elements

--- kernel-modules/support-packages/04-create-support-packages.sh
+++ kernel-modules/support-packages/04-create-support-packages.sh
@@ -26,7 +26,7 @@ use_downstream() {
     IFS='.' read -ra version <<< "${1%-*}"
     min_version=(2 6 0)
 
-    for ((i = 0; i < ${min_version[@]}; i++)); do
+    for ((i = 0; i < ${#min_version[@]}; i++)); do
         if ((version[i] < min_version[i])); then
             return 1
         fi
