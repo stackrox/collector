1a3105e115a52f835338a24cbd7c70dd0433137c
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-07-24 12:50:55 +0200

    Use version 2.5.0 for downstream driver usage validation

--- kernel-modules/support-packages/04-create-support-packages.sh
+++ kernel-modules/support-packages/04-create-support-packages.sh
@@ -24,8 +24,7 @@ compress_files() (
 
 use_downstream() {
     IFS='.' read -ra version <<< "${1%-*}"
-    # min_version=(2 6 0)
-    min_version=(2 5 0)
+    min_version=(2 6 0)
 
     for ((i = 0; i < ${#min_version[@]}; i++)); do
         if ((version[i] < min_version[i])); then
