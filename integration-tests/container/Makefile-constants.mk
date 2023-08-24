d7e390c4ad67aa700dc7b782b0403687d66b4162
Giles Hutton <ghutton@redhat.com>
2023-08-24 14:54:53 +0100

    Switches to using buildx properly for multi-arch and bumps grpc server image

--- integration-tests/container/Makefile-constants.mk
+++ integration-tests/container/Makefile-constants.mk
@@ -1,5 +1,8 @@
+# PLATFORM can be a comma separated list of platforms of the form linux/<arch>
+# it is passed to buildx, which will build for each one (this is important
+# for pushing multi-arch images because `buildx --push` will push a manifest list
+# containing all the relevant platforms)
 PLATFORM ?= linux/amd64
-IMAGE_ARCH = $(word 2,$(subst /, ,$(PLATFORM)))
 
 # path is relative to the build directory (i.e. subdirectories of integration-test/container)
 COLLECTOR_QA_TAG ?= $(shell cat "../QA_TAG")
