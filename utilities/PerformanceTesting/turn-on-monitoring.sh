bcb6eb68cda9f8902c58e65330523711e37fdbdc
JoukoVirtanen <jouko@stackrox.com>
2023-08-29 16:40:26 -0700

    Removed PerformanceTesting

--- utilities/PerformanceTesting/turn-on-monitoring.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-artifacts_dir=$1
-
-export KUBECONFIG=$artifacts_dir/kubeconfig
-
-# TODO put in a check for if monitoring is already on. Probably do that in another pr
-(oc label namespace/stackrox openshift.io/cluster-monitoring="true" && sleep 120) || true
