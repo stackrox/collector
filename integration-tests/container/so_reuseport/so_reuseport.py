bde5310273def336163fdbbbca77507b25ecbd6f
JoukoVirtanen <jouko@stackrox.com>
2023-09-19 11:25:12 -0700

    Removed comment

--- integration-tests/container/so_reuseport/so_reuseport.py
+++ integration-tests/container/so_reuseport/so_reuseport.py
@@ -4,6 +4,6 @@ import time
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 s.bind(('0.0.0.0', 8080))
-s.listen(5)  # Listen for incoming connections, with a backlog of 5
+s.listen(5)
 
 time.sleep(10000)
