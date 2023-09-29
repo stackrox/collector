150ad1a4c35a8538221300daf1ef754c2f71c534
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-09-29 18:09:28 +0200

    Create fake proc dir from the integration test directly

--- integration-tests/suites/missing_proc_scrape.go
+++ integration-tests/suites/missing_proc_scrape.go
@@ -1,7 +1,11 @@
 package suites
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/stackrox/collector/integration-tests/suites/common"
 	"github.com/stretchr/testify/assert"
@@ -11,15 +15,53 @@ type MissingProcScrapeTestSuite struct {
 	IntegrationTestSuiteBase
 }
 
-const fakeProcDir = "/tmp/fake-proc"
+const (
+	fakePid      = 8989
+	fakeProcName = "short"
+	fakeProcDir  = "/tmp/fake-proc"
+)
+
+func (s *MissingProcScrapeTestSuite) createFakeProcDir() {
+	fakeProc := fmt.Sprintf("%s/%d", fakeProcDir, fakePid)
+	err := os.MkdirAll(fakeProc, os.ModePerm)
+	s.Require().NoError(err, "Failed to create fake proc directory")
+
+	err = os.Symlink("/bin/sh", filepath.Join(fakeProc, "exe"))
+	s.Require().NoError(err, "Failed to symlink exe")
+
+	status := fmt.Sprintf("Name: %s\n", fakeProcName)
+	err = ioutil.WriteFile(filepath.Join(fakeProc, "status"), []byte(status), 0644)
+	s.Require().NoError(err, "Failed to create status file")
+
+	cmdline := fmt.Sprintf("/bin/%s\n", fakeProcName)
+	err = ioutil.WriteFile(filepath.Join(fakeProc, "cmdline"), []byte(cmdline), 0644)
+	s.Require().NoError(err, "Failed to create cmdline file")
+
+	f, err := os.OpenFile(filepath.Join(fakeProc, "environ"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	s.Require().NoError(err, "Failed to create environ file")
+	defer f.Close()
+
+	for _, v := range os.Environ() {
+		_, err := f.WriteString(v + "\n")
+		s.Require().NoError(err, "Failed to write to environ")
+	}
+
+	err = ioutil.WriteFile(filepath.Join(fakeProcDir, "stat"), []byte("btime 1695972922\n"), 0644)
+	s.Require().NoError(err, "Failed to write to stat")
+
+	err = os.MkdirAll(filepath.Join(fakeProcDir, "1"), os.ModePerm)
+	s.Require().NoError(err, "Failed to create /proc/1")
+
+	err = ioutil.WriteFile(filepath.Join(fakeProcDir, "1", "mounts"), []byte("cgroup2 /sys/fs/cgroup cgroup2 rw,seclabel,nosuid,nodev,noexec,relatime,nsdelegate,memory_recursiveprot 0 0\n"), 0644)
+	s.Require().NoError(err, "Failed to write to /proc/1/mounts")
+}
 
 func (s *MissingProcScrapeTestSuite) SetupSuite() {
 	s.RegisterCleanup()
 
 	_, err := os.Stat(fakeProcDir)
 	if os.IsNotExist(err) {
-		err = os.MkdirAll(fakeProcDir, os.ModePerm)
-		s.Require().NoError(err, "Failed to create fake proc directory")
+		s.createFakeProcDir()
 	}
 
 	collectorOptions := common.CollectorStartupOptions{
