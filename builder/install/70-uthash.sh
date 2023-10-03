b1025af09cc38257caab884fed58fdc3993838e3
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-10-03 14:58:00 +0200

    Cleanups + use falco 0.13.1
    Add uthash submodule

--- builder/install/70-uthash.sh
+++ builder/install/70-uthash.sh
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/env bash
-
-set -ex
-
-cd third_party/uthash
-cp LICENSE "${LICENSE_DIR}/uthash-${UTHASH_VERSION}"
-
-# uthash is a header only library, we just copy the one file we need
-# into /usr/local/include and call it a day
-cp src/uthash.h /usr/local/include
-||||||| parent of 1df1465a2 (Cleanups + use falco 0.13.1)
-=======
 #!/usr/bin/env bash
 
 set -e
@@ -19,6 +6,5 @@ cd third_party/uthash
 cp LICENSE "${LICENSE_DIR}/uthash-${UTHASH_VERSION}"
 
 # uthash is a header only library, we just copy the one file we need
-# into /usr/include and call it a day
-cp src/uthash.h /usr/include
->>>>>>> 1df1465a2 (Cleanups + use falco 0.13.1)
+# into /usr/local/include and call it a day
+cp src/uthash.h /usr/local/include
