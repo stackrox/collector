8be9e85def62031d81ff899c4b5342985a6b318e
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-10-05 11:14:19 +0200

    Mounts /sys/kernel/debug directly
    Fix locations for BTF symbols

--- builder/install/70-uthash.sh
+++ builder/install/70-uthash.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 cd third_party/uthash
 cp LICENSE "${LICENSE_DIR}/uthash-${UTHASH_VERSION}"
 
 # uthash is a header only library, we just copy the one file we need
-# into /usr/include and call it a day
-cp src/uthash.h /usr/include
+# into /usr/local/include and call it a day
+cp src/uthash.h /usr/local/include
