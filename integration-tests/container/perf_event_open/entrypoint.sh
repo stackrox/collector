5205825e49add3da6f76537606724395a67310ab
Dmitrii Dolgov <9erthalion6@gmail.com>
2023-10-12 13:41:52 +0200

    Hide the load output

--- integration-tests/container/perf_event_open/entrypoint.sh
+++ integration-tests/container/perf_event_open/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # start workload to generate sched:sched_process_exit on cpu 0
-while true; do taskset -c 0 ls; done &
+while true; do taskset -c 0 ls &> /dev/null; done &
 
 # start capturing
 /sched_process_exit
