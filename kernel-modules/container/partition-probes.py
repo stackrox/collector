a3b1afa2c0d98c6c05c6d2348030f5431f014276
Giles Hutton <ghutton@redhat.com>
2023-12-06 17:11:28 +0000

    disable full builds for s390x and power, forcing core-bpf testing

--- kernel-modules/container/partition-probes.py
+++ kernel-modules/container/partition-probes.py
@@ -4,18 +4,18 @@
 # image can be properly cached by CloudFlare. Given a directory that contains
 # kernel probes and max MB per layer, output a a manifest file for each layer.
 
+import collections
 import os
 import re
 import sys
 
 probe_name_re = re.compile(r'^collector.*.gz$')
 
-
 def buckets_from_probe_dir(probe_dir, bytes_per_layer):
     probe_info = []
     for f in os.scandir(probe_dir):
         if probe_name_re.match(f.name):
-            probe_info.append({"name": f.name, "mtime": f.stat().st_mtime, "size": f.stat().st_size})
+            probe_info.append({"name":f.name, "mtime":f.stat().st_mtime, "size":f.stat().st_size})
 
     remaining_bucket_bytes = 0
     result = []
@@ -29,21 +29,12 @@ def buckets_from_probe_dir(probe_dir, bytes_per_layer):
 
     return result
 
-
 def write_layer_manifests(probe_buckets, output_dir):
     os.makedirs(output_dir, exist_ok=True)
-
-    def write_layer_file(name, probes=()):
-        with open(name, 'w') as f:
-            f.writelines("{0}\n".format(probe_info['name'] for probe_info in probes))
-
-    if len(probe_buckets) == 0:
-        # Write a zero-length bucket if there are no drivers
-        write_layer_file(os.path.join(output_dir, "0"))
-
     for idx, probes in enumerate(probe_buckets):
-        write_layer_file(os.path.join(output_dir, "{}".format(idx)), probes)
-
+        layer_file = os.path.join(output_dir, "{}".format(idx))
+        with open(layer_file, "w") as f:
+            f.writelines("%s\n" % probe_info["name"] for probe_info in probes)
 
 def main(args):
     if len(args) != 5:
@@ -60,7 +51,6 @@ def main(args):
     print(len(probe_buckets))
     if output_dir != "-":
         write_layer_manifests(probe_buckets, output_dir)
-
 
 if __name__ == '__main__':
     main(sys.argv)
