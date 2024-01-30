eb2dc3a5f57391d386cc1d5aea7b2f0a1c037d24
Olivier Valentin <ovalenti@redhat.com>
2024-01-30 10:01:52 +0100

    Consider an empty env-var as potentially valid.
    The boolean parser is made to still return the default value in
    this case, but now will print a warning message.

--- collector/lib/EnvVar.h
+++ collector/lib/EnvVar.h
@@ -26,7 +26,7 @@ class EnvVar {
   const T& value() const {
     std::call_once(init_once_, [this]() {
       const char* env_val = std::getenv(env_var_name_);
-      if (!env_val || !*env_val) {
+      if (!env_val) {
         return;
       }
       if (!ParseT()(&val_, env_val)) {
@@ -50,6 +50,9 @@ namespace internal {
 
 struct ParseBool {
   bool operator()(bool* out, std::string str_val) const {
+    if (str_val.empty()) {
+      return false;
+    }
     std::transform(str_val.begin(), str_val.end(), str_val.begin(), [](char c) -> char {
       return static_cast<char>(std::tolower(c));
     });
