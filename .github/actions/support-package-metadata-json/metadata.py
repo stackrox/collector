5b0869d1967c8b4284be052dfdd4baa03c832bc7
Giles Hutton <ghutton@redhat.com>
2024-03-15 16:09:00 +0000

    Account for non-directories in metadata script

--- .github/actions/support-package-metadata-json/metadata.py
+++ .github/actions/support-package-metadata-json/metadata.py
@@ -11,19 +11,22 @@ def main():
     for mod_ver in os.listdir(support_pkg_dir):
         version_dir = os.path.join(support_pkg_dir, mod_ver)
 
-        with open(os.path.join(version_dir, 'latest'), 'r') as f:
-            latest_file = f.readline().strip()
+        try:
+            with open(os.path.join(version_dir, 'latest'), 'r') as f:
+                latest_file = f.readline().strip()
 
-            st = os.stat(os.path.join(version_dir, latest_file))
-            last_mod_time = datetime.utcfromtimestamp(st.st_mtime).strftime('%Y/%m/%d, %H:%M:%S')
+                st = os.stat(os.path.join(version_dir, latest_file))
+                last_mod_time = datetime.utcfromtimestamp(st.st_mtime).strftime('%Y/%m/%d, %H:%M:%S')
 
-            metadata = {
-                'file_name': latest_file,
-                'last_modified': last_mod_time,
-            }
+                metadata = {
+                    'file_name': latest_file,
+                    'last_modified': last_mod_time,
+                }
 
-            with open(os.path.join(version_dir, 'metadata.json'), 'w') as output:
-                json.dump(metadata, output)
+                with open(os.path.join(version_dir, 'metadata.json'), 'w') as output:
+                    json.dump(metadata, output)
+        except NotADirectoryError:
+            continue
 
 
 if __name__ == '__main__':
