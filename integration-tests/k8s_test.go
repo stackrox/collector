5cf44daabe254a5adf5463469a5969e8ff190afa
Olivier Valentin <ovalenti@redhat.com>
2024-04-23 18:39:52 +0200

    [temp] replace k8s_namespace test with runtime-control
    We have a problem running those tests in a raw for now.

--- integration-tests/k8s_test.go
+++ integration-tests/k8s_test.go
@@ -11,9 +11,9 @@ import (
 	"github.com/stackrox/collector/integration-tests/suites"
 )
 
-func TestK8sNamespace(t *testing.T) {
+func TestK8sRuntimeControl(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Not running k8s in short mode")
 	}
-	suite.Run(t, new(suites.K8sNamespaceTestSuite))
+	suite.Run(t, new(suites.RuntimeControlTestSuite))
 }
