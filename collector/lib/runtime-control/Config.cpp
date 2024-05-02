68c3ff86294da54f9abf37ff865d90ac8f87d8cd
JoukoVirtanen <jouko@stackrox.com>
2024-05-01 21:02:31 -0700

    Brought in needed changes but not compiling

---
+++ collector/lib/runtime-control/Config.cpp
@@ -0,0 +1,118 @@
+#include <runtime-control/Config.h>
+
+#include "Logging.h"
+
+namespace collector::runtime_control {
+
+Config& Config::GetOrCreate() {
+  static Config config;
+
+  return config;
+}
+
+bool Config::WaitUntilInitialized(unsigned int timeout_ms) {
+  std::unique_lock<std::mutex> lock(mutex_);
+
+  return config_message_ || (condition_.wait_for(lock, std::chrono::milliseconds(timeout_ms)) == std::cv_status::no_timeout);
+}
+
+void Config::Update(const storage::RuntimeFilteringConfiguration& msg) {
+  std::unique_lock<std::mutex> lock(mutex_);
+
+  config_message_.emplace(msg);
+  ConfigMessageToConfig(msg);
+  std::string cluster = "cluster-1";  // Cluster is always the same, so should probably not use it anywhere. Using it but hardcoding it for now.
+  SetBitMasksForNamespaces(cluster);
+
+  condition_.notify_all();
+}
+
+bool Config::IsFeatureEnabled(uint64_t bitMask, storage::RuntimeFilter_RuntimeFilterFeatures feature) {
+  CLOG(INFO) << "bitMask= " << bitMask;
+  return (bitMask & kFeatureFlags_[feature]) != 0;
+}
+
+void Config::ConfigMessageToConfig(const storage::RuntimeFilteringConfiguration& msg) {
+  for (storage::RuntimeFilter runtimeConfig : msg.runtime_filters()) {
+    if (runtimeConfig.default_status().compare("on") == 0) {
+      default_status_map_[runtimeConfig.feature()] = true;
+    } else {
+      default_status_map_[runtimeConfig.feature()] = false;
+    }
+
+    for (auto runtimeConfigRule : runtimeConfig.rules()) {
+      config_by_feature_[runtimeConfig.feature()].push_back(runtimeConfigRule);
+    }
+  }
+
+  for (storage::ResourceCollection rc : msg.resource_collections()) {
+    rcMap_[rc.id()] = rc;
+  }
+}
+
+bool Config::IsFeatureEnabled(std::string cluster, std::string ns, storage::RuntimeFilter_RuntimeFilterFeatures feature) {
+  auto filteringRulesPair = config_by_feature_.find(feature);
+  if (filteringRulesPair != config_by_feature_.end()) {
+    bool defaultStatus = default_status_map_[feature];
+    auto filteringRules = filteringRulesPair->second;
+    return ResourceSelector::IsFeatureEnabledForClusterAndNamespace(filteringRules, rcMap_, defaultStatus, cluster, ns);
+  } else {
+    return false;
+  }
+}
+
+uint64_t Config::SetFeatureBitMask(uint64_t bitMask, bool enabled, storage::RuntimeFilter_RuntimeFilterFeatures feature) {
+  bitMask = enabled ? (bitMask | kFeatureFlags_[feature]) : (bitMask & ~kFeatureFlags_[feature]);
+  CLOG(INFO) << "bitMask= " << bitMask;
+  return bitMask;
+}
+
+void Config::SetBitMask(std::string cluster, std::string ns) {
+  uint64_t bitMask = 0;
+  for (int i = 0; i < storage::RuntimeFilter_RuntimeFilterFeatures_RuntimeFilterFeatures_ARRAYSIZE; i++) {
+    storage::RuntimeFilter_RuntimeFilterFeatures feature = static_cast<storage::RuntimeFilter_RuntimeFilterFeatures>(i);
+    bool enabled = IsFeatureEnabled(cluster, ns, feature);
+    bitMask = SetFeatureBitMask(bitMask, enabled, feature);
+  }
+  namespaceFeatureBitMask_[ns] = bitMask;
+}
+
+void Config::SetBitMasksForNamespaces(std::string cluster) {
+  for (auto it = namespaceFeatureBitMask_.begin(); it != namespaceFeatureBitMask_.end();) {
+    auto& pair = *it;
+    std::string ns = pair.first;
+    SetBitMask(cluster, ns);
+  }
+}
+
+void Config::SetBitMask(std::string cluster, std::string container_id, std::string ns) {
+  SetBitMask(cluster, ns);
+  containerFeatureBitMask_[container_id] = &namespaceFeatureBitMask_[ns];
+  CLOG(INFO) << "namespaceFeatureBitMask_[ns]= " << namespaceFeatureBitMask_[ns];
+  CLOG(INFO) << "*containerFeatureBitMask_[container_id]= " << *containerFeatureBitMask_[container_id];
+}
+
+// Should probably just take in the container_id, check the containerFeatureBitMask_ map, and only figure out the namespace if the container is not in the map.
+// For now this is easier for testing purposes.
+bool Config::IsFeatureEnabled(std::string cluster, std::string ns, std::string container_id, storage::RuntimeFilter_RuntimeFilterFeatures feature) {
+  auto bitMaskPair = containerFeatureBitMask_.find(container_id);
+  if (bitMaskPair != containerFeatureBitMask_.end()) {
+    CLOG(INFO) << "Found in container map";
+    uint64_t* bitMask = bitMaskPair->second;
+    return IsFeatureEnabled(*bitMask, feature);
+  } else {
+    auto bitMaskPairNs = namespaceFeatureBitMask_.find(ns);
+    if (bitMaskPairNs != namespaceFeatureBitMask_.end()) {
+      CLOG(INFO) << "Found in namespace map";
+      uint64_t bitMask = bitMaskPairNs->second;
+      containerFeatureBitMask_[container_id] = &namespaceFeatureBitMask_[ns];
+      return IsFeatureEnabled(bitMask, feature);
+    } else {
+      SetBitMask(cluster, container_id, ns);
+      uint64_t bitMask = *containerFeatureBitMask_[container_id];
+      return IsFeatureEnabled(bitMask, feature);
+    }
+  }
+}
+
+}  // namespace collector::runtime_control
---
+++ collector/lib/runtime-control/Config.h
@@ -0,0 +1,54 @@
+#ifndef _RUNTIME_CONTROL_CONFIG_H_
+#define _RUNTIME_CONTROL_CONFIG_H_
+
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
+#include <optional>
+
+#include <storage/runtime_filters.pb.h>
+
+#include "Hash.h"
+
+#include "../ResourceSelector.h"
+
+namespace collector::runtime_control {
+
+class Config {
+ public:
+  Config() {
+    for (int i = 0; i < storage::RuntimeFilter_RuntimeFilterFeatures_RuntimeFilterFeatures_ARRAYSIZE; i++) {
+      kFeatureFlags_[i] = 1UL << (i + 1);
+    }
+  }
+
+  static Config& GetOrCreate();
+
+  // returns true when initialized, false for a timeout.
+  bool WaitUntilInitialized(unsigned int timeout_ms);
+
+  void Update(const storage::RuntimeFilteringConfiguration& msg);
+  bool IsFeatureEnabled(uint64_t bitMask, storage::RuntimeFilter_RuntimeFilterFeatures feature);
+  bool IsFeatureEnabled(std::string cluster, std::string ns, std::string container_id, storage::RuntimeFilter_RuntimeFilterFeatures feature);
+  bool IsFeatureEnabled(std::string cluster, std::string ns, storage::RuntimeFilter_RuntimeFilterFeatures feature);
+  void ConfigMessageToConfig(const storage::RuntimeFilteringConfiguration& msg);
+
+ private:
+  std::mutex mutex_;
+  std::condition_variable condition_;
+  std::optional<storage::RuntimeFilteringConfiguration> config_message_;
+  uint64_t kFeatureFlags_[storage::RuntimeFilter_RuntimeFilterFeatures_RuntimeFilterFeatures_ARRAYSIZE];
+  UnorderedMap<std::string, uint64_t> namespaceFeatureBitMask_;
+  UnorderedMap<std::string, uint64_t*> containerFeatureBitMask_;
+  uint64_t SetFeatureBitMask(uint64_t bitMask, bool enabled, storage::RuntimeFilter_RuntimeFilterFeatures feature);
+  void SetBitMask(std::string cluster, std::string ns);
+  void SetBitMask(std::string cluster, std::string container_id, std::string ns);
+  void SetBitMasksForNamespaces(std::string cluster);
+  UnorderedMap<storage::RuntimeFilter_RuntimeFilterFeatures, bool> default_status_map_;
+  UnorderedMap<storage::RuntimeFilter_RuntimeFilterFeatures, std::vector<storage::RuntimeFilter_RuntimeFilterRule> > config_by_feature_;
+  UnorderedMap<std::string, storage::ResourceCollection> rcMap_;
+};
+
+}  // namespace collector::runtime_control
+
+#endif
