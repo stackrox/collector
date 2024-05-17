8daa7a7bbbb32042f3cb5a67b25a382d6a726b12
Robby Cochran <rcochran@redhat.com>
2024-05-17 13:25:58 -0700

    script cleanup

--- ansible/group_vars/fetch_ocp_rhcos_bootimage.sh
+++ ansible/group_vars/fetch_ocp_rhcos_bootimage.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-# Check if release version is provided as an argument
 if [ -z "$1" ]; then
   echo "Usage: $0 <ocp-version> [<architectures.path>]"
   exit 1
 fi
 
-# Release version from the first argument
-RELEASE_VERSION=$1
+OCP_VERSION=$1
 
-# Json path with architecture and image name
-# e.g., s390x.artifacts.ibmcloud.release
+# Json path with architecture and image name, e.g., x86_64.images.gcp.name or s390x.artifacts.ibmcloud.release
 JSONPATH=${2:-"x86_64.images.gcp.name"}
 
-
-# URL of the JSON data
-URL="https://raw.githubusercontent.com/openshift/installer/release-${RELEASE_VERSION}/data/data/coreos/rhcos.json"
+URL="https://raw.githubusercontent.com/openshift/installer/release-${OCP_VERSION}/data/data/coreos/rhcos.json"
 
 json_data=$(curl -s "$URL")
 if [ -z "$json_data" ]; then
@@ -28,8 +23,5 @@ if [ "$image_name" == "null" ]; then
   echo "Failed to parse JSON data or path does not exist"
   exit 1
 fi
-
-#echo "The image name for release $RELEASE_VERSION and architecture $ARCHITECTURE is: $image_name"
 echo "$image_name"
-
 
