219a7e11945655448b7a542864dc634eb91ada58
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2024-06-12 16:44:19 +0200

    Move drops on threadcache to userspace metrics

--- collector/lib/system-inspector/SystemInspector.h
+++ collector/lib/system-inspector/SystemInspector.h
@@ -17,19 +17,17 @@ struct Stats {
   using uint64_t = std::uint64_t;
 
   // stats gathered in kernel space
-  volatile uint64_t nEvents = 0;            // the number of kernel events
-  volatile uint64_t nDrops = 0;             // the number of drops
-  volatile uint64_t nDropsBuffer = 0;       // the number of drops due to full
-                                            // ringbuf
-  volatile uint64_t nDropsThreadCache = 0;  // the number of drops due to full
-                                            // thread cache
-  volatile uint64_t nPreemptions = 0;       // the number of preemptions
+  volatile uint64_t nEvents = 0;       // the number of kernel events
+  volatile uint64_t nDrops = 0;        // the number of drops
+  volatile uint64_t nDropsBuffer = 0;  // the number of drops due to full ringbuf
+  volatile uint64_t nPreemptions = 0;  // the number of preemptions
 
   // stats gathered in user space
   volatile uint64_t nFilteredEvents[PPM_EVENT_MAX] = {0};   // events post filtering
   volatile uint64_t nUserspaceEvents[PPM_EVENT_MAX] = {0};  // events processed by userspace
   volatile uint64_t nGRPCSendFailures = 0;                  // number of signals that were not sent on GRPC
   volatile uint64_t nThreadCacheSize = 0;                   // number of thread-info entries stored in the cache
+  volatile uint64_t nDropsThreadCache = 0;                  // the number of drops due to full thread cache
 
   // process related metrics
   volatile uint64_t nProcessSent = 0;                       // number of process signals sent
