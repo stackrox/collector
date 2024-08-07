ebae04967905337feafc46f38bd8d0fbf7a26d8c
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2024-08-07 11:25:58 +0200

    Skip MODULE_VERSION if it doesn't exist

--- .github/actions/support-package-metadata/create-metadata-dir.sh
+++ .github/actions/support-package-metadata/create-metadata-dir.sh
@@ -6,7 +6,10 @@ for version_dir in /tmp/support-packages/metadata/collector-versions/*; do
     version="$(basename "$version_dir")"
     git checkout "${version}"
 
-    cp "${GITHUB_WORKSPACE}/kernel-modules/MODULE_VERSION" "${version_dir}/MODULE_VERSION"
+    module_version_file="${GITHUB_WORKSPACE}/kernel-modules/MODULE_VERSION"
+    if [[ -f "${module_version_file}" ]]; then
+        cp "${module_version_file}" "${version_dir}/MODULE_VERSION"
+    fi
 done
 
 git checkout "${BRANCH_NAME}"
