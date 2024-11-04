77d79944a190c039d9a3707c41034b2d71585782
Dmitrii Dolgov <9erthalion6@gmail.com>
2024-11-04 16:21:12 +0100

    Keep rpm to confirm glib version

--- collector/container/rhel/install.sh
+++ collector/container/rhel/install.sh
@@ -6,5 +6,5 @@ microdnf upgrade -y --nobest
 microdnf install -y kmod findutils elfutils-libelf gdb procps
 
 microdnf clean all
-rpm --query --all 'curl' '*rpm*' '*dnf*' '*libsolv*' '*hawkey*' 'yum*' 'findutils' | xargs -t rpm -e --nodeps
+#rpm --query --all 'curl' '*rpm*' '*dnf*' '*libsolv*' '*hawkey*' 'yum*' 'findutils' | xargs -t rpm -e --nodeps
 rm -rf /var/cache/yum
