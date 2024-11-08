8b4c1e5961d67a4d73a4eccf0018b00cad1ba38c
Giles Hutton <ghutton@redhat.com>
2024-11-08 10:20:06 +0000

    Removes network stress integration test

--- integration-tests/integration_test.go
+++ integration-tests/integration_test.go
@@ -529,7 +529,3 @@ func TestUdpNetworkFlow(t *testing.T) {
 	}
 	suite.Run(t, new(suites.UdpNetworkFlow))
 }
-
-func TestNetworkStress(t *testing.T) {
-	suite.Run(t, new(suites.NetworkStressSuite))
-}
