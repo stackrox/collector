22e8c3e86d41c97070f26ce14b89a2816cd6eb7d
Giles Hutton <ghutton@redhat.com>
2024-11-13 16:59:11 +0000

    Add Rust toolchains and example rust library to Collector

---
+++ collector/lib/rust/test/src/lib.rs
@@ -0,0 +1,19 @@
+#[no_mangle]
+pub extern "C" fn external_add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
