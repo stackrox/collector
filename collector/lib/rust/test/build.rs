1a95c84b841028f372fc06b5710820b9eda7d58d
Giles Hutton <ghutton@redhat.com>
2024-11-13 17:55:52 +0000

    Default to include directory

--- collector/lib/rust/test/build.rs
+++ collector/lib/rust/test/build.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 
 fn main(){
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let target_dir = env::var("BINDINGS_INCLUDE_DIRECTORY").unwrap_or(".".to_string());
+    let target_dir = env::var("BINDINGS_INCLUDE_DIRECTORY").unwrap_or("./include".to_string());
 
     let path = Path::new(&target_dir).join("RustTest.h");
 
