f157ab85052c1bcb46c07edad36feeb81aa546d2
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-03-26 15:21:06 +0100

    Restore ProcessSignalFormatter, add SensorClientFormatter
    This change makes it possible to have backwards compatibility with older
    sensor versions that might no implement the new service.

--- collector/lib/CollectorOutput.cpp
+++ collector/lib/CollectorOutput.cpp
@@ -1,6 +1,34 @@
 #include "CollectorOutput.h"
 
+#include "internalapi/sensor/collector.pb.h"
+#include "internalapi/sensor/collector_iservice.pb.h"
+
+#include "HostInfo.h"
+
 namespace collector {
+
+CollectorOutput::CollectorOutput(const CollectorConfig& config) {
+  if (config.grpc_channel != nullptr) {
+    auto sensor_client = std::make_unique<SensorClient>(config.grpc_channel);
+    auto signal_client = std::make_unique<SignalServiceClient>(config.grpc_channel);
+    sensor_clients_.emplace_back(std::move(sensor_client));
+    signal_clients_.emplace_back(std::move(signal_client));
+  }
+
+  if (config.UseStdout()) {
+    auto sensor_client = std::make_unique<SensorClientStdout>();
+    auto signal_client = std::make_unique<StdoutSignalServiceClient>();
+    sensor_clients_.emplace_back(std::move(sensor_client));
+    signal_clients_.emplace_back(std::move(signal_client));
+  }
+
+  if (sensor_clients_.empty() || signal_clients_.empty()) {
+    CLOG(FATAL) << "No available output configured";
+  }
+
+  StartClients(sensor_clients_);
+  StartClients(signal_clients_);
+}
 
 namespace {
 SignalHandler::Result SensorOutput(std::vector<std::unique_ptr<ISensorClient>>& clients, const sensor::MsgFromCollector& msg) {
@@ -38,5 +66,20 @@ SignalHandler::Result CollectorOutput::SendMsg(const MessageType& msg) {
   };
 
   return std::visit(visitor, msg);
+}
+
+void CollectorOutput::Register() {
+  sensor::MsgFromCollector msg;
+  msg.clear_info();
+  msg.clear_process_signal();
+  msg.mutable_register_()->set_hostname(GetHostname());
+
+  for (const auto& client : sensor_clients_) {
+    auto res = client->SendMsg(msg);
+    if (res != SignalHandler::PROCESSED) {
+      use_sensor_client_ = false;
+      break;
+    }
+  }
 }
 }  // namespace collector
