596de85c9591ca74a7f218a95f35ade433c6c35c
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-03-28 12:18:03 +0100

    Unify client reconnection thread in CollectorOutput

--- collector/lib/SignalServiceClient.cpp
+++ collector/lib/SignalServiceClient.cpp
@@ -1,61 +1,23 @@
 #include "SignalServiceClient.h"
 
-#include <fstream>
-
-#include "GRPCUtil.h"
 #include "Logging.h"
-#include "ProtoUtil.h"
 #include "Utility.h"
 
 namespace collector {
 
-bool SignalServiceClient::EstablishGRPCStreamSingle() {
-  std::mutex mtx;
-  std::unique_lock<std::mutex> lock(mtx);
-  stream_interrupted_.wait(lock, [this]() { return !stream_active_.load(std::memory_order_acquire) || thread_.should_stop(); });
-  if (thread_.should_stop()) {
-    return false;
-  }
-
-  CLOG(INFO) << "Trying to establish GRPC stream for signals ...";
-
-  if (!WaitForChannelReady(channel_, [this]() { return thread_.should_stop(); })) {
-    return false;
-  }
-  if (thread_.should_stop()) {
-    return false;
-  }
-
-  // stream writer
+bool SignalServiceClient::Refresh() {
   context_ = std::make_unique<grpc::ClientContext>();
   writer_ = DuplexClient::CreateWithReadsIgnored(&SignalService::Stub::AsyncPushSignals, channel_, context_.get());
   if (!writer_->WaitUntilStarted(std::chrono::seconds(30))) {
     CLOG(ERROR) << "Signal stream not ready after 30 seconds. Retrying ...";
     CLOG(ERROR) << "Error message: " << writer_->FinishNow().error_message();
     writer_.reset();
-    return true;
+    return false;
   }
-  CLOG(INFO) << "Successfully established GRPC stream for signals.";
 
   first_write_ = true;
   stream_active_.store(true, std::memory_order_release);
   return true;
-}
-
-void SignalServiceClient::EstablishGRPCStream() {
-  while (EstablishGRPCStreamSingle());
-  CLOG(INFO) << "Signal service client terminating.";
-}
-
-void SignalServiceClient::Start() {
-  thread_.Start([this] { EstablishGRPCStream(); });
-}
-
-void SignalServiceClient::Stop() {
-  stream_interrupted_.notify_one();
-  thread_.Stop();
-  context_->TryCancel();
-  context_.reset();
 }
 
 SignalHandler::Result SignalServiceClient::PushSignals(const SignalStreamMessage& msg) {
@@ -76,10 +38,7 @@ SignalHandler::Result SignalServiceClient::PushSignals(const SignalStreamMessage
       CLOG(ERROR) << "GRPC writes failed: " << status.error_message();
     }
     writer_.reset();
-
     stream_active_.store(false, std::memory_order_release);
-    CLOG(ERROR) << "GRPC stream interrupted";
-    stream_interrupted_.notify_one();
     return SignalHandler::ERROR;
   }
 
