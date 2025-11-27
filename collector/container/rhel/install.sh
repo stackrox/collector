4ee8d1f2b7e47ac47203ecd770964cc800a3ac23
Olivier Valentin <ovalenti@redhat.com>
2025-11-27 15:17:52 +0100

    ROX-31971: update ubi and centos base images to 10 (#2692)

--- collector/container/rhel/install.sh
+++ collector/container/rhel/install.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-# UBI 9 requires confirmation with -y flag.
 microdnf upgrade -y --nobest
 microdnf install -y elfutils-libelf
 
 microdnf clean all
 # shellcheck disable=SC2046
 rpm --verbose -e --nodeps $(
-    rpm -qa 'curl' '*rpm*' '*dnf*' '*libsolv*' '*hawkey*' 'yum*' 'libyaml*' 'libarchive*'
+    rpm -qa '*rpm*' '*dnf*' '*libsolv*' '*hawkey*' 'yum*' 'libyaml*' 'libarchive*'
 )
 rm -rf /var/cache/yum
