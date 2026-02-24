9f3cc5b0a5755bfd3cdd7434fa6d3f419f45003a
Olivier Valentin <ovalenti@redhat.com>
2026-02-24 14:41:59 +0100

    Acknoledge quoting for package names

--- collector/container/rhel/install.sh
+++ collector/container/rhel/install.sh
@@ -8,6 +8,7 @@ microdnf clean all
 
 SUPERFLUOUS_PACKAGES=$(/usr/local/bin/orphaner bash curl elfutils-libelf openssl-libs libuuid libstdc++ libcurl-minimal)
 
+# shellcheck disable=SC2046,SC2086
 rpm --verbose -e --nodeps $(rpm -qa ${SUPERFLUOUS_PACKAGES})
 
 rm -rf /var/cache/yum /usr/local/bin/orphaner
