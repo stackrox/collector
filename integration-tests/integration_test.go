95691a927e4f4b86d9666ff0850317b926ac212c
Olivier Valentin <ovalenti@redhat.com>
2026-09-21 16:28:36 +0200

    Reproduce collector crash
    ... using a delay in the mock Sensor netflow processing.

--- integration-tests/integration_test.go
+++ integration-tests/integration_test.go
@@ -623,3 +623,7 @@ func TestGetStatus(t *testing.T) {
 func TestLogLevelEndpoint(t *testing.T) {
 	suite.Run(t, new(suites.LogLevelTestSuite))
 }
+
+func TestGRPCStreamDelay(t *testing.T) {
+	suite.Run(t, new(suites.GRPCStreamDelayTestSuite))
+}
